bf2119b2ee2e275be565f526b0b1fc59f66674c9
el_kaboing <el_kaboing@localhost>
2006-08-17 15:49:27 +0000

    Removing the ssl/jks integration tests. They are replaced with the security integration tests which will be committed soon.

--- opends/tests/integration-tests-testng/src/server/org/opends/server/integration/ssl/jks/generate_server_cert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-keytool -genkey -alias server-cert -keyalg rsa -dname "cn=server-cert,dc=com" -keystore "keystore" -storepass "servercert" -keypass "servercert" 
-
-keytool -selfcert -alias server-cert -keystore "keystore" -storepass "servercert" 
-
