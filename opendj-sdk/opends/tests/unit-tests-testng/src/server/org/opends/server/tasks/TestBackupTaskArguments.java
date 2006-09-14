bf2ad9edbffafaf46564cfb641ad862dd350de10
coulbeck <coulbeck@localhost>
2006-09-14 20:41:34 +0000

    Add some unit test cases for backup tasks.

---
+++ opendj-sdk/opends/tests/unit-tests-testng/src/server/org/opends/server/tasks/TestBackupTaskArguments.java
@@ -0,0 +1,178 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying * information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2006 Sun Microsystems, Inc.
+ */
+
+
+package org.opends.server.tasks;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.opends.server.backends.task.TaskState;
+import org.opends.server.types.*;
+import org.opends.server.TestCaseUtils;
+
+import java.util.UUID;
+
+/**
+ * Tests various valid and invalid combinations of arguments to the backup task.
+ */
+public class TestBackupTaskArguments extends TasksTestCase
+{
+  @BeforeClass
+  public final void setUp() throws Exception {
+    TestCaseUtils.startServer();
+  }
+
+
+  /**
+   * Backup tasks test data provider.
+   *
+   * @return The array of tasks test data.  The first column is a task entry
+   *  and the second column is the expected completed task state.
+   */
+  @DataProvider(name = "backups")
+  public Object[][] createData() throws Exception
+  {
+    return new Object[][] {
+         {
+              // A valid backup task.
+              TestCaseUtils.makeEntry(
+                   "dn: ds-task-id=" + UUID.randomUUID() +
+                        ",cn=Scheduled Tasks,cn=Tasks",
+                   "objectclass: top",
+                   "objectclass: ds-task",
+                   "objectclass: ds-task-backup",
+                   "ds-task-class-name: org.opends.server.tasks.BackupTask",
+                   "ds-backup-directory-path: bak",
+                   "ds-task-backup-all: TRUE"
+              ),
+              TaskState.COMPLETED_SUCCESSFULLY
+         },
+         {
+              // Incompatible settings of backup-directory-path and
+              // incremental-base-id.
+              TestCaseUtils.makeEntry(
+                   "dn: ds-task-id=" + UUID.randomUUID() +
+                        ",cn=Scheduled Tasks,cn=Tasks",
+                   "objectclass: top",
+                   "objectclass: ds-task",
+                   "objectclass: ds-task-backup",
+                   "ds-task-class-name: org.opends.server.tasks.BackupTask",
+                   "ds-backup-directory-path: bak",
+                   "ds-task-backup-incremental: TRUE",
+                   "ds-task-backup-incremental-base-id: monday",
+                   "ds-task-backup-all: TRUE"),
+              TaskState.COMPLETED_WITH_ERRORS
+         },
+         {
+              // Incompatible settings for backend-id and backup-all.
+              TestCaseUtils.makeEntry(
+                   "dn: ds-task-id=" + UUID.randomUUID() +
+                        ",cn=Scheduled Tasks,cn=Tasks",
+                   "objectclass: top",
+                   "objectclass: ds-task",
+                   "objectclass: ds-task-backup",
+                   "ds-task-class-name: org.opends.server.tasks.BackupTask",
+                   "ds-backup-directory-path: bak",
+                   "ds-task-backup-backend-id: example",
+                   "ds-task-backup-all: TRUE"),
+              TaskState.STOPPED_BY_ERROR
+         },
+         {
+              // Neither of backend-id or backup-all specified.
+              TestCaseUtils.makeEntry(
+                   "dn: ds-task-id=" + UUID.randomUUID() +
+                        ",cn=Scheduled Tasks,cn=Tasks",
+                   "objectclass: top",
+                   "objectclass: ds-task",
+                   "objectclass: ds-task-backup",
+                   "ds-task-class-name: org.opends.server.tasks.BackupTask",
+                   "ds-backup-directory-path: bak"),
+              TaskState.STOPPED_BY_ERROR
+         },
+         {
+              // Incompatible settings for incremental and incremental-base-id.
+              TestCaseUtils.makeEntry(
+                   "dn: ds-task-id=" + UUID.randomUUID() +
+                        ",cn=Scheduled Tasks,cn=Tasks",
+                   "objectclass: top",
+                   "objectclass: ds-task",
+                   "objectclass: ds-task-backup",
+                   "ds-task-class-name: org.opends.server.tasks.BackupTask",
+                   "ds-backup-directory-path: bak",
+                   "ds-task-backup-all: TRUE",
+                   "ds-task-backup-incremental-base-id: monday",
+                   "ds-task-backup-incremental: FALSE"),
+              TaskState.STOPPED_BY_ERROR
+         },
+         {
+              // Incompatible settings for hash and sign-hash.
+              TestCaseUtils.makeEntry(
+                   "dn: ds-task-id=" + UUID.randomUUID() +
+                        ",cn=Scheduled Tasks,cn=Tasks",
+                   "objectclass: top",
+                   "objectclass: ds-task",
+                   "objectclass: ds-task-backup",
+                   "ds-task-class-name: org.opends.server.tasks.BackupTask",
+                   "ds-backup-directory-path: bak",
+                   "ds-task-backup-all: TRUE",
+                   "ds-task-backup-hash: FALSE",
+                   "ds-task-backup-sign-hash: TRUE"
+                   ),
+              TaskState.STOPPED_BY_ERROR
+         },
+         {
+              // Specified backend does not support backup.
+              TestCaseUtils.makeEntry(
+                   "dn: ds-task-id=" + UUID.randomUUID() +
+                        ",cn=Scheduled Tasks,cn=Tasks",
+                   "objectclass: top",
+                   "objectclass: ds-task",
+                   "objectclass: ds-task-backup",
+                   "ds-task-class-name: org.opends.server.tasks.BackupTask",
+                   "ds-backup-directory-path: bak",
+                   "ds-task-backup-backend-id: monitor"),
+              TaskState.STOPPED_BY_ERROR
+         },
+    };
+  }
+
+
+
+  /**
+   * Test that various backup task definitions complete with the expected state.
+   * @param taskEntry The task entry.
+   * @param expectedState The expected completion state of the task.
+   */
+  @Test(dataProvider = "backups")
+  public void testBackups(Entry taskEntry, TaskState expectedState)
+       throws Exception
+  {
+    testTask(taskEntry, expectedState);
+  }
+
+}
