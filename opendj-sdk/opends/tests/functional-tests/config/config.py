16732313715b967d4616d92dd8fd8549fc63a086
gary_williams <gary_williams@localhost>
2006-10-19 08:45:14 +0000

    On line import test suite

--- opendj-sdk/opends/tests/functional-tests/config/config.py
+++ opendj-sdk/opends/tests/functional-tests/config/config.py
@@ -1,24 +1,24 @@
 STAF_LOCAL_HOSTNAME     = 'localhost'
-STAF_REMOTE_HOSTNAME    = 'localhost'
+STAF_REMOTE_HOSTNAME    = 'ringhorn'
 STAF_HOME_DIR           = '/usr/local/staf'
 STAX_HOME_DIR           = '/export/services/stax'
 TMPDIR                  = '/tmp'
 PSWDFILE                = '/tmp/password'
-OPENDSDIR               = '/export/mikek'
+OPENDSDIR               = '/home/gwilliam/projects/OpenDS/trunk/opends'
 OPENDSNAME              = 'OpenDS-0.1'
 ZIPNAME                 = '%s.zip' % OPENDSNAME
-ZIPPATH                 = '/export/mikek/zip'
-TESTS_DIR               = '%s/functional-tests' % OPENDSDIR
-TESTS_ROOT              = '%s/functional-tests' % OPENDSDIR
+ZIPPATH                 = '%s/build/package' % OPENDSDIR
+TESTS_DIR               = '%s/tests/functional-tests' % OPENDSDIR
+TESTS_ROOT              = '%s/tests/functional-tests' % OPENDSDIR
 TESTS_SHARED_DIR        = '%s/shared' % TESTS_DIR
 TESTS_FUNCTIONS_DIR     = '%s/functions' % TESTS_SHARED_DIR
 TESTS_DATA_DIR          = '%s/data' % TESTS_SHARED_DIR
 DIRECTORY_INSTANCE_DN   = 'cn=Directory Manager'
 DIRECTORY_INSTANCE_PSWD = 'password'
-DIRECTORY_INSTANCE_DIR  = '/export/mikek'
-DIRECTORY_INSTANCE_HOST = 'localhost'
+DIRECTORY_INSTANCE_DIR  = '/usr/ds/gwilliam'
+DIRECTORY_INSTANCE_HOST = 'ringhorn'
 DIRECTORY_INSTANCE_PORT = '1389'
 DIRECTORY_INSTANCE_SSL_PORT = '1636'
 DIRECTORY_INSTANCE_SFX  = 'dc=com'
-LDIF_DATA_DIR           = '%s/data' % TESTS_SHARED_DIR
-JAVA_HOME               = '/usr/jdk/jdk1.5.0_06'
+LDIF_DATA_DIR           = '%s/shared/data' % DIRECTORY_INSTANCE_DIR
+JAVA_HOME               = '/usr/jdk/jdk1.5.0_01'
