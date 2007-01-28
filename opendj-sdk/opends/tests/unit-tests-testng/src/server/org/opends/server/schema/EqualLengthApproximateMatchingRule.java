87f4db9c6b6779447693fdb53dca69881e2c528b
neil_a_wilson <neil_a_wilson@localhost>
2007-01-28 23:02:28 +0000

    Update files created or modified since the beginning of the year to include 2007 in their copyright notices.

--- opendj-sdk/opends/tests/unit-tests-testng/src/server/org/opends/server/schema/EqualLengthApproximateMatchingRule.java
+++ opendj-sdk/opends/tests/unit-tests-testng/src/server/org/opends/server/schema/EqualLengthApproximateMatchingRule.java
@@ -22,7 +22,7 @@
  * CDDL HEADER END
  *
  *
- *      Portions Copyright 2006 Sun Microsystems, Inc.
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
  */
 package org.opends.server.schema;
 
