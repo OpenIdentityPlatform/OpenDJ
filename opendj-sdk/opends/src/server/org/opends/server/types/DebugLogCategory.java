6f25c07117bf911e93b62cdb4b6960473c35f6e2
boli <boli@localhost>
2007-03-01 03:35:57 +0000

    Add a file that was left out of the previous commit.
    This removes old debug logging framework method calls that are going to be automatically instrumented by AspectJ. Non instrumented debug method calls are updated to use the new debug framework methods. However, the new debug logging framework is not yet active as the Aspects are not weaved in. After this revision, debug logging will be disabled in the server until the new AOP framework is complete. 
    
    - Removed debugEnter and debugConstructor from all source files.
    - Removed CLASS_NAME static string for debug purposes.
    - Removed old debug logging framework.
    - Added new debug logging frameworking using AOP.
    - Added AspectJ binary and libraries.
    - Change all modified files copyrights to include 2007.

---
+++ opendj-sdk/opends/src/server/org/opends/server/types/DebugLogCategory.java
@@ -0,0 +1,110 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying * information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2006-2007 Sun Microsystems, Inc.
+ */
+package org.opends.server.types;
+
+import static org.opends.server.util.ServerConstants.*;
+import org.opends.server.loggers.LogCategory;
+
+/**
+ * Logging categories for the debug log messages.
+ */
+public class DebugLogCategory extends LogCategory
+{
+  /**
+   * The log category that will be used for general debug messages.
+   */
+  public static final LogCategory MESSAGE = new DebugLogCategory(
+      DEBUG_CATEGORY_MESSAGE);
+
+  /**
+   * The log category that will be used for constructor messages.
+   * Only logger related classes may use this.
+   */
+  public static final LogCategory CONSTRUCTOR = new DebugLogCategory(
+      DEBUG_CATEGORY_CONSTRUCTOR);
+
+
+  /**
+   * The log category that will be used for raw data read messages.
+   */
+  public static final LogCategory DATA = new DebugLogCategory(
+      DEBUG_CATEGORY_DATA);
+
+
+  /**
+   * The log category that will be used for thrown exception messages.
+   * Only logger related classes may use this.
+   */
+  public static final LogCategory THROWN = new DebugLogCategory(
+      DEBUG_CATEGORY_THROWN);
+
+  /**
+   * The log category that will be used for cought exception messages.
+   * Only logger related classes may use this.
+   */
+  public static final LogCategory COUGHT = new DebugLogCategory(
+      DEBUG_CATEGORY_THROWN);
+
+  /**
+   * The log category that will be used for method entry messages.
+   * Only logger related classes may use this.
+   */
+  public static final LogCategory ENTER = new DebugLogCategory(
+      DEBUG_CATEGORY_ENTER);
+
+  /**
+   * The log category that will be used for method exit messages.
+   * Only logger related classes may use this.
+   */
+  public static final LogCategory EXIT = new DebugLogCategory(
+      DEBUG_CATEGORY_EXIT);
+
+  /**
+   * The log category that will be used for protocol
+   * elements messages.
+   */
+  public static final LogCategory PROTOCOL = new DebugLogCategory(
+      DEBUG_CATEGORY_PROTOCOL);
+
+  /**
+   * The log category that will be used for raw data access
+   * from the JE database messages.
+   */
+  public static final LogCategory DATABASE_ACCESS =
+      new DebugLogCategory(DEBUG_CATEGORY_DATABASE_ACCESS);
+
+
+  /**
+   * Constructor for the DebugLogCategory class.
+   *
+   * @param  name  The name of the level.
+   */
+  public DebugLogCategory(String name)
+  {
+    super(name);
+  }
+}
