9b97b90c77daa7c02491be3894b3ccdd51c974cd
lutoff <lutoff@localhost>
2007-04-18 12:08:18 +0000

    Issue 1483 fix (use new admin framework for Access control configuration)

--- opends/src/server/org/opends/server/authorization/dseecompat/AciProvider.java
+++ opends/src/server/org/opends/server/authorization/dseecompat/AciProvider.java
@@ -27,16 +27,18 @@
 
 package org.opends.server.authorization.dseecompat;
 
+import org.opends.server.admin.std.server.DseeCompatAccessControlHandlerCfg;
 import org.opends.server.api.AccessControlHandler;
 import org.opends.server.api.AccessControlProvider;
-import org.opends.server.config.ConfigEntry;
 import org.opends.server.config.ConfigException;
 import org.opends.server.types.InitializationException;
 
 /**
  * This class is the provider class for the dseecompt ACI.
  */
-public class AciProvider  implements AccessControlProvider  {
+public class AciProvider implements
+    AccessControlProvider<DseeCompatAccessControlHandlerCfg>
+{
 
     /*
      * The AciHandler does all the work in this package.
@@ -52,15 +54,21 @@ public class AciProvider  implements AccessControlProvider  {
 
 
     /**
-     * Creates the AciHandler class and calls its initialization method.
-     * @param configEntry The entry containing the configuration Access Control
-     * entry.
-     * @throws ConfigException If the initialization fails.
-     * @throws InitializationException If the initialization fails.
+     * Creates the AciHandler class and calls its initialization
+     * method.
+     *
+     * @param configuration
+     *          The entry containing the configuration Access Control
+     *          entry.
+     * @throws ConfigException
+     *           If the initialization fails.
+     * @throws InitializationException
+     *           If the initialization fails.
      */
-    public void initializeAccessControlHandler(ConfigEntry configEntry)
+    public void initializeAccessControlHandler(
+      DseeCompatAccessControlHandlerCfg configuration)
     throws ConfigException, InitializationException {
-         instance=new AciHandler(configEntry);
+         instance=new AciHandler(configuration);
     }
 
     /**
