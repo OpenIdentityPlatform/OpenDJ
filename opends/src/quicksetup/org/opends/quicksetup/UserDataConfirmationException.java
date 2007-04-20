3135529ee8b112f09ba782e1b484a0b2b2424cd0
jvergara <jvergara@localhost>
2007-04-20 16:25:49 +0000

    Fix broken build.

---
+++ opends/src/quicksetup/org/opends/quicksetup/UserDataConfirmationException.java
@@ -0,0 +1,50 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
+ */
+
+package org.opends.quicksetup;
+
+/**
+ * This exception is used when there is an confirmation to be asked
+ * concerning the data provided by the user.  It will be thrown by the class
+ * that is in charge of validating the user data (the Application class).
+ *
+ */
+public class UserDataConfirmationException extends UserDataException
+{
+  private static final long serialVersionUID = -2662491859817280513L;
+
+  /**
+   * Constructor for UserDataException.
+   * @param step the step in the wizard where the exception occurred.
+   * @param localizedMessage the localized message describing the error.
+   */
+  public UserDataConfirmationException(WizardStep step, String localizedMessage)
+  {
+    super(step, localizedMessage);
+  }
+}
+
