4bd78f7c29f0348bbb04c17246c16b8691294f85
jvergara <jvergara@localhost>
2007-04-25 17:13:06 +0000

    Fix for issue 1273 (exception if JAVA_HOME undefined in scripts).
    The fix consists of modifying the scripts to launch the InstallDS classwith a test option using the JVM that was found.  If the JVM can be successfully launched, the JVM is compatible with 1.5, if not we as sume that is not compatible with JDK 1.5.
    
    The downside of the fix is that it requires an additional start of the JVM each time the command line is launched. That is why the fix only applies to setup, uninstall, upgrade, start and stop command lines.  The modifications in _server-script.* and _client-script.sh just modify the message displayed when a JVM could not be found (informing that JDK 1.5 or later can be used).

--- opendj-sdk/opends/resource/uninstall.bat
+++ opendj-sdk/opends/resource/uninstall.bat
@@ -48,13 +48,22 @@ goto setClassPath
 
 :noSetJavaHome
 echo Error: JAVA_HOME environment variable is not set.
-echo        Please set it to a valid Java 5 installation.
+echo        Please set it to a valid Java 5 (or later) installation.
+goto end
+
+:noValidJavaHome
+echo ERROR:  The detected Java version could not be used.  Please set 
+echo         JAVA_HOME to to a valid Java 5 (or later) installation.
 goto end
 
 :setClassPath
 FOR %%x in ("%DIR_HOME%\lib\*.jar") DO call "%DIR_HOME%\bat\setcp.bat" %%x
 
 set PATH=%SystemRoot%
+
+rem Test that the provided JDK is 1.5 compatible.
+"%JAVA_BIN%" org.opends.server.tools.InstallDS -t > NUL 2>&1
+if not %errorlevel% == 0 goto noValidJavaHome
 
 if "%*" == "" goto callLaunch
 goto callJava
