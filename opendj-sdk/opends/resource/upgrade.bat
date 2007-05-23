f22ba1b0628e415ce428a2569b30e2fddf305533
kenneth_suter <kenneth_suter@localhost>
2007-05-23 01:18:09 +0000

    corrected some problems with Windows upgrade script

--- opendj-sdk/opends/resource/upgrade.bat
+++ opendj-sdk/opends/resource/upgrade.bat
@@ -32,14 +32,12 @@ set INSTANCE_ROOT=%DIR_HOME%
 
 :checkJavaBin
 if "%JAVA_BIN%" == "" goto noJavaBin
-if "%*" == "" goto callWebStartUpgrade
 goto callExtractor
 
 :noJavaBin
 if "%JAVA_HOME%" == "" goto noJavaHome
 if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
 set JAVA_BIN=%JAVA_HOME%\bin\java.exe
-if "%*" == "" goto callWebStartUpgrade
 goto callExtractor
 
 :noJavaHome
@@ -66,18 +64,15 @@ rem Test that the provided JDK is 1.5 compatible.
 "%JAVA_BIN%" org.opends.server.tools.InstallDS -t > NUL 2>&1
 if not %errorlevel% == 0 goto noValidJavaHome
 
-if "%*" == "" goto callWebStartUpgrade
-
-
 :callExtractor
-if EXIST %INSTANCE_ROOT%\tmp\upgrade rd %INSTANCE_ROOT%\tmp\upgrade /s /q
+if EXIST "%INSTANCE_ROOT%\tmp\upgrade" rd "%INSTANCE_ROOT%\tmp\upgrade" /s /q
 set CLASSPATH=""
 FOR %%x in ("%INSTANCE_ROOT%\lib\*.jar") DO call "%INSTANCE_ROOT%\lib\setcp.bat" %%x
 "%JAVA_BIN%" org.opends.quicksetup.upgrader.BuildExtractor %*
 goto prepUpgrader
 
 :prepUpgrader
-if EXIST %INSTANCE_ROOT%\tmp\upgrade goto setClassPathToStageDir
+if EXIST "%INSTANCE_ROOT%\tmp\upgrade" goto setClassPathToStageDir
 goto callUpgrader
 
 :setClassPathToStageDir
@@ -87,7 +82,6 @@ goto callUpgrader
 
 :callUpgrader
 "%JAVA_BIN%" org.opends.quicksetup.upgrader.UpgradeLauncher %*
-if EXIST %INSTANCE_ROOT%\tmp\upgrade rd %INSTANCE_ROOT%\tmp\upgrade /s /q
 goto end
 
 :end
