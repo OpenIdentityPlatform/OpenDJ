7be96f191167c24266433bf54eb98b8790e97798
matthew_swift <matthew_swift@localhost>
2007-06-22 15:59:44 +0000

    Add various classes which facilitate generating tables in text-based applications.
    This includes a generic API for constructing the table (TableBuilder) and three "table printers":
    
    * TextTablePrinter: a pretty human-readable table that supports configurable padding, column sizes, separators, etc
    
    * CSVTablePrinter: a table printer that outputs in CSV format (useful for spreadsheet imports)
    
    * TabSeparatedTablePrinter: a table printer which uses tabs as field delimiters (e.g. useful for unix cut command)

---
+++ opendj-sdk/opends/src/server/org/opends/server/util/table/CSVTablePrinter.java
@@ -0,0 +1,253 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
+ */
+package org.opends.server.util.table;
+
+
+
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+
+
+/**
+ * An interface for creating a CSV formatted table.
+ */
+public final class CSVTablePrinter extends TablePrinter {
+
+  /**
+   * Table serializer implementation.
+   */
+  private static final class Serializer extends TableSerializer {
+
+    // The current column being output.
+    private int column = 0;
+
+    // Indicates whether or not the headings should be output.
+    private final boolean displayHeadings;
+
+    // Counts the number of separators that should be output the next
+    // time a non-empty cell is displayed. The comma separators are
+    // not displayed immediately so that we can avoid displaying
+    // unnecessary trailing separators.
+    private int requiredSeparators = 0;
+
+    // The output destination.
+    private final PrintWriter writer;
+
+
+
+    // Private constructor.
+    private Serializer(PrintWriter writer, boolean displayHeadings) {
+      this.writer = writer;
+      this.displayHeadings = displayHeadings;
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addCell(String s) {
+      // Avoid printing comma separators for trailing empty cells.
+      if (s.length() == 0) {
+        requiredSeparators++;
+      } else {
+        for (int i = 0; i < requiredSeparators; i++) {
+          writer.print(',');
+        }
+        requiredSeparators = 1;
+      }
+
+      boolean needsQuoting = false;
+
+      if (s.contains(",")) {
+        needsQuoting = true;
+      }
+
+      if (s.contains("\n")) {
+        needsQuoting = true;
+      }
+
+      if (s.contains("\r")) {
+        needsQuoting = true;
+      }
+
+      if (s.contains("\"")) {
+        needsQuoting = true;
+        s = s.replace("\"", "\"\"");
+      }
+
+      if (s.startsWith(" ")) {
+        needsQuoting = true;
+      }
+
+      if (s.endsWith(" ")) {
+        needsQuoting = true;
+      }
+
+      StringBuilder builder = new StringBuilder();
+      if (needsQuoting) {
+        builder.append("\"");
+      }
+
+      builder.append(s);
+
+      if (needsQuoting) {
+        builder.append("\"");
+      }
+
+      writer.print(builder.toString());
+      column++;
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addHeading(String s) {
+      if (displayHeadings) {
+        addCell(s);
+      }
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void endHeader() {
+      if (displayHeadings) {
+        writer.println();
+      }
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void endRow() {
+      writer.println();
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void endTable() {
+      writer.flush();
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void startHeader() {
+      column = 0;
+      requiredSeparators = 0;
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void startRow() {
+      column = 0;
+      requiredSeparators = 0;
+    }
+  }
+
+  // Indicates whether or not the headings should be output.
+  private boolean displayHeadings = false;
+
+  // The output destination.
+  private PrintWriter writer = null;
+
+
+
+  /**
+   * Creates a new CSV table printer for the specified output stream.
+   * Headings will not be displayed by default.
+   *
+   * @param stream
+   *          The stream to output tables to.
+   */
+  public CSVTablePrinter(OutputStream stream) {
+    this(new BufferedWriter(new OutputStreamWriter(stream)));
+  }
+
+
+
+  /**
+   * Creates a new CSV table printer for the specified writer.
+   * Headings will not be displayed by default.
+   *
+   * @param writer
+   *          The writer to output tables to.
+   */
+  public CSVTablePrinter(Writer writer) {
+    this.writer = new PrintWriter(writer);
+  }
+
+
+
+  /**
+   * Specify whether or not table headings should be displayed.
+   *
+   * @param displayHeadings
+   *          <code>true</code> if table headings should be
+   *          displayed.
+   */
+  public void setDisplayHeadings(boolean displayHeadings) {
+    this.displayHeadings = displayHeadings;
+  }
+
+
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected TableSerializer getSerializer() {
+    return new Serializer(writer, displayHeadings);
+  }
+
+}
---
+++ opendj-sdk/opends/src/server/org/opends/server/util/table/TableBuilder.java
@@ -0,0 +1,381 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
+ */
+package org.opends.server.util.table;
+
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+
+/**
+ * A class which can be used to construct tables of information to be
+ * displayed in a terminal. Once built the table can be output using a
+ * {@link TableSerializer}.
+ */
+public final class TableBuilder {
+
+  // The current column number in the current row where 0 represents
+  // the left-most column in the table.
+  private int column = 0;
+
+  // The current with of each column.
+  private List<Integer> columnWidths = new ArrayList<Integer>();
+
+  // The list of column headings.
+  private List<String> header = new ArrayList<String>();
+
+  // The current number of rows in the table.
+  private int height = 0;
+
+  // The list of table rows.
+  private List<List<String>> rows = new ArrayList<List<String>>();
+
+  // The linked list of sort keys comparators.
+  private List<Comparator<String>> sortComparators =
+    new ArrayList<Comparator<String>>();
+
+  // The linked list of sort keys.
+  private List<Integer> sortKeys = new ArrayList<Integer>();
+
+  // The current number of columns in the table.
+  private int width = 0;
+
+
+
+  /**
+   * Creates a new table printer.
+   */
+  public TableBuilder() {
+    // No implementation required.
+  }
+
+
+
+  /**
+   * Adds a table sort key. The table will be sorted according to the
+   * case-insensitive string ordering of the cells in the specified
+   * column.
+   *
+   * @param column
+   *          The column which will be used as a sort key.
+   */
+  public void addSortKey(int column) {
+    addSortKey(column, String.CASE_INSENSITIVE_ORDER);
+  }
+
+
+
+  /**
+   * Adds a table sort key. The table will be sorted according to the
+   * provided string comparator.
+   *
+   * @param column
+   *          The column which will be used as a sort key.
+   * @param comparator
+   *          The string comparator.
+   */
+  public void addSortKey(int column, Comparator<String> comparator) {
+    sortKeys.add(column);
+    sortComparators.add(comparator);
+  }
+
+
+
+  /**
+   * Appends a new blank cell to the current row.
+   */
+  public void appendCell() {
+    appendCell("");
+  }
+
+
+
+  /**
+   * Appends a new cell to the current row containing the provided
+   * boolean value.
+   *
+   * @param value
+   *          The boolean value.
+   */
+  public void appendCell(boolean value) {
+    appendCell(String.valueOf(value));
+  }
+
+
+
+  /**
+   * Appends a new cell to the current row containing the provided
+   * byte value.
+   *
+   * @param value
+   *          The byte value.
+   */
+  public void appendCell(byte value) {
+    appendCell(String.valueOf(value));
+  }
+
+
+
+  /**
+   * Appends a new cell to the current row containing the provided
+   * char value.
+   *
+   * @param value
+   *          The char value.
+   */
+  public void appendCell(char value) {
+    appendCell(String.valueOf(value));
+  }
+
+
+
+  /**
+   * Appends a new cell to the current row containing the provided
+   * double value.
+   *
+   * @param value
+   *          The double value.
+   */
+  public void appendCell(double value) {
+    appendCell(String.valueOf(value));
+  }
+
+
+
+  /**
+   * Appends a new cell to the current row containing the provided
+   * float value.
+   *
+   * @param value
+   *          The float value.
+   */
+  public void appendCell(float value) {
+    appendCell(String.valueOf(value));
+  }
+
+
+
+  /**
+   * Appends a new cell to the current row containing the provided
+   * integer value.
+   *
+   * @param value
+   *          The boolean value.
+   */
+  public void appendCell(int value) {
+    appendCell(String.valueOf(value));
+  }
+
+
+
+  /**
+   * Appends a new cell to the current row containing the provided
+   * long value.
+   *
+   * @param value
+   *          The long value.
+   */
+  public void appendCell(long value) {
+    appendCell(String.valueOf(value));
+  }
+
+
+
+  /**
+   * Appends a new cell to the current row containing the provided
+   * object value.
+   *
+   * @param value
+   *          The object value.
+   */
+  public void appendCell(Object value) {
+    // Make sure that the first row has been created.
+    if (height == 0) {
+      startRow();
+    }
+
+    // Create the cell.
+    String s = String.valueOf(value);
+    rows.get(height - 1).add(String.valueOf(value));
+    column++;
+
+    // Update statistics.
+    if (column > width) {
+      width = column;
+      columnWidths.add(s.length());
+    } else if (columnWidths.get(column - 1) < s.length()) {
+      columnWidths.set(column - 1, s.length());
+    }
+  }
+
+
+
+  /**
+   * Appends a new blank column heading to the header row.
+   */
+  public void appendHeading() {
+    appendHeading("");
+  }
+
+
+
+  /**
+   * Appends a new column heading to the header row.
+   *
+   * @param value
+   *          The column heading value.
+   */
+  public void appendHeading(String value) {
+    header.add(value);
+
+    // Update statistics.
+    if (header.size() > width) {
+      width = header.size();
+      columnWidths.add(value.length());
+    } else if (columnWidths.get(header.size() - 1) < value.length()) {
+      columnWidths.set(header.size() - 1, value.length());
+    }
+  }
+
+
+
+  /**
+   * Gets the width of the current row.
+   *
+   * @return Returns the width of the current row.
+   */
+  public int getRowWidth() {
+    return column;
+  }
+
+
+
+  /**
+   * Gets the number of rows in table.
+   *
+   * @return Returns the number of rows in table.
+   */
+  public int getTableHeight() {
+    return height;
+  }
+
+
+
+  /**
+   * Gets the number of columns in table.
+   *
+   * @return Returns the number of columns in table.
+   */
+  public int getTableWidth() {
+    return width;
+  }
+
+
+
+  /**
+   * Prints the table in its current state using the provided table
+   * printer.
+   *
+   * @param printer
+   *          The table printer.
+   */
+  public void print(TablePrinter printer) {
+    // Create a new printer instance.
+    TableSerializer serializer = printer.getSerializer();
+
+    // First sort the table.
+    List<List<String>> sortedRows = new ArrayList<List<String>>(rows);
+
+    Comparator<List<String>> comparator = new Comparator<List<String>>() {
+
+      public int compare(List<String> row1, List<String> row2) {
+        for (int i = 0; i < sortKeys.size(); i++) {
+          String cell1 = row1.get(sortKeys.get(i));
+          String cell2 = row2.get(sortKeys.get(i));
+
+          int rc = sortComparators.get(i).compare(cell1, cell2);
+          if (rc != 0) {
+            return rc;
+          }
+        }
+
+        // Both rows are equal.
+        return 0;
+      }
+
+    };
+
+    Collections.sort(sortedRows, comparator);
+
+    // Now ouput the table.
+    serializer.startTable(height, width);
+    for (int i = 0; i < width; i++) {
+      serializer.addColumn(columnWidths.get(i));
+    }
+
+    // Column headings.
+    serializer.startHeader();
+    for (String s : header) {
+      serializer.addHeading(s);
+    }
+    serializer.endHeader();
+
+    // Table contents.
+    serializer.startContent();
+    for (List<String> row : sortedRows) {
+      serializer.startRow();
+
+      // Print each cell in the row, padding missing trailing cells.
+      for (int i = 0; i < width; i++) {
+        if (i < row.size()) {
+          serializer.addCell(row.get(i));
+        } else {
+          serializer.addCell("");
+        }
+      }
+
+      serializer.endRow();
+    }
+    serializer.endContent();
+    serializer.endTable();
+  }
+
+
+
+  /**
+   * Appends a new row to the table.
+   */
+  public void startRow() {
+    rows.add(new ArrayList<String>());
+    height++;
+    column = 0;
+  }
+}
