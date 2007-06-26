a2e8cebb1f861f0e6b9a5a284779f7814f818ac5
al_xipe <al_xipe@localhost>
2007-06-26 06:58:28 +0000

    updated xml files to point to shared/stax.dtd

--- opendj-sdk/opends/tests/functional-tests/config/config.py
+++ opendj-sdk/opends/tests/functional-tests/config/config.py
@@ -1,30 +1,30 @@
-TEST_OS_STRING              = 'OS_STRING'
-TEST_HW_STRING              = 'HW_STRING'
-TEST_JVM_STRING             = 'JVM_STRING'
+TEST_OS_STRING              = 'Linux'
+TEST_HW_STRING              = 'i386'
+TEST_JVM_STRING             = '1.6.0_01(32 bits)'
 STAF_LOCAL_HOSTNAME         = 'localhost'
 STAF_REMOTE_HOSTNAME        = 'localhost'
-TMPDIR                      = '/tmp'
-PSWDFILE                    = '/tmp/password'
-OPENDSDIR                   = '/home/username/trunk/opends'
+TMPDIR                      = '/path/to/opends/tests/functional-run/tmp'
+PSWDFILE                    = '%s/password' % TMPDIR
+OPENDSDIR                   = '/path/to/opends'
 OPENDSNAME                  = 'OpenDS-0.9.0'
-ZIPNAME                     = '%s.zip' % OPENDSNAME
-ZIPPATH                     = '%s/build/package' % OPENDSDIR
-TESTS_DIR                   = '%s/tests/functional-tests' % OPENDSDIR
+ZIPNAME                     = 'OpenDS-0.9.0.zip'
+ZIPPATH                     = '/path/to/opends/build/package'
 TESTS_ROOT                  = '%s/tests' % OPENDSDIR
+TESTS_DIR                   = '%s/functional-tests' % TESTS_ROOT
 TESTS_SHARED_DIR            = '%s/shared' % TESTS_DIR
 TESTS_FUNCTIONS_DIR         = '%s/functions' % TESTS_SHARED_DIR
 TESTS_DATA_DIR              = '%s/data' % TESTS_SHARED_DIR
 TESTS_JAVA_DIR		    = '%s/java' % TESTS_SHARED_DIR
-DIRECTORY_INSTANCE_DN       = 'cn=Directory Manager'
+DIRECTORY_INSTANCE_DN       = 'cn=myself'
 DIRECTORY_INSTANCE_PSWD     = 'password'
-DIRECTORY_INSTANCE_DIR      = '/tmp'
+DIRECTORY_INSTANCE_DIR      = '%s' % TMPDIR
 DIRECTORY_INSTANCE_HOST     = 'localhost'
 DIRECTORY_INSTANCE_PORT     = '1389'
 DIRECTORY_INSTANCE_SSL_PORT = '1636'
 DIRECTORY_INSTANCE_SFX      = 'dc=com'
 DIRECTORY_INSTANCE_BE       = 'userRoot'
-JAVA_HOME                   = '/usr/jdk/latest'
+JAVA_HOME                   = '/path/to/java/6/jdk1.6.0_01/jre'
 LOGS_ROOT                   = '%s' % TMPDIR
 LOGS_URI                    = ''
-SEND_MAIL_AFTER_TEST_RUN    = 'FALSE'
-SEND_MAIL_TO                = ' '
+SEND_MAIL_AFTER_TEST_RUN    = 'false'
+SEND_MAIL_TO                = ''
