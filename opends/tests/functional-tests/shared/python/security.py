613c63434b8f8f1ef95e8b016e7f0a60e0fdbbcd
smaguin <smaguin@localhost>
2007-08-03 09:39:59 +0000

    don't use ldaps_port.ldif to confdigure LDAPS connection handler

--- opends/tests/functional-tests/shared/python/security.py
+++ opends/tests/functional-tests/shared/python/security.py
@@ -34,23 +34,3 @@ def retrieve_aci(filename):
         if aci_index > -1:
             ret_str = ret_str + line
     return ret_str
-
-def write_ldaps_ldif_file(path, port):
-    ldif_file = open(path + "/ldaps_port.ldif","w")
-
-    ldif_file.write("dn: cn=LDAPS Connection Handler,cn=Connection Handlers,cn=config\n")
-    ldif_file.write("changetype: modify\n")
-    ldif_file.write("replace: ds-cfg-listen-port\n")
-    ldif_file.write("ds-cfg-listen-port: ")
-    ldif_file.write(port)
-    ldif_file.write("\n")
-    ldif_file.write("-\n")
-    ldif_file.write("replace: ds-cfg-connection-handler-enabled\n")
-    ldif_file.write("ds-cfg-connection-handler-enabled: true\n")
-    ldif_file.write("-\n")
-    ldif_file.write("replace: ds-cfg-ssl-cert-nickname\n")
-    ldif_file.write("ds-cfg-ssl-cert-nickname: server-cert\n")
-     
-    ldif_file.close()
-
-
