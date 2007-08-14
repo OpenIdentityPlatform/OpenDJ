c991a7c7d91551411804ad719134e869bc108d47
neil_a_wilson <neil_a_wilson@localhost>
2007-08-14 00:48:04 +0000

    Add some missing package-info.java files, and add a unit test to ensure that these files are properly created from now on.

---
+++ opends/src/server/org/opends/server/admin/client/cli/package-info.java
@@ -0,0 +1,37 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
+ */
+
+
+
+/**
+ * Command-line interface code for common client-side administration classes.
+ * <p>
+ * This package contains classes which client applications and client-side
+ * driver implementations are expected to use for their command-line interfaces.
+ */
+package org.opends.server.admin.client.cli;
+
