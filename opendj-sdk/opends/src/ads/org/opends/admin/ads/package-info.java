cae9e9d00630de0b91075af355555aaceed848d4
jvergara <jvergara@localhost>
2007-08-17 10:12:18 +0000

    Fix for issues 2099 and 2101 (add package-info.java file for the packages of the ADS and of guitools).

---
+++ opendj-sdk/opends/src/ads/org/opends/admin/ads/package-info.java
@@ -0,0 +1,41 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
+ */
+
+
+
+/**
+ * Defines the different classes that are you used to have a read-only
+ * view of the configuration of the servers that are registered in the
+ * Administration Directory Server (ADS).
+ *
+ * These includes objects that are used to represent the server and its
+ * configuration (<code>org.opends.admin.ads.ServerDescriptor</code>,
+ * <code>org.opends.admin.ads.ReplicaDescriptor</code>, etc.) and classes
+ * used to retrieve the information (like
+ * <code>org.opends.admin.ads.ADSContext</code>).
+ */
+package org.opends.admin.ads;
