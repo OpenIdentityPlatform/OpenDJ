4700a31d399d3e3fe5594cce350744ea71ede5ee
neil_a_wilson <neil_a_wilson@localhost>
2007-08-24 23:00:23 +0000

    Add a mechanism for generating an RC script that can be used on UNIX systems to configure the server to automatically start when the system boots.
    Also, update the stop-ds script so that if the server is to be stopped via kill
    but no PID file is present, the stop script will generate an error instead of
    trying to invoke the stop task (which is guaranteed to fail since the attempt
    will be made anonymously, and this will give a confusing error message).
    
    OpenDS Issue Number:  526

--- opends/src/server/org/opends/server/util/ServerConstants.java
+++ opends/src/server/org/opends/server/util/ServerConstants.java
@@ -2660,5 +2660,36 @@ public final class ServerConstants
    */
   public static final String ALERT_TYPE_REPLICATION_UNRESOLVED_CONFLICT =
           "org.opends.server.replication.UnresolvedConflict";
+
+
+
+  /**
+   * The lines that make up the CDDL header.  They will not have any prefix, so
+   * an appropriate prefix may need to be added for some cases (e.g., "# " for
+   * shell scripts, "rem " for batch files, etc.).
+   */
+  public static final String[] CDDL_HEADER_LINES =
+  {
+    "CDDL HEADER START",
+    "",
+    "The contents of this file are subject to the terms of the",
+    "Common Development and Distribution License, Version 1.0 only",
+    "(the \"License\").  You may not use this file except in compliance",
+    "with the License.",
+    "",
+    "You can obtain a copy of the license at",
+    "https://OpenDS.dev.java.net/OpenDS.LICENSE.",
+    "See the License for the specific language governing permissions",
+    "and limitations under the License.",
+    "",
+    "When distributing Covered Code, include this CDDL HEADER in each",
+    "file and include the License file at",
+    "trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,",
+    "add the following below this CDDL HEADER, with the fields enclosed",
+    "by brackets \"[]\" replaced with your own identifying information:",
+    "     Portions Copyright [yyyy] [name of copyright owner]",
+    "",
+    "CDDL HEADER END"
+  };
 }
 
