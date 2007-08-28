b0eb0b4e1c0db69396920120daa33db757f3abcf
neil_a_wilson <neil_a_wilson@localhost>
2007-08-28 02:00:49 +0000

    Add an SMTP account status notification handler, which can be configured to send e-mail messages whenever an account status notification is generated.  The message can be sent to the end user impacted by the notification (based on an attribute in the user's entry) and/or a fixed set of recipients.
    The messages that will be generated are created from template files, which can
    include tokens that will be replced with things like:
    
    - The name of the notification type
    - The notification message
    - The DN of the target user's entry
    - Attribute values from the target user's entry
    - Values of account status notification properties, which may vary based on the
      type of notification
    
    This change also includes a fix for a problem that could allow password
    expiration warning messages to be sent to a client even if the bind was not
    successful.
    
    
    OpenDS Issue Number:  581

---
+++ opendj-sdk/opends/src/server/org/opends/server/extensions/NotificationMessageTemplateElement.java
@@ -0,0 +1,54 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
+ */
+package org.opends.server.extensions;
+
+
+
+import org.opends.messages.MessageBuilder;
+import org.opends.server.types.AccountStatusNotification;
+
+
+
+/**
+ * This class defines the base class for elements that may be used to generate
+ * an account status notification message.
+ */
+public abstract class NotificationMessageTemplateElement
+{
+  /**
+   * Generates a value for this template element using the information contained
+   * in the provided account status notification and appends it to the given
+   * buffer.
+   *
+   * @param  buffer        The buffer to which the generated value should be
+   *                       appended.
+   * @param  notification  The account status notification to process.
+   */
+  public abstract void generateValue(MessageBuilder buffer,
+                                     AccountStatusNotification notification);
+}
+
---
+++ opendj-sdk/opends/src/server/org/opends/server/extensions/UserAttributeNotificationMessageTemplateElement.java
@@ -0,0 +1,90 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
+ */
+package org.opends.server.extensions;
+
+
+
+import java.util.List;
+
+import org.opends.messages.MessageBuilder;
+import org.opends.server.types.AccountStatusNotification;
+import org.opends.server.types.Attribute;
+import org.opends.server.types.AttributeType;
+import org.opends.server.types.AttributeValue;
+import org.opends.server.types.Entry;
+
+
+
+/**
+ * This class implements a notification message template element that will
+ * generate a value that is the value of a specified attribute from the target
+ * user's entry.
+ */
+public class UserAttributeNotificationMessageTemplateElement
+       extends NotificationMessageTemplateElement
+{
+  // The attribute type for which to obtain the value.
+  private AttributeType attributeType;
+
+
+
+  /**
+   * Creates a new user DN notification message template element.
+   *
+   * @param  attributeType  The attribute type for which to obtain the value.
+   */
+  public UserAttributeNotificationMessageTemplateElement(AttributeType
+                                                              attributeType)
+  {
+    this.attributeType = attributeType;
+  }
+
+
+
+  /**
+   * {@inheritDoc}
+   */
+  public void generateValue(MessageBuilder buffer,
+                            AccountStatusNotification notification)
+  {
+    Entry userEntry = notification.getUserEntry();
+
+    List<Attribute> attrList = userEntry.getAttribute(attributeType);
+    if (attrList != null)
+    {
+      for (Attribute a : attrList)
+      {
+        for (AttributeValue v : a.getValues())
+        {
+          buffer.append(v.getStringValue());
+          return;
+        }
+      }
+    }
+  }
+}
+
