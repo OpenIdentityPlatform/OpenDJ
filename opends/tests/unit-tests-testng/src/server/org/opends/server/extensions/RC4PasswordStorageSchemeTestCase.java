66e851eb4f47f909a235549ead09ef159fd20f88
neil_a_wilson <neil_a_wilson@localhost>
2007-09-10 00:56:52 +0000

    Add support for password storage schemes using AES, 3DES, RC4, and Blowfish. The AES, RC4, and Blowfish implementations all use 128-bit ciphers, and the 3DES implementation uses a 168-bit cipher.
    Note that while these password storage schemes are functional, they rely on the
    crypto manager, which is not fully implemented.  The storage schemes are not
    exposed in the server configuration because the crypto manager does not have
    any mechanism to persist secret keys for symmetric encryption.  Until the
    crypto manager provides persistence for these keys, passwords encoded using
    these schemes will not be usable after the server is restarted.  Once the
    crypto manager implementation is complete, these schemes should be exposed in
    the server configuration.
    
    OpenDS Issue Numbers:  315, 316, 317, 318

---
+++ opends/tests/unit-tests-testng/src/server/org/opends/server/extensions/RC4PasswordStorageSchemeTestCase.java
@@ -0,0 +1,75 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Portions Copyright 2007 Sun Microsystems, Inc.
+ */
+package org.opends.server.extensions;
+
+
+
+import org.opends.server.admin.server.AdminTestCaseUtils;
+import org.opends.server.admin.std.meta.RC4PasswordStorageSchemeCfgDefn;
+import org.opends.server.admin.std.server.RC4PasswordStorageSchemeCfg;
+import org.opends.server.api.PasswordStorageScheme;
+
+
+
+/**
+ * A set of test cases for the RC4 password storage scheme.
+ */
+public class RC4PasswordStorageSchemeTestCase
+       extends PasswordStorageSchemeTestCase
+{
+  /**
+   * Creates a new instance of this storage scheme test case.
+   */
+  public RC4PasswordStorageSchemeTestCase()
+  {
+    super("cn=RC4,cn=Password Storage Schemes,cn=config");
+  }
+
+
+
+  /**
+   * Retrieves an initialized instance of this password storage scheme.
+   *
+   * @return  An initialized instance of this password storage scheme.
+   *
+   * @throws  Exception  If an unexpected problem occurs.
+   */
+  protected PasswordStorageScheme getScheme()
+         throws Exception
+  {
+    RC4PasswordStorageScheme scheme = new RC4PasswordStorageScheme();
+
+    RC4PasswordStorageSchemeCfg configuration =
+      AdminTestCaseUtils.getConfiguration(
+          RC4PasswordStorageSchemeCfgDefn.getInstance(),
+          configEntry.getEntry());
+
+    scheme.initializePasswordStorageScheme(configuration);
+    return scheme;
+  }
+}
+
