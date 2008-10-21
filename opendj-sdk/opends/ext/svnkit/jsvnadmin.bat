68413f6d3dd828661f21d19cb5cae9410e82144d
ludovicp <ludovicp@localhost>
2008-10-21 15:30:14 +0000

    Fix an issue where OpenDS revision number is set to -1 when using svn 1.5. The solution involves upgrading svnkit to version 1.2.0 and updating code calling svnkit.

---
+++ opendj-sdk/opends/ext/svnkit/jsvnadmin.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set DEFAULT_SVNKIT_HOME=%~dp0
+
+if "%SVNKIT_HOME%"=="" set SVNKIT_HOME=%DEFAULT_SVNKIT_HOME%
+
+set SVNKIT_CLASSPATH= "%SVNKIT_HOME%svnkit.jar";"%SVNKIT_HOME%svnkit-cli.jar";"%SVNKIT_HOME%trilead.jar";"%SVNKIT_HOME%jna.jar"
+set SVNKIT_MAINCLASS=org.tmatesoft.svn.cli.svnadmin.SVNAdmin
+set SVNKIT_OPTIONS=-Djava.util.logging.config.file="%SVNKIT_HOME%/logging.properties"
+
+"%JAVA_HOME%\bin\java" %SVNKIT_OPTIONS% -cp %SVNKIT_CLASSPATH% %SVNKIT_MAINCLASS% %*
