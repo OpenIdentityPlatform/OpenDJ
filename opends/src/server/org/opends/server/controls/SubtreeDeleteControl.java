d7baad6867108e212886bb6c9bda318ec308fcc4
gbellato <gbellato@localhost>
2009-01-15 07:40:36 +0000

    Fix for 3683 (cont) Add SubtreeDeleteControl forgotten with previous commit

---
+++ opends/src/server/org/opends/server/controls/SubtreeDeleteControl.java
@@ -0,0 +1,46 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Copyright 2009 Sun Microsystems, Inc.
+ */
+package org.opends.server.controls;
+
+import org.opends.server.types.Control;
+import static org.opends.server.util.ServerConstants.*;
+
+/**
+ * This class implements the subtree delete control.
+ */
+public class SubtreeDeleteControl extends Control
+{
+  /**
+   * Creates a new instance of Subtree Delete control with the
+   * default settings.
+   */
+  public SubtreeDeleteControl()
+  {
+    super(OID_SUBTREE_DELETE_CONTROL, false);
+  }
+
+}
