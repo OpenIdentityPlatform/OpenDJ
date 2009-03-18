4be19b98e3601eb6efa8a44af986eabeb5e950cc
jvergara <jvergara@localhost>
2009-03-18 13:48:07 +0000

    Use 'Java' instead of 'java' in the messages.

--- opends/resource/bin/_script-util.bat
+++ opends/resource/bin/_script-util.bat
@@ -112,16 +112,16 @@ set OPENDS_JAVA_BIN=%JAVA_HOME%\bin\java.exe
 goto endJavaHomeAndArgs
 
 :noJavaFound
-echo ERROR:  Could not find a valid java binary to be used.
+echo ERROR:  Could not find a valid Java binary to be used.
 echo You must specify the path to a valid Java 5.0 or higher version.
 echo The procedure to follow is:
 echo 1. Delete the file %INSTANCE_ROOT%\lib\set-java-home.bat if it exists.
 echo 2. Set the environment variable OPENDS_JAVA_HOME to the root of a valid
 echo Java 5.0 installation.
-echo If you want to have specific java  settings for each command line you must
+echo If you want to have specific Java settings for each command line you must
 echo follow the steps 3 and 4.
-echo 3. Edit the properties file specifying the java binary and the java arguments
-echo for each command line.  The java properties file is located in:
+echo 3. Edit the properties file specifying the Java binary and the Java arguments
+echo for each command line.  The Java properties file is located in:
 echo %INSTANCE_ROOT%\config\java.properties.
 echo 4. Run the command-line %INSTALL_ROOT%\bat\dsjavaproperties.bat
 pause
@@ -144,17 +144,17 @@ goto end
 :noValidJavaHome
 if NOT "%OPENDS_JAVA_ARGS%" == "" goto noValidHomeWithArgs
 echo ERROR:  The detected Java version could not be used.  The detected
-echo java binary is:
+echo Java binary is:
 echo %OPENDS_JAVA_BIN%
 echo You must specify the path to a valid Java 5.0 or higher version.
 echo The procedure to follow is:
 echo 1. Delete the file %INSTANCE_ROOT%\lib\set-java-home.bat if it exists.
 echo 2. Set the environment variable OPENDS_JAVA_HOME to the root of a valid
 echo Java 5.0 installation.
-echo If you want to have specific java  settings for each command line you must
+echo If you want to have specific Java settings for each command line you must
 echo follow the steps 3 and 4.
-echo 3. Edit the properties file specifying the java binary and the java arguments
-echo for each command line.  The java properties file is located in:
+echo 3. Edit the properties file specifying the Java binary and the Java arguments
+echo for each command line.  The Java properties file is located in:
 echo %INSTANCE_ROOT%\config\java.properties.
 echo 4. Run the command-line %INSTALL_ROOT%\bat\dsjavaproperties.bat
 pause
@@ -168,19 +168,19 @@ pause
 exit /B 1
 
 :noValidHomeWithArgs
-echo ERROR:  The detected Java version could not be used with the set of java
+echo ERROR:  The detected Java version could not be used with the set of Java
 echo arguments %OPENDS_JAVA_ARGS%.
-echo The detected java binary is:
+echo The detected Java binary is:
 echo %OPENDS_JAVA_BIN%
 echo You must specify the path to a valid Java 5.0 or higher version.
 echo The procedure to follow is:
 echo 1. Delete the file %INSTANCE_ROOT%\lib\set-java-home.bat if it exists.
 echo 2. Set the environment variable OPENDS_JAVA_HOME to the root of a valid
 echo Java 5.0 installation.
-echo If you want to have specific java  settings for each command line you must
+echo If you want to have specific Java settings for each command line you must
 echo follow the steps 3 and 4.
-echo 3. Edit the properties file specifying the java binary and the java arguments
-echo for each command line.  The java properties file is located in:
+echo 3. Edit the properties file specifying the Java binary and the Java arguments
+echo for each command line.  The Java properties file is located in:
 echo %INSTANCE_ROOT%\config\java.properties.
 echo 4. Run the command-line %INSTALL_ROOT%\bat\dsjavaproperties.bat
 pause
