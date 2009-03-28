1dcdc6a34fa1ba543a425ab96987ed4db7ea344a
jvergara <jvergara@localhost>
2009-03-28 11:09:16 +0000

    Fix for issue 2758 (incorrect messages in debug file)

--- opendj-sdk/opends/src/server/org/opends/server/protocols/LDIFConnectionHandler.java
+++ opendj-sdk/opends/src/server/org/opends/server/protocols/LDIFConnectionHandler.java
@@ -22,7 +22,7 @@
  * CDDL HEADER END
  *
  *
- *      Copyright 2008 Sun Microsystems, Inc.
+ *      Copyright 2008-2009 Sun Microsystems, Inc.
  */
 package org.opends.server.protocols;
 
@@ -296,7 +296,7 @@ public final class LDIFConnectionHandler
             {
               TRACER.debugInfo("LDIF connection handler directory " +
                                dir.getAbsolutePath() +
-                               "doesn't exist or isn't a file");
+                               " doesn't exist or isn't a directory");
               alreadyWarn = true;
             }
           }
