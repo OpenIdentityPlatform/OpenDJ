439544d24d842c19a0792f2612b5b088d992fc65
pgamba <pgamba@localhost>
2009-08-25 08:39:19 +0000

    Fix 4184 ECL: lastExternalChangelogCookie contains info about private naming contexts

--- opends/src/server/org/opends/server/replication/server/ExternalChangeLogSessionImpl.java
+++ opends/src/server/org/opends/server/replication/server/ExternalChangeLogSessionImpl.java
@@ -26,14 +26,11 @@
  */
 package org.opends.server.replication.server;
 
-import java.util.Iterator;
 
 import org.opends.server.replication.common.ExternalChangeLogSession;
-import org.opends.server.replication.common.MultiDomainServerState;
 import org.opends.server.replication.protocol.ECLUpdateMsg;
 import org.opends.server.replication.protocol.StartECLSessionMsg;
 import org.opends.server.types.DirectoryException;
-import org.opends.server.util.ServerConstants;
 
 /**
  * This interface defines a session used to search the external changelog
@@ -96,30 +93,5 @@ public class ExternalChangeLogSessionImpl
   public void close()
   {
     handler.getDomain().stopServer(handler);
-  }
-
-  /**
-   * Returns the last (newest) cookie value.
-   * @return the last cookie value.
-   */
-  public MultiDomainServerState getLastCookie()
-  {
-    MultiDomainServerState result = new MultiDomainServerState();
-    // Initialize start state for  all running domains with empty state
-    Iterator<ReplicationServerDomain> rsdk = rs.getDomainIterator();
-    if (rsdk != null)
-    {
-      while (rsdk.hasNext())
-      {
-        // process a domain
-        ReplicationServerDomain rsd = rsdk.next();
-        if (rsd.getBaseDn().compareToIgnoreCase(
-            ServerConstants.DN_EXTERNAL_CHANGELOG_ROOT)==0)
-          continue;
-        result.update(rsd.getBaseDn(), rsd.getEligibleState(
-            rs.getEligibleCN()));
-      }
-    }
-    return result;
   }
 }
