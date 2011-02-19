4d6c3e1ccdb89c6e48a63962b0bc5f2c289dcfa6
Ludovic Poitou <ludovic.poitou@forgerock.com>
2011-02-19 13:13:38 +0000

    Fix OPENDJ-59 : search with paged result control issue

--- opendj-sdk/opends/src/server/org/opends/server/backends/jeb/EntryContainer.java
+++ opendj-sdk/opends/src/server/org/opends/server/backends/jeb/EntryContainer.java
@@ -23,6 +23,7 @@
  *
  *
  *      Copyright 2006-2010 Sun Microsystems, Inc.
+ *      Portions Copyright 2011 ForgeRock AS
  */
 package org.opends.server.backends.jeb;
 import org.opends.messages.Message;
@@ -1304,8 +1305,7 @@ implements ConfigurationChangeListener<LocalDBBackendCfg>
       // The cookie contains the DN of the next entry to be returned.
       try
       {
-        DN lastDN = DN.decode(pageRequest.getCookie());
-        begin = StaticUtils.getBytes(lastDN.toNormalizedString());
+        begin = pageRequest.getCookie().toByteArray();
       }
       catch (Exception e)
       {
