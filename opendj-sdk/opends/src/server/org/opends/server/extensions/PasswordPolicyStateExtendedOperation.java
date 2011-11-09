ed92d98837428e9e50ee030a2badcd770f9c2d87
Ludovic Poitou <ludovic.poitou@forgerock.com>
2011-11-09 23:14:45 +0000

    Fix for OPENDJ-349: manage-account returns Seconds Until Idle Account Lockout: 0 (zero) if the last log on date is more than 24 days before the idle lock out interval. There's a bad casting between int and long.

--- opendj-sdk/opends/src/server/org/opends/server/extensions/PasswordPolicyStateExtendedOperation.java
+++ opendj-sdk/opends/src/server/org/opends/server/extensions/PasswordPolicyStateExtendedOperation.java
@@ -1148,7 +1148,7 @@ public class PasswordPolicyStateExtendedOperation
         {
           long lockoutTime = lastLoginTime + (lockoutInterval*1000);
           long currentTime = pwpState.getCurrentTime();
-          int secondsUntilLockout = (int) ((lockoutTime - currentTime) / 1000);
+          int secondsUntilLockout = (int) ((lockoutTime - currentTime) / 1000L);
           if (secondsUntilLockout <= 0)
           {
             secondsStr = "0";
@@ -1184,7 +1184,7 @@ public class PasswordPolicyStateExtendedOperation
         {
           long currentTime = pwpState.getCurrentTime();
           long changedTime = pwpState.getPasswordChangedTime();
-          int changeAge = (int) ((currentTime - changedTime) / 1000);
+          int changeAge = (int) ((currentTime - changedTime) / 1000L);
           long timeToLockout = maxAge - changeAge;
           if (timeToLockout <= 0)
           {
