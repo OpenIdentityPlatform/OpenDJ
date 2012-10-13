f91d73736b3e59bec8c754fb708338928a3bd383
Matthew Swift <matthew.swift@forgerock.com>
2012-10-13 09:35:02 +0000

    OPENDJ-612: SDK: Race conditions installing client/server filter chains during connect/bind/accept
    Re-enable DefaultTCPNIOTransportTestCase to see if it was the cause of the problems.

--- opendj-sdk/opendj3/opendj-ldap-sdk/src/test/java/com/forgerock/opendj/ldap/DefaultTCPNIOTransportTestCase.java
+++ opendj-sdk/opendj3/opendj-ldap-sdk/src/test/java/com/forgerock/opendj/ldap/DefaultTCPNIOTransportTestCase.java
@@ -48,7 +48,7 @@ public class DefaultTCPNIOTransportTestCase extends LDAPTestCase {
      * @throws Exception
      *             If an unexpected error occurred.
      */
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void testGetInstance() throws Exception {
         // Create a transport.
         final TCPNIOTransport transport = DefaultTCPNIOTransport.getInstance();
