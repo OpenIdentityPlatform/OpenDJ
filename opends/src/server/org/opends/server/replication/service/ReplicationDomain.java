5f04e2771954735e8c79189f9b11aaf5939f9195
Chris Ridd <chris.ridd@forgerock.com>
2013-05-02 12:07:19 +0000

    CR-1612 Fix OPENDJ-886 connected-to attributes under cn=monitor are wrong when all RSes are down

--- opends/src/server/org/opends/server/replication/service/ReplicationDomain.java
+++ opends/src/server/org/opends/server/replication/service/ReplicationDomain.java
@@ -2765,7 +2765,7 @@ public abstract class ReplicationDomain
     if (broker != null)
       return broker.getReplicationServer();
     else
-      return "Not connected";
+      return ReplicationBroker.NO_CONNECTED_SERVER;
   }
 
   /**
