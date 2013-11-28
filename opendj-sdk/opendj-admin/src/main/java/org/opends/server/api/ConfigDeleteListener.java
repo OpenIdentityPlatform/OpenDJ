55100ced9edd185781ef1ba020c228e4f1d2916b
Nicolas Capponi <nicolas.capponi@forgerock.com>
2013-11-28 15:04:58 +0000

    OpenDJ 3 : config framework
    At this point, all classes generated from xml definitons 
    compile.
    
    * Fix import in serverMO.xsl
    
    * Add some server classes to satisfy missing dependencies 
      for generated classes
     * ConfigException and other exceptions it depends on
     * AddressMask class
     * ConfigChangeResult 
    
    * Add stubs of some server classes to satisfy missing
      dependencies for generated classes
     * DirectoryServer
     * Aci

---
+++ opendj-sdk/opendj-admin/src/main/java/org/opends/server/api/ConfigDeleteListener.java
@@ -0,0 +1,80 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Copyright 2006-2008 Sun Microsystems, Inc.
+ */
+package org.opends.server.api;
+
+
+
+import org.opends.server.config.ConfigEntry;
+import org.opends.server.types.ConfigChangeResult;
+import org.opends.messages.MessageBuilder;
+
+
+/**
+ * This interface defines the methods that a Directory Server
+ * component should implement if it wishes to be able to receive
+ * notification if entries below a configuration entry are removed.
+ */
+@org.opends.server.types.PublicAPI(
+     stability=org.opends.server.types.StabilityLevel.VOLATILE,
+     mayInstantiate=false,
+     mayExtend=true,
+     mayInvoke=false)
+public interface ConfigDeleteListener
+{
+  /**
+   * Indicates whether it is acceptable to remove the provided
+   * configuration entry.
+   *
+   * @param  configEntry         The configuration entry that will be
+   *                             removed from the configuration.
+   * @param  unacceptableReason  A buffer to which this method can
+   *                             append a human-readable message
+   *                             explaining why the proposed delete is
+   *                             not acceptable.
+   *
+   * @return  {@code true} if the proposed entry may be removed from
+   *          the configuration, or {@code false} if not.
+   */
+  public boolean configDeleteIsAcceptable(ConfigEntry configEntry,
+                      MessageBuilder unacceptableReason);
+
+
+
+  /**
+   * Attempts to apply a new configuration based on the provided
+   * deleted entry.
+   *
+   * @param  configEntry  The new configuration entry that has been
+   *                      deleted.
+   *
+   * @return  Information about the result of processing the
+   *          configuration change.
+   */
+  public ConfigChangeResult applyConfigurationDelete(
+                                 ConfigEntry configEntry);
+}
+
