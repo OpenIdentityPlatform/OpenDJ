6c6423cd20273cf4ddce2b44a496df0c531ada4a
Nicolas Capponi <nicolas.capponi@forgerock.com>
2013-12-02 11:15:22 +0000

    OpenDJ 3 : config framework
    Reducing compilation errors in org.opends.server.admin packages : 
     all classes in package org.opends.server.admin.client.spi compile
     
    * Simple changes in org.opends.server.admin.client.spi package
    ** replace Message class by LocalizableMessage
    ** remove API annotation

--- opendj-admin/src/main/java/org/opends/server/admin/client/spi/package-info.java
+++ opendj-admin/src/main/java/org/opends/server/admin/client/spi/package-info.java
@@ -33,7 +33,5 @@
  * This package contains classes which client-side driver
  * implementations are expected to use.
  */
-@org.opends.server.types.PublicAPI(
-     stability=org.opends.server.types.StabilityLevel.PRIVATE)
 package org.opends.server.admin.client.spi;
 
