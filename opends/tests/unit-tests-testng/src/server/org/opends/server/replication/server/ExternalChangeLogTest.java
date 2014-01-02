84dbf0fcc1b99d31956590258ddf2aae00b0ffda
Jean-Noel Rouvignac <jean-noel.rouvignac@forgerock.com>
2014-01-02 13:32:49 +0000

    ExternalChangeLogTest.java: Updated copyright.

--- opends/tests/unit-tests-testng/src/server/org/opends/server/replication/server/ExternalChangeLogTest.java
+++ opends/tests/unit-tests-testng/src/server/org/opends/server/replication/server/ExternalChangeLogTest.java
@@ -22,7 +22,7 @@
  *
  *
  *      Copyright 2006-2010 Sun Microsystems, Inc.
- *      Portions copyright 2011-2013 ForgeRock AS
+ *      Portions copyright 2011-2014 ForgeRock AS
  */
 package org.opends.server.replication.server;
 
