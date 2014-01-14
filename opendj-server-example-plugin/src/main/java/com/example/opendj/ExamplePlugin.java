23ab7b85e3631cb4c75a6f9277c1d1aa7a7bfbd6
Matthew Swift <matthew.swift@forgerock.com>
2014-01-14 22:53:17 +0000

    Move XSLT stylesheets and core XML descriptors to opendj-config-maven-plugin:
    * rewrite Maven plugin so that it performs XSLT transformations itself rather than delegating to the xml-maven-plugin. This is required in order to be able to access the stylesheets and core descriptors within the plugin's classloader
    
    * remove dependency on Xalan-J in XSLT transformations since https://java.net/jira/browse/OPENDS-2296 seems to have been fixed, at least in OpenJDK 6u27 and OpenDJ 7u25
    
    * re-enable build of opendj-config, opendj-server, and opendj-server-example-plugin.
    
    TODO once the new Maven plugin has received some more testing in Jenkins:
    
    * add XML validation
    * fix support for Windows file names
    * simplify code.

--- opendj-server-example-plugin/src/main/java/com/example/opendj/ExamplePlugin.java
+++ opendj-server-example-plugin/src/main/java/com/example/opendj/ExamplePlugin.java
@@ -26,26 +26,24 @@
  */
 package com.example.opendj;
 
-import static com.example.opendj.ExamplePluginMessages.*;
-
 import java.util.List;
-import java.util.Set;
 
 import org.forgerock.i18n.LocalizableMessage;
 import org.forgerock.opendj.config.server.ConfigChangeResult;
-import org.forgerock.opendj.config.server.ConfigException;
 import org.forgerock.opendj.config.server.ConfigurationChangeListener;
 import org.forgerock.opendj.ldap.ResultCode;
-import org.forgerock.opendj.server.config.meta.PluginCfgDefn.PluginType;
-import org.opends.server.types.InitializationException;
+
+import com.example.opendj.server.ExamplePluginCfg;
 
 /**
  * The example plugin implementation class. This plugin will output the
  * configured message to the error log during server start up.
  */
 public class ExamplePlugin implements ConfigurationChangeListener<ExamplePluginCfg> {
+    // FIXME: fill in the remainder of this class once the server plugin API is migrated.
 
     // The current configuration.
+    @SuppressWarnings("unused")
     private ExamplePluginCfg config;
 
     /**
@@ -56,71 +54,11 @@ public class ExamplePlugin implements ConfigurationChangeListener<ExamplePluginC
     }
 
     /**
-     * Performs any initialization necessary for this plugin. This will be
-     * called as soon as the plugin has been loaded and before it is registered
-     * with the server.
-     *
-     * @param pluginTypes
-     *            The set of plugin types that indicate the ways in which this
-     *            plugin will be invoked.
-     * @param configuration
-     *            The configuration for this plugin.
-     * @throws ConfigException
-     *             If the provided entry does not contain a valid configuration
-     *             for this plugin.
-     * @throws InitializationException
-     *             If a problem occurs while initializing the plugin that is not
-     *             related to the server configuration.
-     */
-    @Override()
-    public void initializePlugin(Set<PluginType> pluginTypes, ExamplePluginCfg configuration)
-            throws ConfigException, InitializationException {
-        // This plugin may only be used as a server startup plugin.
-        for (PluginType t : pluginTypes) {
-            switch (t) {
-            case STARTUP:
-                // This is fine.
-                break;
-            default:
-                LocalizableMessage message = SEVERE_ERR_INITIALIZE_PLUGIN.get(String.valueOf(t));
-                throw new ConfigException(message);
-            }
-        }
-
-        // Register change listeners. These are not really necessary for
-        // this plugin since it is only used during server start-up.
-        configuration.addExampleChangeListener(this);
-
-        // Save the configuration.
-        this.config = configuration;
-    }
-
-    /**
-     * Performs any processing that should be done when the Directory Server is
-     * in the process of starting. This method will be called after virtually
-     * all other initialization has been performed but before the connection
-     * handlers are started.
-     *
-     * @return The result of the startup plugin processing.
+     * {@inheritDoc}
      */
     @Override
-    public PluginResult.Startup doStartup() {
-        // Log the provided message.
-        LocalizableMessage message = NOTICE_DO_STARTUP.get(String.valueOf(config.getMessage()));
-        logError(message);
-        return PluginResult.Startup.continueStartup();
-    }
-
-    public ConfigChangeResult applyConfigurationChange(ExamplePluginCfg config) {
+    public ConfigChangeResult applyConfigurationChange(final ExamplePluginCfg config) {
         // The new configuration has already been validated.
-
-        // Log a message to say that the configuration has changed. This
-        // isn't necessary, but we'll do it just to show that the change
-        // has taken effect.
-        LocalizableMessage message =
-                NOTICE_APPLY_CONFIGURATION_CHANGE.get(String.valueOf(this.config.getMessage()),
-                        String.valueOf(config.getMessage()));
-        logError(message);
 
         // Update the configuration.
         this.config = config;
@@ -129,11 +67,16 @@ public class ExamplePlugin implements ConfigurationChangeListener<ExamplePluginC
         return new ConfigChangeResult(ResultCode.SUCCESS, false);
     }
 
-    public boolean isConfigurationChangeAcceptable(ExamplePluginCfg config,
-            List<LocalizableMessage> messages) {
-        // The only thing that can be validated here is the plugin's
-        // message. However, it is always going to be valid, so let's
-        // always return true.
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isConfigurationChangeAcceptable(final ExamplePluginCfg config,
+            final List<LocalizableMessage> messages) {
+        /*
+         * The only thing that can be validated here is the plugin's message.
+         * However, it is always going to be valid, so let's always return true.
+         */
         return true;
     }
 }
