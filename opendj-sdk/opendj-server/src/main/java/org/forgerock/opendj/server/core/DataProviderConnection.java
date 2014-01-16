abf2b0c3d4dc2d23dd7f00402f8213d9ec54a9e9
Matthew Swift <matthew.swift@forgerock.com>
2014-01-16 00:19:43 +0000

    Fix build break from previous commit:
    * replace references to DirectoryException with ErrorResultException
    * replace references to InitializationException with ConfigException.

--- opendj-sdk/opendj-server/src/main/java/org/forgerock/opendj/server/core/DataProviderConnection.java
+++ opendj-sdk/opendj-server/src/main/java/org/forgerock/opendj/server/core/DataProviderConnection.java
@@ -30,8 +30,8 @@ import java.util.Set;
 
 import org.forgerock.opendj.ldap.DN;
 import org.forgerock.opendj.ldap.Entry;
+import org.forgerock.opendj.ldap.ErrorResultException;
 import org.forgerock.opendj.ldap.RequestHandler;
-import org.opends.server.types.DirectoryException;
 
 /**
  * A connection to a data provider. When a connection is no longer needed it
@@ -53,12 +53,12 @@ public interface DataProviderConnection extends RequestHandler<Operation> {
      *            The DN of the entry.
      * @return {@code true} if the underlying data provider contains the
      *         specified entry, or {@code false} if it does not.
-     * @throws DirectoryException
+     * @throws ErrorResultException
      *             If a problem occurs while trying to make the determination,
      *             or if {@code dn} is not a DN equal to or subordinate to one
      *             of the base DNs managed by the underlying data provider.
      */
-    boolean containsEntry(DN dn) throws DirectoryException;
+    boolean containsEntry(DN dn) throws ErrorResultException;
 
     /**
      * Deregisters an event listener from the underlying data provider.
@@ -84,12 +84,12 @@ public interface DataProviderConnection extends RequestHandler<Operation> {
      *            The DN of the entry.
      * @return The requested entry, or {@code null} if the underlying data
      *         provider does not contain the specified entry.
-     * @throws DirectoryException
+     * @throws ErrorResultException
      *             If a problem occurs while trying to retrieve the entry, or if
      *             {@code dn} is not a DN equal to or subordinate to one of the
      *             base DNs managed by the underlying data provider.
      */
-    Entry getEntry(DN dn) throws DirectoryException;
+    Entry getEntry(DN dn) throws ErrorResultException;
 
     /**
      * Returns the current status of the provided base DN in the underlying data
@@ -99,11 +99,11 @@ public interface DataProviderConnection extends RequestHandler<Operation> {
      *            The base DN in the underlying data provider.
      * @return The current status of the provided base DN in the underlying data
      *         provider.
-     * @throws DirectoryException
+     * @throws ErrorResultException
      *             If {@code baseDN} is not one of the base DNs managed by the
      *             underlying data provider.
      */
-    DataProviderStatus getStatus(DN baseDN) throws DirectoryException;
+    DataProviderStatus getStatus(DN baseDN) throws ErrorResultException;
 
     /**
      * Returns an unmodifiable set containing the OIDs of the controls that may
@@ -114,11 +114,11 @@ public interface DataProviderConnection extends RequestHandler<Operation> {
      * @return An unmodifiable set containing the OIDs of the controls that may
      *         be supported by the provided base DN in the underlying data
      *         provider.
-     * @throws DirectoryException
+     * @throws ErrorResultException
      *             If {@code baseDN} is not one of the base DNs managed by the
      *             underlying data provider.
      */
-    Set<String> getSupportedControls(DN baseDN) throws DirectoryException;
+    Set<String> getSupportedControls(DN baseDN) throws ErrorResultException;
 
     /**
      * Returns an unmodifiable set containing the OIDs of the features that may
@@ -129,11 +129,11 @@ public interface DataProviderConnection extends RequestHandler<Operation> {
      * @return An unmodifiable set containing the OIDs of the features that may
      *         be supported by the provided base DN in the underlying data
      *         provider.
-     * @throws DirectoryException
+     * @throws ErrorResultException
      *             If {@code baseDN} is not one of the base DNs managed by the
      *             underlying data provider.
      */
-    Set<String> getSupportedFeatures(DN baseDN) throws DirectoryException;
+    Set<String> getSupportedFeatures(DN baseDN) throws ErrorResultException;
 
     /**
      * Registers an event listener with the underlying data provider.
@@ -151,9 +151,9 @@ public interface DataProviderConnection extends RequestHandler<Operation> {
      *            The base DN in the underlying data provider.
      * @return {@code true} if the provided base DN in the underlying data
      *         provider supports change notification.
-     * @throws DirectoryException
+     * @throws ErrorResultException
      *             If {@code baseDN} is not one of the base DNs managed by the
      *             underlying data provider.
      */
-    boolean supportsChangeNotification(DN baseDN) throws DirectoryException;
+    boolean supportsChangeNotification(DN baseDN) throws ErrorResultException;
 }
