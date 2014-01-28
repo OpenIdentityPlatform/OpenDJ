OpenIdentityPlatform/OpenDJ

commit 826aad4b93f2478bfb0e819ba20c1bcde7966690

OPENDJ-1263 Changenumber does not progress on the second replication server of a topology
The medium consistency point was not progressing on DSRS 2 because the ChangeNumberIndexer mediumConsistencyCSN had been polluted by CSN from "cn=admin data".
Problem is that ChangeNumberIndexer's publishUpdateMsg() was filtering out updates from ECL disabled domains, but not from domains unknown to ECL.
On start up, update messages can be received from replication domains have not been been configured yet on the current replication server.
The fix consists in only updating the medium consistency point with update messages or replica heartbeats coming from explicitly enabled replication domains.
After this fix, changes from ECL disabled domains will still be stored in dedicated replicaDBs, there is no change in this functionality.


MultimasterReplication.java
Renamed isECLDisabledDomain() to isECLEnabledDomain() so it caters better for the 3 states of the domains: enabled, disabled and unknown
Code cleanup.

ChangeNumberIndexer.java:
Extracted method isECLEnabledDomain() to use as a seam for unit testing which calls to MultimasterReplication.isECLEnabledDomain().

ChangeNumberIndexerTest.java
Added one test emptyDBThreeInitialDSsOneIsNotECLEnabledDomain().
Renamed assertAddedRecords() to assertExternalChangelogContent(), startIndexer() to startCNIndexer(), stopIndexer() to stopCNIndexer() and indexer to cnIndexer.
In startCNIndexer(), overrode ChangeNumberIndexer.isNotECLEnabledDomain().

AttributeTypeConstants.java:
Added support for "cn"


git-svn-id: https://svn.forgerock.org/opendj/trunk@10049 41b1ffd8-f28e-4786-ab96-9950f0a78031

opends/src/server/org/opends/server/replication/plugin/MultimasterReplication.java
@@ -222,9 +222,10 @@ public class MultimasterReplication
   public static void deleteDomain(DN dn)
   {
     LDAPReplicationDomain domain = domains.remove(dn);
-
     if (domain != null)
+    {
       domain.delete();
+    }
 
     // No replay threads running if no replication need
     if (domains.size() == 0) {
@@ -257,8 +258,7 @@ public class MultimasterReplication
     //  Create the list of domains that are already defined.
     for (String name : configuration.listReplicationDomains())
     {
-      ReplicationDomainCfg domain = configuration.getReplicationDomain(name);
-      createNewDomain(domain);
+      createNewDomain(configuration.getReplicationDomain(name));
     }
 
     /*
@@ -458,9 +458,7 @@ public class MultimasterReplication
       modifyOperation.setAttachment(EntryHistorical.HISTORICAL,
           historicalInformation);
     }
-
     historicalInformation.setPurgeDelay(domain.getHistoricalPurgeDelay());
-
     historicalInformation.setHistoricalAttrToOperation(modifyOperation);
 
     if (modifyOperation.getModifications().isEmpty())
@@ -509,7 +507,6 @@ public class MultimasterReplication
       modifyDNOperation.setAttachment(EntryHistorical.HISTORICAL,
           historicalInformation);
     }
-
     historicalInformation.setPurgeDelay(domain.getHistoricalPurgeDelay());
 
     // Add to the operation the historical attribute : "dn:changeNumber:moddn"
@@ -798,18 +795,20 @@ public class MultimasterReplication
   }
 
   /**
-   * Returns whether the provided baseDN is disabled for the external changelog.
+   * Returns whether the provided baseDN represents a replication domain enabled
+   * for the external changelog.
    *
    * @param baseDN
-   *          the domain to check
-   * @return true if the provided baseDN is disabled for the external changelog,
-   *         false otherwise
+   *          the replication domain to check
+   * @return true if the provided baseDN is enabled for the external changelog,
+   *         false if the provided baseDN is disabled for the external changelog
+   *         or unknown to multimaster replication.
    */
-  public static boolean isECLDisabledDomain(DN baseDN)
+  public static boolean isECLEnabledDomain(DN baseDN)
   {
     for (LDAPReplicationDomain domain : domains.values())
     {
-      if (!domain.isECLEnabled() && domain.getBaseDN().equals(baseDN))
+      if (domain.isECLEnabled() && domain.getBaseDN().equals(baseDN))
       {
         return true;
       }

opends/src/server/org/opends/server/replication/server/changelog/je/ChangeNumberIndexer.java
@@ -176,7 +176,7 @@ public class ChangeNumberIndexer extends DirectoryThread
    */
   public void publishHeartbeat(DN baseDN, CSN heartbeatCSN)
   {
-    if (MultimasterReplication.isECLDisabledDomain(baseDN))
+    if (!isECLEnabledDomain(baseDN))
     {
       return;
     }
@@ -198,7 +198,7 @@ public class ChangeNumberIndexer extends DirectoryThread
   public void publishUpdateMsg(DN baseDN, UpdateMsg updateMsg)
       throws ChangelogException
   {
-    if (MultimasterReplication.isECLDisabledDomain(baseDN))
+    if (!isECLEnabledDomain(baseDN))
     {
       return;
     }
@@ -208,6 +208,23 @@ public class ChangeNumberIndexer extends DirectoryThread
     // only keep the oldest CSN that will be the new cursor's starting point
     newCursors.putIfAbsent(Pair.of(baseDN, csn.getServerId()), csn);
     tryNotify(baseDN);
+  }
+
+  /**
+   * Returns whether the provided baseDN represents a replication domain enabled
+   * for the external changelog.
+   * <p>
+   * This method is a test seam that break the dependency on a static method.
+   *
+   * @param baseDN
+   *          the replication domain to check
+   * @return true if the provided baseDN is enabled for the external changelog,
+   *         false if the provided baseDN is disabled for the external changelog
+   *         or unknown to multimaster replication.
+   */
+  protected boolean isECLEnabledDomain(DN baseDN)
+  {
+    return MultimasterReplication.isECLEnabledDomain(baseDN);
   }
 
   /**
@@ -288,6 +305,11 @@ public class ChangeNumberIndexer extends DirectoryThread
         : changelogState.getDomainToServerIds().entrySet())
     {
       final DN baseDN = entry.getKey();
+      if (!isECLEnabledDomain(baseDN))
+      {
+        continue;
+      }
+
       for (Integer serverId : entry.getValue())
       {
         final CSN csn = mediumConsistencyRUV.getCSN(baseDN, serverId);

opends/tests/unit-tests-testng/src/server/org/opends/server/replication/server/changelog/je/ChangeNumberIndexerTest.java
@@ -93,8 +93,10 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   }
 
   private static DN BASE_DN;
+  private static DN ADMIN_DATA_DN;
   private static final int serverId1 = 101;
   private static final int serverId2 = 102;
+  private static final int serverId3 = 103;
 
   private ChangelogDB changelogDB;
   private ChangeNumberIndexDB cnIndexDB;
@@ -102,7 +104,7 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   private Map<Pair<DN, Integer>, SequentialDBCursor> cursors =
       new HashMap<Pair<DN, Integer>, SequentialDBCursor>();
   private ChangelogState initialState;
-  private ChangeNumberIndexer indexer;
+  private ChangeNumberIndexer cnIndexer;
   private MultiDomainServerState initialCookie;
 
   @BeforeClass
@@ -110,6 +112,7 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   {
     TestCaseUtils.startFakeServer();
     BASE_DN = DN.decode("dc=example,dc=com");
+    ADMIN_DATA_DN = DN.decode("cn=admin data");
   }
 
   @AfterClass
@@ -134,7 +137,7 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   @AfterMethod
   public void tearDown() throws Exception
   {
-    stopIndexer();
+    stopCNIndexer();
   }
 
   private static final String EMPTY_DB_NO_DS = "emptyDBNoDS";
@@ -142,19 +145,19 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   @Test
   public void emptyDBNoDS() throws Exception
   {
-    startIndexer();
-    verify(cnIndexDB, never()).addRecord(any(ChangeNumberIndexRecord.class));
+    startCNIndexer();
+    assertExternalChangelogContent();
   }
 
   @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
   public void emptyDBOneInitialDS() throws Exception
   {
     addReplica(BASE_DN, serverId1);
-    startIndexer();
+    startCNIndexer();
 
     final ReplicatedUpdateMsg msg1 = msg(BASE_DN, serverId1, 1);
     publishUpdateMsg(msg1);
-    assertAddedRecords(msg1);
+    assertExternalChangelogContent(msg1);
   }
 
   @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
@@ -163,11 +166,11 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
     final ReplicatedUpdateMsg msg1 = msg(BASE_DN, serverId1, 1);
     addReplica(BASE_DN, serverId1);
     setDBInitialRecords(msg1);
-    startIndexer();
+    startCNIndexer();
 
     final ReplicatedUpdateMsg msg2 = msg(BASE_DN, serverId1, 2);
     publishUpdateMsg(msg2);
-    assertAddedRecords(msg2);
+    assertExternalChangelogContent(msg2);
   }
 
   @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
@@ -175,12 +178,12 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   {
     addReplica(BASE_DN, serverId1);
     addReplica(BASE_DN, serverId2);
-    startIndexer();
+    startCNIndexer();
 
     final ReplicatedUpdateMsg msg1 = msg(BASE_DN, serverId1, 1);
     final ReplicatedUpdateMsg msg2 = msg(BASE_DN, serverId2, 2);
     publishUpdateMsg(msg2, msg1);
-    assertAddedRecords(msg1);
+    assertExternalChangelogContent(msg1);
   }
 
   @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
@@ -191,20 +194,20 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
     addReplica(BASE_DN, serverId1);
     addReplica(BASE_DN, serverId2);
     setDBInitialRecords(msg1, msg2);
-    startIndexer();
+    startCNIndexer();
 
     final ReplicatedUpdateMsg msg3 = msg(BASE_DN, serverId2, 3);
     final ReplicatedUpdateMsg msg4 = msg(BASE_DN, serverId1, 4);
     publishUpdateMsg(msg3, msg4);
-    assertAddedRecords(msg3);
+    assertExternalChangelogContent(msg3);
 
     final ReplicatedUpdateMsg msg5 = msg(BASE_DN, serverId1, 5);
     publishUpdateMsg(msg5);
-    assertAddedRecords(msg3);
+    assertExternalChangelogContent(msg3);
 
     final ReplicatedUpdateMsg msg6 = msg(BASE_DN, serverId2, 6);
     publishUpdateMsg(msg6);
-    assertAddedRecords(msg3, msg4, msg5);
+    assertExternalChangelogContent(msg3, msg4, msg5);
   }
 
   @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
@@ -212,7 +215,7 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   {
     addReplica(BASE_DN, serverId1);
     addReplica(BASE_DN, serverId2);
-    startIndexer();
+    startCNIndexer();
 
     final ReplicatedUpdateMsg msg1Sid2 = msg(BASE_DN, serverId2, 1);
     final ReplicatedUpdateMsg emptySid2 = emptyCursor(BASE_DN, serverId2);
@@ -220,14 +223,34 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
     final ReplicatedUpdateMsg msg3Sid2 = msg(BASE_DN, serverId2, 3);
     // simulate no messages received during some time for replica 2
     publishUpdateMsg(msg1Sid2, emptySid2, emptySid2, emptySid2, msg3Sid2, msg2Sid1);
-    assertAddedRecords(msg1Sid2, msg2Sid1);
+    assertExternalChangelogContent(msg1Sid2, msg2Sid1);
+  }
+
+  @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
+  public void emptyDBThreeInitialDSsOneIsNotECLEnabledDomain() throws Exception
+  {
+    addReplica(ADMIN_DATA_DN, serverId1);
+    addReplica(BASE_DN, serverId2);
+    addReplica(BASE_DN, serverId3);
+    startCNIndexer();
+
+    // cn=admin data will does not participate in the external changelog
+    // so it cannot add to it
+    final ReplicatedUpdateMsg msg1 = msg(ADMIN_DATA_DN, serverId1, 1);
+    publishUpdateMsg(msg1);
+    assertExternalChangelogContent();
+
+    final ReplicatedUpdateMsg msg2 = msg(BASE_DN, serverId2, 2);
+    final ReplicatedUpdateMsg msg3 = msg(BASE_DN, serverId3, 3);
+    publishUpdateMsg(msg2, msg3);
+    assertExternalChangelogContent(msg2);
   }
 
   @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
   public void emptyDBOneInitialDSAnotherDSJoining() throws Exception
   {
     addReplica(BASE_DN, serverId1);
-    startIndexer();
+    startCNIndexer();
 
     final ReplicatedUpdateMsg msg1 = msg(BASE_DN, serverId1, 1);
     publishUpdateMsg(msg1);
@@ -235,11 +258,11 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
     addReplica(BASE_DN, serverId2);
     final ReplicatedUpdateMsg msg2 = msg(BASE_DN, serverId2, 2);
     publishUpdateMsg(msg2);
-    assertAddedRecords(msg1);
+    assertExternalChangelogContent(msg1);
 
     final ReplicatedUpdateMsg msg3 = msg(BASE_DN, serverId1, 3);
     publishUpdateMsg(msg3);
-    assertAddedRecords(msg1, msg2);
+    assertExternalChangelogContent(msg1, msg2);
   }
 
   @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
@@ -247,15 +270,15 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   {
     addReplica(BASE_DN, serverId1);
     addReplica(BASE_DN, serverId2);
-    startIndexer();
+    startCNIndexer();
 
     final ReplicatedUpdateMsg msg1 = msg(BASE_DN, serverId1, 1);
     final ReplicatedUpdateMsg msg2 = msg(BASE_DN, serverId2, 2);
     publishUpdateMsg(msg1, msg2);
-    assertAddedRecords(msg1);
+    assertExternalChangelogContent(msg1);
 
     sendHeartbeat(BASE_DN, serverId1, 3);
-    assertAddedRecords(msg1, msg2);
+    assertExternalChangelogContent(msg1, msg2);
   }
 
   @Test(dependsOnMethods = { EMPTY_DB_NO_DS })
@@ -263,22 +286,22 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
   {
     addReplica(BASE_DN, serverId1);
     addReplica(BASE_DN, serverId2);
-    startIndexer();
+    startCNIndexer();
 
     final ReplicatedUpdateMsg msg1 = msg(BASE_DN, serverId1, 1);
     final ReplicatedUpdateMsg msg2 = msg(BASE_DN, serverId2, 2);
     publishUpdateMsg(msg1, msg2);
-    assertAddedRecords(msg1);
+    assertExternalChangelogContent(msg1);
 
     replicaOffline(BASE_DN, serverId2, 3);
     // MCP cannot move forward since no new updates from serverId1
-    assertAddedRecords(msg1);
+    assertExternalChangelogContent(msg1);
 
     final ReplicatedUpdateMsg msg4 = msg(BASE_DN, serverId1, 4);
     publishUpdateMsg(msg4);
     // MCP moved forward after receiving update from serverId1
     // (last replica in the domain)
-    assertAddedRecords(msg1, msg2, msg4);
+    assertExternalChangelogContent(msg1, msg2, msg4);
   }
 
 
@@ -292,16 +315,23 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
     initialState.addServerIdToDomain(serverId, baseDN);
   }
 
-  private void startIndexer()
+  private void startCNIndexer()
   {
-    indexer = new ChangeNumberIndexer(changelogDB, initialState);
-    indexer.start();
-    waitForWaitingState(indexer);
+    cnIndexer = new ChangeNumberIndexer(changelogDB, initialState)
+    {
+      @Override
+      protected boolean isECLEnabledDomain(DN baseDN)
+      {
+        return BASE_DN.equals(baseDN);
+      }
+    };
+    cnIndexer.start();
+    waitForWaitingState(cnIndexer);
   }
 
-  private void stopIndexer()
+  private void stopCNIndexer()
   {
-    indexer.initiateShutdown();
+    cnIndexer.initiateShutdown();
   }
 
   private ReplicatedUpdateMsg msg(DN baseDN, int serverId, long time)
@@ -356,22 +386,22 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
     {
       if (!msg.isEmptyCursor())
       {
-        indexer.publishUpdateMsg(msg.getBaseDN(), msg);
+        cnIndexer.publishUpdateMsg(msg.getBaseDN(), msg);
       }
     }
-    waitForWaitingState(indexer);
+    waitForWaitingState(cnIndexer);
   }
 
   private void sendHeartbeat(DN baseDN, int serverId, int time) throws Exception
   {
-    indexer.publishHeartbeat(baseDN, new CSN(time, 0, serverId));
-    waitForWaitingState(indexer);
+    cnIndexer.publishHeartbeat(baseDN, new CSN(time, 0, serverId));
+    waitForWaitingState(cnIndexer);
   }
 
   private void replicaOffline(DN baseDN, int serverId, int time) throws Exception
   {
-    indexer.replicaOffline(baseDN, new CSN(time, 0, serverId));
-    waitForWaitingState(indexer);
+    cnIndexer.replicaOffline(baseDN, new CSN(time, 0, serverId));
+    waitForWaitingState(cnIndexer);
   }
 
   private void waitForWaitingState(final Thread t)
@@ -391,8 +421,15 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
    * Asserts which records have been added to the CNIndexDB since starting the
    * {@link ChangeNumberIndexer} thread.
    */
-  private void assertAddedRecords(ReplicatedUpdateMsg... msgs) throws Exception
+  private void assertExternalChangelogContent(ReplicatedUpdateMsg... msgs)
+      throws Exception
   {
+    if (msgs.length == 0)
+    {
+      verify(cnIndexDB, never()).addRecord(any(ChangeNumberIndexRecord.class));
+      return;
+    }
+
     final ArgumentCaptor<ChangeNumberIndexRecord> arg =
         ArgumentCaptor.forClass(ChangeNumberIndexRecord.class);
     verify(cnIndexDB, atLeast(0)).addRecord(arg.capture());
@@ -410,7 +447,7 @@ public class ChangeNumberIndexerTest extends DirectoryServerTestCase
       final ChangeNumberIndexRecord record = allValues.get(i);
       if (previousCookie.isEmpty())
       {
-        // ugly hack to go round strange legacy code
+        // ugly hack to go round strange legacy code @see OPENDJ-67
         previousCookie.replace(record.getBaseDN(), new ServerState());
       }
       // check content in order

opends/tests/unit-tests-testng/src/server/org/opends/server/types/AttributeTypeConstants.java
@@ -47,6 +47,12 @@ public interface AttributeTypeConstants
       null, null, OID_SYNTAX, AttributeUsage.USER_APPLICATIONS,
       false, false, false, false);
 
+  AttributeType COMMON_NAME = new AttributeType(
+      "( 2.5.4.3 NAME ( 'cn' 'commonName' ) SUP name X-ORIGIN 'RFC 4519' )",
+      "commonName", Arrays.asList("cn", "commonName"), "2.5.4.3",
+      null, null, OID_SYNTAX, AttributeUsage.USER_APPLICATIONS,
+      false, false, false, false);
+
   AttributeType ORGANIZATION_NAME = new AttributeType(
       "( 2.5.4.10 NAME ( 'o' 'organizationName' ) SUP name X-ORIGIN 'RFC 4519' )",
       "organizationName", Arrays.asList("o", "organizationName"), "2.5.4.10",
@@ -67,7 +73,7 @@ public interface AttributeTypeConstants
       null, null, OID_SYNTAX, AttributeUsage.USER_APPLICATIONS,
       false, false, false, false);
 
-  AttributeType[] ALL = { OBJECT_CLASS, ORGANIZATION_NAME,
+  AttributeType[] ALL = { OBJECT_CLASS, COMMON_NAME, ORGANIZATION_NAME,
     ORGANIZATIONAL_UNIT_NAME, DOMAIN_COMPONENT, };
 
 }

commit 08727ec559e7018316d3b25b04a828ee0a127b2a

OPENDJ-1262 NPE in ChangeNumberIndex during server startup
Code review: Matthew Swift

Caused by r10049.
Problem is down to the initialization sequence:
1. thread 1 - MultimasterReplication.initializeSynchronizationProvider()
1.1. it creates the ReplicationServerListener
1.1.1. the ReplicationServerListener in turn creates the ReplicationServer
1.1.1.1. the ReplicationServer in turn creates the ChangelogDB
1.1.1.1.1. the ChangelogDB in turn creates the ChangeNumberIndexer thread and STARTs it
1.1.1.1.1. the ChangelogDB starts the ChangeNumberIndexer thread
1.2. it proceeds with creating the LDAPReplicationDomain objects one by one
2. thread 2 - ChangeNumberIndexer.run()
2.1. it calls ChangeNumberIndexer.initialize()
2.1.1. ChangeNumberIndexer.initialize() calls MultimasterReplication.isECLEnabledDomain(baseDN)

Steps 1.2. and 2.1.1. are running concurrently.
If 2.1.1. is run before 1.2. is completed, In ChangeNumberIndexer.initialize():
1) MultimasterReplication.isECLEnabledDomain(baseDN) returns false, hence a cursor to the relevant replica DBs is not created
2) then the call to nextChangeForInsertDBCursor.getRecord() returns null, later throwing a NullPointerException because the ChangeNumberIndexer thread is in an illegal state: it was expecting to find an UpdateMsg with the correct CSN stamped on it.



MultimasterReplication.java:
Added State enum + state instance member to tell whether MultimasterReplication is ready for work.
Removed isRegistered instance member superseded by state instance member.
In isECLEnabledDomain(), completeSynchronizationProvider() and finalizeSynchronizationProvider(), deal with thread waits.

DomainFakeCfg.java:
Implemented toString().

opends/src/server/org/opends/server/replication/plugin/MultimasterReplication.java
@@ -22,7 +22,7 @@
  *
  *
  *      Copyright 2006-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2011-2013 ForgeRock AS
+ *      Portions Copyright 2011-2014 ForgeRock AS
  */
 package org.opends.server.replication.plugin;
 
@@ -30,6 +30,7 @@ import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.opends.messages.Message;
 import org.opends.server.admin.server.ConfigurationAddListener;
@@ -67,7 +68,7 @@ public class MultimasterReplication
                   ExportTaskListener
 {
   private ReplicationServerListener replicationServerListener = null;
-  private static Map<DN, LDAPReplicationDomain> domains =
+  private static final Map<DN, LDAPReplicationDomain> domains =
     new ConcurrentHashMap<DN, LDAPReplicationDomain>(4) ;
 
   /**
@@ -80,7 +81,7 @@ public class MultimasterReplication
   /**
    * The list of ReplayThread threads.
    */
-  private static List<ReplayThread> replayThreads =
+  private static final List<ReplayThread> replayThreads =
     new ArrayList<ReplayThread>();
 
   /**
@@ -88,7 +89,16 @@ public class MultimasterReplication
    */
   private static int replayThreadNumber = 10;
 
-  private boolean isRegistered = false;
+  /**
+   * enum that symbolizes the state of the multimaster replication.
+   */
+  private static enum State
+  {
+    STARTING, RUNNING, STOPPING
+  }
+
+  private static final AtomicReference<State> state =
+      new AtomicReference<State>(State.STARTING);
 
   /**
    * The configurable connection/handshake timeout.
@@ -338,9 +348,12 @@ public class MultimasterReplication
     try
     {
       LDAPReplicationDomain rd = createNewDomain(configuration);
-      if (isRegistered)
+      if (State.RUNNING.equals(state.get()))
       {
         rd.start();
+        if (State.STOPPING.equals(state.get())) {
+          rd.shutdown();
+        }
       }
       return new ConfigChangeResult(ResultCode.SUCCESS, false);
     } catch (ConfigException e)
@@ -540,19 +553,16 @@ public class MultimasterReplication
   @Override
   public void finalizeSynchronizationProvider()
   {
-    isRegistered = false;
+    setState(State.STOPPING);
 
-    // shutdown all the domains
     for (LDAPReplicationDomain domain : domains.values())
     {
       domain.shutdown();
     }
     domains.clear();
 
-    // Stop replay threads
     stopReplayThreads();
 
-    // shutdown the ReplicationServer Service if necessary
     if (replicationServerListener != null)
       replicationServerListener.shutdown();
 
@@ -757,12 +767,19 @@ public class MultimasterReplication
   @Override
   public void completeSynchronizationProvider()
   {
-    isRegistered = true;
-
-    // start all the domains
     for (LDAPReplicationDomain domain : domains.values())
     {
       domain.start();
+    }
+    setState(State.RUNNING);
+  }
+
+  private void setState(State newState)
+  {
+    state.set(newState);
+    synchronized (state)
+    {
+      state.notifyAll();
     }
   }
 
@@ -806,6 +823,24 @@ public class MultimasterReplication
    */
   public static boolean isECLEnabledDomain(DN baseDN)
   {
+    if (State.STARTING.equals(state.get()))
+    {
+      synchronized (state)
+      {
+        while (State.STARTING.equals(state.get()))
+        {
+          try
+          {
+            state.wait();
+          }
+          catch (InterruptedException ignored)
+          {
+            // loop and check state again
+          }
+        }
+      }
+    }
+    // if state is STOPPING, then we need to return from this method
     for (LDAPReplicationDomain domain : domains.values())
     {
       if (domain.isECLEnabled() && domain.getBaseDN().equals(baseDN))

opends/tests/unit-tests-testng/src/server/org/opends/server/replication/plugin/DomainFakeCfg.java
@@ -22,7 +22,7 @@
  *
  *
  *      Copyright 2007-2010 Sun Microsystems, Inc.
- *      Portions copyright 2011-2013 ForgeRock AS
+ *      Portions copyright 2011-2014 ForgeRock AS
  */
 package org.opends.server.replication.plugin;
 
@@ -389,13 +389,10 @@ public class DomainFakeCfg implements ReplicationDomainCfg
    * @throws ConfigException
    *          If the add listener could not be registered.
    */
-  public
-  void addECLDomainAddListener(
+  public void addECLDomainAddListener(
       ConfigurationAddListener<ExternalChangelogDomainCfg> listener)
-  throws ConfigException
+      throws ConfigException
   {}
-
-
 
   /**
    * Deregisters an existing ECL Domain configuration add listener.
@@ -404,11 +401,8 @@ public class DomainFakeCfg implements ReplicationDomainCfg
    *          The ECL Domain configuration add listener.
    */
   public void removeECLDomainAddListener(
-      ConfigurationAddListener<ExternalChangelogDomainCfg>
-  listener)
+      ConfigurationAddListener<ExternalChangelogDomainCfg> listener)
   {}
-
-
 
   /**
    * Registers to be notified the ECL Domain is deleted.
@@ -418,13 +412,10 @@ public class DomainFakeCfg implements ReplicationDomainCfg
    * @throws ConfigException
    *          If the delete listener could not be registered.
    */
-  public void
-  addECLDomainDeleteListener(
+  public void addECLDomainDeleteListener(
       ConfigurationDeleteListener<ExternalChangelogDomainCfg> listener)
-  throws ConfigException
+      throws ConfigException
   {}
-
-
 
   /**
    * Deregisters an existing ECL Domain configuration delete listener.
@@ -432,8 +423,7 @@ public class DomainFakeCfg implements ReplicationDomainCfg
    * @param listener
    *          The ECL Domain configuration delete listener.
    */
-  public void
-  removeECLDomainDeleteListener(
+  public void removeECLDomainDeleteListener(
       ConfigurationDeleteListener<ExternalChangelogDomainCfg> listener)
   {}
 
@@ -457,4 +447,10 @@ public class DomainFakeCfg implements ReplicationDomainCfg
     return 1440;
   }
 
+  @Override
+  public String toString()
+  {
+    return getClass().getSimpleName() + ", baseDN=" + baseDN + ", serverId="
+        + serverId + ", replicationServers=" + replicationServers;
+  }
 }