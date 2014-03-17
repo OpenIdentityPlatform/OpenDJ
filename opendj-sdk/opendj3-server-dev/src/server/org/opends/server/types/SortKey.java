0793bcdacd4da21866bc2b9424c0634cd7bf312f
Jean-Noel Rouvignac <jean-noel.rouvignac@forgerock.com>
2014-03-17 14:02:23 +0000

    Fixed checkstyle error.

--- opendj-sdk/opendj3-server-dev/src/server/org/opends/server/types/SortKey.java
+++ opendj-sdk/opendj3-server-dev/src/server/org/opends/server/types/SortKey.java
@@ -38,7 +38,7 @@ import org.opends.server.api.OrderingMatchingRule;
  * the sorting process, although if none is provided it will use the
  * default ordering matching rule for the attribute type.
  * <p>
- * FIXME: replace with the equivalent SDK type. 
+ * FIXME: replace with the equivalent SDK type.
  */
 @org.opends.server.types.PublicAPI(
      stability=org.opends.server.types.StabilityLevel.VOLATILE,
