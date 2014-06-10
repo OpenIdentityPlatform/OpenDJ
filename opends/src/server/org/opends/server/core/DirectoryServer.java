f27e4758940306ae5f602f68fa20f33064757bbc
Jean-Noel Rouvignac <jean-noel.rouvignac@forgerock.com>
2014-06-10 07:16:30 +0000

    Fixing the build.
    DirectoryServer.getClassLoader() is used by SNMPClassLoaderProvider, so made it public again.

--- opends/src/server/org/opends/server/core/DirectoryServer.java
+++ opends/src/server/org/opends/server/core/DirectoryServer.java
@@ -9421,7 +9421,7 @@ public final class DirectoryServer
    * @return Returns the class loader to be used with this directory
    *         server application.
    */
-  private static ClassLoader getClassLoader()
+  public static ClassLoader getClassLoader()
   {
     return ClassLoaderProvider.getInstance().getClassLoader();
   }
