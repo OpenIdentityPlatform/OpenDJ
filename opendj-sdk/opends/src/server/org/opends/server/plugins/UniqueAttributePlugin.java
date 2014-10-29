c9e89aad4892dfe1476c95c647fab049aa3d1c43
Jean-Noel Rouvignac <jean-noel.rouvignac@forgerock.com>
2014-10-29 16:46:02 +0000

    Updated copyrights after r11216.

--- opendj-sdk/opends/src/server/org/opends/server/plugins/UniqueAttributePlugin.java
+++ opendj-sdk/opends/src/server/org/opends/server/plugins/UniqueAttributePlugin.java
@@ -22,7 +22,7 @@
  *
  *
  *      Copyright 2008-2009 Sun Microsystems, Inc.
- *      Portions Copyright 2011-2013 ForgeRock AS
+ *      Portions Copyright 2011-2014 ForgeRock AS
  */
 package org.opends.server.plugins;
 
--- opendj-sdk/opends/tests/unit-tests-testng/src/server/org/opends/server/controls/PersistentSearchControlTest.java
+++ opendj-sdk/opends/tests/unit-tests-testng/src/server/org/opends/server/controls/PersistentSearchControlTest.java
@@ -22,6 +22,7 @@
  *
  *
  *      Copyright 2006-2010 Sun Microsystems, Inc.
+ *      Portions Copyright 2014 ForgeRock AS
  */
 package org.opends.server.controls;
 
