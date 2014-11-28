030cd9b2dbc0d7016114f75e8b93190e34d1e213
Nicolas Capponi <nicolas.capponi@forgerock.com>
2014-11-28 14:11:01 +0000

    OPENDJ-1611 CR-5492 Filter replicaIds when filling late queue in MessageHandler

--- opends/src/server/org/opends/server/replication/server/ServerHandler.java
+++ opends/src/server/org/opends/server/replication/server/ServerHandler.java
@@ -29,6 +29,7 @@ package org.opends.server.replication.server;
 import java.io.IOException;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -960,13 +961,15 @@ public abstract class ServerHandler extends MessageHandler
    * Select the next update that must be sent to the server managed by this
    * ServerHandler.
    *
+   * @param connectedReplicaIds
+   *          Ids of replicas to accept when returning a message.
    * @return the next update that must be sent to the server managed by this
    *         ServerHandler.
    */
-  public UpdateMsg take()
+  public UpdateMsg take(Set<Integer> connectedReplicaIds)
   {
     boolean interrupted = true;
-    UpdateMsg msg = getNextMessage(true); // synchronous:block until msg
+    UpdateMsg msg = getNextMessage(connectedReplicaIds, true); // synchronous:block until msg
 
     boolean acquired = false;
     do
