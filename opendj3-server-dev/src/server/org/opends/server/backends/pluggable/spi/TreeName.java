58398db6b3177cad8845a4a458cb631966ccc711
Matthew Swift <matthew.swift@forgerock.com>
2014-12-17 10:31:22 +0000

    Move SPI interfaces to separate package.

---
+++ opendj3-server-dev/src/server/org/opends/server/backends/pluggable/spi/TreeName.java
@@ -0,0 +1,123 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at legal-notices/CDDLv1_0.txt
+ * or http://forgerock.org/license/CDDLv1.0.html.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at legal-notices/CDDLv1_0.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Copyright 2014 ForgeRock AS
+ */
+
+package org.opends.server.backends.pluggable.spi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/** Assumes name components don't contain a '/'. */
+public final class TreeName
+{
+  public static TreeName of(final String... names)
+  {
+    return new TreeName(Arrays.asList(names));
+  }
+
+  private final List<String> names;
+  private final String s;
+
+  public TreeName(final List<String> names)
+  {
+    this.names = names;
+    final StringBuilder builder = new StringBuilder();
+    for (final String name : names)
+    {
+      builder.append('/');
+      builder.append(name);
+    }
+    this.s = builder.toString();
+  }
+
+  public List<String> getNames()
+  {
+    return names;
+  }
+
+  public TreeName child(final String name)
+  {
+    final List<String> newNames = new ArrayList<String>(names.size() + 1);
+    newNames.addAll(names);
+    newNames.add(name);
+    return new TreeName(newNames);
+  }
+
+  public TreeName getSuffix()
+  {
+    if (names.size() == 0)
+    {
+      throw new IllegalStateException();
+    }
+    return new TreeName(Collections.singletonList(names.get(0)));
+  }
+
+  public boolean isSuffixOf(TreeName tree)
+  {
+    if (names.size() > tree.names.size())
+    {
+      return false;
+    }
+    for (int i = 0; i < names.size(); i++)
+    {
+      if (!tree.names.get(i).equals(names.get(i)))
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public boolean equals(final Object obj)
+  {
+    if (this == obj)
+    {
+      return true;
+    }
+    else if (obj instanceof TreeName)
+    {
+      return s.equals(((TreeName) obj).s);
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return s.hashCode();
+  }
+
+  @Override
+  public String toString()
+  {
+    return s;
+  }
+}
