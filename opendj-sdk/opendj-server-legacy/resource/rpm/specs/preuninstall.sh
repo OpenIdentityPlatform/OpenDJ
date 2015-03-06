579c116bcf1545d08d78422424f5f55949eb9ca7
Gaetan Boismal <gaetan.boismal@forgerock.com>
2015-03-06 15:20:52 +0000

    OPENDJ-1772: Add new maven rpm package resources

---
+++ opendj-sdk/opendj-server-legacy/resource/rpm/specs/preuninstall.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at
+# trunk/opendj3/legal-notices/CDDLv1_0.txt
+# or http://forgerock.org/license/CDDLv1.0.html.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at
+# trunk/opendj3/legal-notices/CDDLv1_0.txt.  If applicable,
+# add the following below this CDDL HEADER, with the fields enclosed
+# by brackets "[]" replaced with your own identifying information:
+#      Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#      Copyright 2013-2015 ForgeRock AS
+
+# =================================
+# RPM Pre Uninstall Script (%preun)
+# =================================
+
+# If the first argument to %preun and %postun is 0, the action is
+#  uninstallation.
+# If the first argument to %preun and %postun is 1, the action is an upgrade.
+
+if [ "$1" == "0" ] ; then
+    echo "Pre Uninstall - uninstall"
+# Stops the service and delete it.
+    /etc/init.d/opendj stop >/dev/null 2>&1
+    /sbin/chkconfig --del opendj
+    # Unlink the symlink to the process ID.
+    test -h "/var/run/opendj.pid" && unlink /var/run/opendj.pid
+# Only if the instance has been configured
+    if [ -e "%{_prefix}"/config/buildinfo ] && [ "$(ls -A "%{_prefix}"/config/archived-configs)" ]
+    then
+	"%{_prefix}"/bin/./stop-ds
+    fi
+else if [ "$1" == "1" ] ; then
+    echo "Pre Uninstall - upgrade uninstall"
+    fi
+fi
