2cbef260cac1de98641ba39eb0611a909eefaf4a
Nicolas Capponi <nicolas.capponi@forgerock.com>
2015-05-22 09:26:19 +0000

    OPENDJ-2040 Provide annotations for tracking of server modifications during migration to SDK

---
+++ opendj-sdk/opendj-server-legacy/src/main/java/org/opends/server/util/ModifyOnceSDKSchemaIsUsed.java
@@ -0,0 +1,40 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at legal-notices/CDDLv1_0.txt
+ * or http://forgerock.org/license/CDDLv1.0.html.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at legal-notices/CDDLv1_0.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Copyright 2015 ForgeRock AS
+ */
+package org.opends.server.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Temporary annotation to remind that annotated code could be modified once the
+ * SDK schema is used in place of legacy server schema.
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface ModifyOnceSDKSchemaIsUsed
+{
+  /** Optional comment on modification. */
+  String value() default "";
+}
