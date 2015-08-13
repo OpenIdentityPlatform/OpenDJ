072c48855ee64b0e3daca42a281e278724b6424d
Fabio Pistolesi <fabio.pistolesi@forgerock.com>
2015-08-13 08:31:01 +0000

    Following Jean-Noel suggestion (thank you, btw), replace an expensive operation with a simpler one.

--- opendj-sdk/opendj-server-legacy/src/main/java/org/opends/server/backends/pluggable/ID2Entry.java
+++ opendj-sdk/opendj-server-legacy/src/main/java/org/opends/server/backends/pluggable/ID2Entry.java
@@ -235,7 +235,11 @@ class ID2Entry extends AbstractTree
   void open0(WriteableTransaction txn) throws StorageRuntimeException
   {
     // Make sure the tree is there and readable, even if the storage is READ_ONLY.
-    txn.getRecordCount(getName());
+    // Would be nice if there were a better way...
+    try (final Cursor<ByteString, ByteString> cursor = txn.openCursor(getName()))
+    {
+      cursor.next();
+    }
   }
 
   /**
