21a1bf623c6c4316a8160fb83a05d2adf125416e
Matthew Swift <matthew.swift@forgerock.com>
2015-10-12 09:54:34 +0200

    Minor fix: Tree should implement Comparable not AbstractTree

--- opendj-server-legacy/src/main/java/org/opends/server/backends/pluggable/AbstractTree.java
+++ opendj-server-legacy/src/main/java/org/opends/server/backends/pluggable/AbstractTree.java
@@ -36,7 +36,7 @@ import org.opends.server.backends.pluggable.spi.WriteableTransaction;
  * This class is a wrapper around the tree object and provides basic
  * read and write methods for entries.
  */
-abstract class AbstractTree implements Tree, Comparable<Tree>
+abstract class AbstractTree implements Tree
 {
   /** The name of the tree within the entryContainer. */
   private final TreeName name;
