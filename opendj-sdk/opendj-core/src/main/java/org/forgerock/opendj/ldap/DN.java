4d81d07ce1ed94a112ac294878ad8cf498534ec0
Matthew Swift <matthew.swift@forgerock.com>
2015-10-28 10:19:40 +0100

    Minor cleanup: Remove DN.normalizedToASCII() as it has nothing to do with DNs

--- opendj-sdk/opendj-core/src/main/java/org/forgerock/opendj/ldap/DN.java
+++ opendj-sdk/opendj-core/src/main/java/org/forgerock/opendj/ldap/DN.java
@@ -912,16 +912,6 @@ public final class DN implements Iterable<RDN>, Comparable<DN> {
     }
 
     /**
-     * Returns an ASCII printable string representation.
-     *
-     * @param seq a normalized DN
-     * @return an ASCII printable string representation
-     */
-    public static String normalizedToASCII(ByteSequence seq) {
-        return seq.toByteString().toASCIIString();
-    }
-
-    /**
      * Retrieves a normalized string representation of this DN.
      * <p>
      * This representation is safe to use in an URL or in a file name.
