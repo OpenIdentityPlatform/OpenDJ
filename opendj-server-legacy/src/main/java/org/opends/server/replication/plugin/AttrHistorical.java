3bea3598871b4dbac200f0eec65e0bc46a71f6ec
Gaetan Boismal <gaetan.boismal@forgerock.com>
2016-02-23 16:36:38 +0100

    Fix copyright headers

--- opendj-server-legacy/src/main/java/org/opends/server/replication/plugin/AttrHistorical.java
+++ opendj-server-legacy/src/main/java/org/opends/server/replication/plugin/AttrHistorical.java
@@ -22,8 +22,7 @@
  *
  *
  *      Copyright 2006-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2013-2014 ForgeRock, AS.
- *      Portions Copyright 2015-2016 ForgeRock AS.
+ *      Portions Copyright 2013-2016 ForgeRock AS.
  */
 package org.opends.server.replication.plugin;
 
