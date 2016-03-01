OpenIdentityPlatform/OpenDJ

commit df993e4e7a2b5af0c8e0907a80e1a4cef10ee56d

OPENDJSDK-42 cli arguments fluent builder
PR-278 sdk PR-45
Consequences of changes done in sdk PR-45
All Arguments objects are now built with fluent builder.
* An argument has a mandatory long identifier
    * The name attribute has been merged with long identifier and removed
    * Remove useless null checks on argument long identifiers
* Now all argument can be set in a property file.
  The argument property name is always the same as the argument long identifier (i.e can not be set any more)
* Remove the possiblity to set if an argument needs a value.
* For IntegerArgument default lower bound is Integer.MIN_VALUE
and the default upper bound is Integer.MAX_VALUE
* Once every needed attributes are contributed, the argument can be
created by the builder or added to an argument parser or a subcommand.
This commit also includes additional related cleanup.

opendj-server-legacy/src/main/java/org/opends/guitools/uninstaller/UninstallCliHelper.java
@@ -22,7 +22,7 @@
  *
  *
  *      Copyright 2006-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2014-2015 ForgeRock AS
+ *      Portions Copyright 2014-2016 ForgeRock AS
  */
 package org.opends.guitools.uninstaller;
 
@@ -1266,7 +1266,7 @@ public class UninstallCliHelper extends ConsoleApplication {
         {
           println();
           printErrorMessage(ERR_UNINSTALL_ERROR_UPDATING_REMOTE_FORCE.get(
-              "--"+parser.getSecureArgsList().adminUidArg.getLongIdentifier(),
+              "--"+parser.getSecureArgsList().adminUidHiddenArg.getLongIdentifier(),
               "--"+OPTION_LONG_BINDPWD,
               "--"+OPTION_LONG_BINDPWD_FILE,
               exceptionMsg));
@@ -1277,7 +1277,7 @@ public class UninstallCliHelper extends ConsoleApplication {
           throw new UserDataException(null,
               ERR_UNINSTALL_ERROR_UPDATING_REMOTE_NO_FORCE.get(
                   "--"+
-                  parser.getSecureArgsList().adminUidArg.getLongIdentifier(),
+                  parser.getSecureArgsList().adminUidHiddenArg.getLongIdentifier(),
                   "--"+OPTION_LONG_BINDPWD,
                   "--"+OPTION_LONG_BINDPWD_FILE,
                   "--"+parser.forceOnErrorArg.getLongIdentifier(),
@@ -1431,7 +1431,7 @@ public class UninstallCliHelper extends ConsoleApplication {
           LocalizableMessage msg =
             ERR_UNINSTALL_ERROR_UPDATING_REMOTE_NO_FORCE.get(
               "--"+
-              parser.getSecureArgsList().adminUidArg.getLongIdentifier(),
+              parser.getSecureArgsList().adminUidHiddenArg.getLongIdentifier(),
               "--"+OPTION_LONG_BINDPWD,
               "--"+OPTION_LONG_BINDPWD_FILE,
               "--"+parser.forceOnErrorArg.getLongIdentifier(),

opendj-server-legacy/src/main/java/org/opends/guitools/uninstaller/Uninstaller.java
@@ -22,7 +22,7 @@
  *
  *
  *      Copyright 2006-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2014-2015 ForgeRock AS
+ *      Portions Copyright 2014-2016 ForgeRock AS
  */
 package org.opends.guitools.uninstaller;
 
@@ -1962,7 +1962,7 @@ public class Uninstaller extends GuiApplication implements CliApplication {
           LocalizableMessage msg =
             ERR_UNINSTALL_ERROR_UPDATING_REMOTE_NO_FORCE.get(
               "--"+
-              parser.getSecureArgsList().adminUidArg.getLongIdentifier(),
+              parser.getSecureArgsList().adminUidHiddenArg.getLongIdentifier(),
               "--"+OPTION_LONG_BINDPWD,
               "--"+OPTION_LONG_BINDPWD_FILE,
               "--"+parser.forceOnErrorArg.getLongIdentifier(),

opendj-server-legacy/src/main/java/org/opends/guitools/uninstaller/UninstallerArgumentParser.java
@@ -22,7 +22,7 @@
  *
  *
  *      Copyright 2008-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2014-2015 ForgeRock AS.
+ *      Portions Copyright 2014-2016 ForgeRock AS.
  */
 package org.opends.guitools.uninstaller;
 
@@ -64,6 +64,7 @@ public class UninstallerArgumentParser extends SecureConnectionCliParser
   private BooleanArgument removeLDIFFilesArg;
 
   private StringArgument referencedHostNameArg;
+  private StringArgument adminUidArg;
 
   /** This CLI is always using the administration connector with SSL. */
   private final boolean alwaysSSL = true;
@@ -103,98 +104,92 @@ public class UninstallerArgumentParser extends SecureConnectionCliParser
   throws ArgumentException
   {
     LinkedHashSet<Argument> args = new LinkedHashSet<>();
+    adminUidArg = CommonArguments.getAdminUid(INFO_DESCRIPTION_ADMIN_UID.get());
     cliArg = CommonArguments.getCLI();
     args.add(cliArg);
 
-    removeAllArg = new BooleanArgument(
-        "remove-all",
-        'a',
-        "remove-all",
-        INFO_UNINSTALLDS_DESCRIPTION_REMOVE_ALL.get()
-        );
+    removeAllArg =
+            BooleanArgument.builder("remove-all")
+                    .shortIdentifier('a')
+                    .description(INFO_UNINSTALLDS_DESCRIPTION_REMOVE_ALL.get())
+                    .buildArgument();
     args.add(removeAllArg);
-    removeServerLibrariesArg = new BooleanArgument(
-        "server-libraries",
-        'l',
-        "server-libraries",
-        INFO_UNINSTALLDS_DESCRIPTION_REMOVE_SERVER_LIBRARIES.get()
-        );
+
+    removeServerLibrariesArg =
+            BooleanArgument.builder("server-libraries")
+                    .shortIdentifier('l')
+                    .description(INFO_UNINSTALLDS_DESCRIPTION_REMOVE_SERVER_LIBRARIES.get())
+                    .buildArgument();
     args.add(removeServerLibrariesArg);
-    removeDatabasesArg = new BooleanArgument(
-        "databases",
-        'd',
-        "databases",
-        INFO_UNINSTALLDS_DESCRIPTION_REMOVE_DATABASES.get()
-        );
+
+    removeDatabasesArg =
+            BooleanArgument.builder("databases")
+                    .shortIdentifier('d')
+                    .description(INFO_UNINSTALLDS_DESCRIPTION_REMOVE_DATABASES.get())
+                    .buildArgument();
     args.add(removeDatabasesArg);
-    removeLogFilesArg = new BooleanArgument(
-        "log-files",
-        'L',
-        "log-files",
-        INFO_UNINSTALLDS_DESCRIPTION_REMOVE_LOG_FILES.get()
-        );
+
+    removeLogFilesArg =
+            BooleanArgument.builder("log-files")
+                    .shortIdentifier('L')
+                    .description(INFO_UNINSTALLDS_DESCRIPTION_REMOVE_LOG_FILES.get())
+                    .buildArgument();
     args.add(removeLogFilesArg);
-    removeConfigurationFilesArg = new BooleanArgument(
-        "configuration-files",
-        'c',
-        "configuration-files",
-        INFO_UNINSTALLDS_DESCRIPTION_REMOVE_CONFIGURATION_FILES.get()
-        );
+
+    removeConfigurationFilesArg =
+            BooleanArgument.builder("configuration-files")
+                    .shortIdentifier('c')
+                    .description(INFO_UNINSTALLDS_DESCRIPTION_REMOVE_CONFIGURATION_FILES.get())
+                    .buildArgument();
     args.add(removeConfigurationFilesArg);
-    removeBackupFilesArg = new BooleanArgument(
-        "backup-files",
-        'b',
-        "backup-files",
-        INFO_UNINSTALLDS_DESCRIPTION_REMOVE_BACKUP_FILES.get()
-        );
+
+    removeBackupFilesArg =
+            BooleanArgument.builder("backup-files")
+                    .shortIdentifier('b')
+                    .description(INFO_UNINSTALLDS_DESCRIPTION_REMOVE_BACKUP_FILES.get())
+                    .buildArgument();
     args.add(removeBackupFilesArg);
-    removeLDIFFilesArg = new BooleanArgument(
-        "ldif-files",
-        'e',
-        "ldif-files",
-        INFO_UNINSTALLDS_DESCRIPTION_REMOVE_LDIF_FILES.get()
-        );
+
+    removeLDIFFilesArg =
+            BooleanArgument.builder("ldif-files")
+                    .shortIdentifier('e')
+                    .description(INFO_UNINSTALLDS_DESCRIPTION_REMOVE_LDIF_FILES.get())
+                    .buildArgument();
     args.add(removeLDIFFilesArg);
 
     noPromptArg = CommonArguments.getNoPrompt();
     args.add(noPromptArg);
 
-    forceOnErrorArg = new BooleanArgument(
-        "forceOnError",
-        'f',
-        "forceOnError",
-        INFO_UNINSTALLDS_DESCRIPTION_FORCE.get(
-            "--"+noPromptArg.getLongIdentifier()));
+    forceOnErrorArg =
+            BooleanArgument.builder("forceOnError")
+                    .shortIdentifier('f')
+                    .description(INFO_UNINSTALLDS_DESCRIPTION_FORCE.get("--" + noPromptArg.getLongIdentifier()))
+                    .buildArgument();
     args.add(forceOnErrorArg);
 
     quietArg = CommonArguments.getQuiet();
     args.add(quietArg);
 
-    for (Argument arg : args)
-    {
-      arg.setPropertyName(arg.getLongIdentifier());
-    }
-
     ArrayList<Argument> defaultArgs = new ArrayList<>(createGlobalArguments(outStream, alwaysSSL));
     int index = defaultArgs.indexOf(secureArgsList.bindDnArg);
     if (index != -1)
     {
-      defaultArgs.add(index, secureArgsList.adminUidArg);
+      defaultArgs.add(index, adminUidArg);
       defaultArgs.remove(secureArgsList.bindDnArg);
     }
     else
     {
-      defaultArgs.add(secureArgsList.adminUidArg);
+      defaultArgs.add(adminUidArg);
     }
-    secureArgsList.adminUidArg.setHidden(false);
     defaultArgs.remove(secureArgsList.hostNameArg);
     defaultArgs.remove(secureArgsList.portArg);
-    referencedHostNameArg = new StringArgument("referencedHostName",
-        OPTION_SHORT_HOST,
-        OPTION_LONG_REFERENCED_HOST_NAME, false, false, true,
-        INFO_HOST_PLACEHOLDER.get(),
-        UserData.getDefaultHostName(), OPTION_LONG_REFERENCED_HOST_NAME,
-        INFO_DESCRIPTION_REFERENCED_HOST.get());
+    referencedHostNameArg =
+            StringArgument.builder(OPTION_LONG_REFERENCED_HOST_NAME)
+                    .shortIdentifier(OPTION_SHORT_HOST)
+                    .description(INFO_DESCRIPTION_REFERENCED_HOST.get())
+                    .defaultValue(UserData.getDefaultHostName())
+                    .valuePlaceholder(INFO_HOST_PLACEHOLDER.get())
+                    .buildArgument();
     defaultArgs.add(referencedHostNameArg);
 
     args.addAll(defaultArgs);
@@ -329,7 +324,7 @@ public class UninstallerArgumentParser extends SecureConnectionCliParser
    */
   public String getDefaultAdministratorUID()
   {
-    return secureArgsList.adminUidArg.getDefaultValue();
+    return adminUidArg.getDefaultValue();
   }
 
   /**

opendj-server-legacy/src/main/java/org/opends/server/admin/client/cli/SecureConnectionCliArgs.java
@@ -22,11 +22,10 @@
  *
  *
  *      Copyright 2007-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2011-2015 ForgeRock AS
+ *      Portions Copyright 2011-2016 ForgeRock AS
  */
 package org.opends.server.admin.client.cli;
 
-import static com.forgerock.opendj.cli.ArgumentConstants.*;
 import static com.forgerock.opendj.cli.CliMessages.*;
 import static com.forgerock.opendj.cli.ReturnCode.*;
 import static com.forgerock.opendj.cli.Utils.*;
@@ -44,6 +43,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.forgerock.opendj.cli.ArgumentParser;
 import org.forgerock.i18n.LocalizableMessage;
 import org.forgerock.i18n.LocalizableMessageBuilder;
 import org.forgerock.i18n.LocalizableMessageDescriptor.Arg1;
@@ -84,7 +84,7 @@ public final class SecureConnectionCliArgs
   /** The 'bindDN' global argument. */
   public StringArgument bindDnArg;
   /** The 'adminUID' global argument. */
-  public StringArgument adminUidArg;
+  public StringArgument adminUidHiddenArg;
   /** The 'bindPasswordFile' global argument. */
   public FileBasedArgument bindPasswordFileArg;
   /** The 'bindPassword' global argument. */
@@ -166,11 +166,11 @@ public final class SecureConnectionCliArgs
    */
   public String getAdministratorUID()
   {
-    if (adminUidArg.isPresent())
+    if (adminUidHiddenArg.isPresent())
     {
-      return adminUidArg.getValue();
+      return adminUidHiddenArg.getValue();
     }
-    return adminUidArg.getDefaultValue();
+    return adminUidHiddenArg.getDefaultValue();
   }
 
   /**
@@ -182,7 +182,7 @@ public final class SecureConnectionCliArgs
    */
   public boolean useAdminUID()
   {
-    return !adminUidArg.isHidden();
+    return !adminUidHiddenArg.isHidden();
   }
 
   /**
@@ -229,32 +229,17 @@ public final class SecureConnectionCliArgs
       argList.add(useStartTLSArg);
     }
 
-    String defaultHostName;
-    try
-    {
-      defaultHostName = InetAddress.getLocalHost().getHostName();
-    }
-    catch (Exception e)
-    {
-      defaultHostName = "Unknown (" + e + ")";
-    }
-    hostNameArg = CommonArguments.getHostName(defaultHostName);
+    hostNameArg = CommonArguments.getHostName(getDefaultHostName());
     argList.add(hostNameArg);
 
-    portArg = CommonArguments.getPort(AdministrationConnector.DEFAULT_ADMINISTRATION_CONNECTOR_PORT,
-                                      alwaysSSL ? INFO_DESCRIPTION_ADMIN_PORT.get() : INFO_DESCRIPTION_PORT.get());
+    portArg = createPortArgument(AdministrationConnector.DEFAULT_ADMINISTRATION_CONNECTOR_PORT);
     argList.add(portArg);
 
     bindDnArg = CommonArguments.getBindDN(CliConstants.DEFAULT_ROOT_USER_DN);
     argList.add(bindDnArg);
 
-    // It is up to the classes that required admin UID to make this argument
-    // visible and add it.
-    adminUidArg = new StringArgument("adminUID", 'I', OPTION_LONG_ADMIN_UID, false, false, true,
-                                     INFO_ADMINUID_PLACEHOLDER.get(), CliConstants.GLOBAL_ADMIN_UID,
-                                     null, INFO_DESCRIPTION_ADMIN_UID.get());
-    adminUidArg.setPropertyName(OPTION_LONG_ADMIN_UID);
-    adminUidArg.setHidden(true);
+    // Classes that required admin UID to be not hidden must use CommonsArguments.getAdminUid().
+    adminUidHiddenArg = CommonArguments.getAdminUidHidden(INFO_DESCRIPTION_ADMIN_UID.get());
 
     bindPasswordArg = CommonArguments.getBindPassword();
     argList.add(bindPasswordArg);
@@ -290,7 +275,6 @@ public final class SecureConnectionCliArgs
     argList.add(certNicknameArg);
 
     connectTimeoutArg = CommonArguments.getConnectTimeOut();
-    connectTimeoutArg.setHidden(false);
     argList.add(connectTimeoutArg);
 
     return argList;
@@ -309,6 +293,23 @@ public final class SecureConnectionCliArgs
       return hostNameArg.getValue();
     }
     return hostNameArg.getDefaultValue();
+  }
+
+  /**
+   * Returns the current hostname.
+   *
+   * If the hostname resolution fails, this method returns {@literal "localhost"}.
+   * @return the current hostname
+     */
+  public String getDefaultHostName() {
+    try
+    {
+      return InetAddress.getLocalHost().getHostName();
+    }
+    catch (Exception e)
+    {
+      return "localhost";
+    }
   }
 
   /**
@@ -636,17 +637,27 @@ public final class SecureConnectionCliArgs
    * Updates the default values of the port and the trust store with what is
    * read in the configuration.
    *
-   * @throws ConfigException
-   *           if there is an error reading the configuration.
+   * @param parser
+   *        The argument parser where the secure connection arguments were added.
    */
-  public void initArgumentsWithConfiguration() throws ConfigException
-  {
-    portArg.setDefaultValue(String.valueOf(getPortFromConfig()));
-
-    String truststoreFileAbsolute = getTruststoreFileFromConfig();
-    if (truststoreFileAbsolute != null)
+  public void initArgumentsWithConfiguration(final ArgumentParser parser) {
+    try
     {
-      trustStorePathArg.setDefaultValue(truststoreFileAbsolute);
+      portArg = createPortArgument(getPortFromConfig());
+      trustStorePathArg = CommonArguments.getTrustStorePathArgument(getTruststoreFileFromConfig());
+      parser.replaceArgument(portArg);
+      parser.replaceArgument(trustStorePathArg);
     }
+    catch (ConfigException | ArgumentException e)
+    {
+      logger.error(LocalizableMessage.raw(
+              "Internal error while reading arguments of this program from configuration"), e);
+    }
+  }
+
+  private IntegerArgument createPortArgument(final int defaultValue) throws ArgumentException
+  {
+    return CommonArguments.getPort(
+            defaultValue, alwaysSSL ? INFO_DESCRIPTION_ADMIN_PORT.get() : INFO_DESCRIPTION_PORT.get());
   }
 }

opendj-server-legacy/src/main/java/org/opends/server/util/cli/LDAPConnectionConsoleInteraction.java
@@ -22,7 +22,7 @@
  *
  *
  *      Copyright 2008-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2011-2015 ForgeRock AS
+ *      Portions Copyright 2011-2016 ForgeRock AS
  */
 package org.opends.server.util.cli;
 
@@ -630,9 +630,9 @@ public class LDAPConnectionConsoleInteraction
 
     // Get the LDAP bind credentials.
     state.bindDN = secureArgsList.bindDnArg.getValue();
-    state.adminUID= secureArgsList.adminUidArg.getValue();
+    state.adminUID= secureArgsList.adminUidHiddenArg.getValue();
     final boolean useAdmin = secureArgsList.useAdminUID();
-    if (useAdmin && secureArgsList.adminUidArg.isPresent())
+    if (useAdmin && secureArgsList.adminUidHiddenArg.isPresent())
     {
       state.providedAdminUID = state.adminUID;
     }
@@ -737,9 +737,9 @@ public class LDAPConnectionConsoleInteraction
         }
         if (addAdmin)
         {
-          copySecureArgsList.adminUidArg.clearValues();
-          copySecureArgsList.adminUidArg.addValue(getAdministratorUID());
-          commandBuilder.addArgument(copySecureArgsList.adminUidArg);
+          copySecureArgsList.adminUidHiddenArg.clearValues();
+          copySecureArgsList.adminUidHiddenArg.addValue(getAdministratorUID());
+          commandBuilder.addArgument(copySecureArgsList.adminUidHiddenArg);
         }
         else if (addBindDN)
         {
@@ -750,9 +750,9 @@ public class LDAPConnectionConsoleInteraction
       }
       else if (useAdmin)
       {
-        copySecureArgsList.adminUidArg.clearValues();
-        copySecureArgsList.adminUidArg.addValue(getAdministratorUID());
-        commandBuilder.addArgument(copySecureArgsList.adminUidArg);
+        copySecureArgsList.adminUidHiddenArg.clearValues();
+        copySecureArgsList.adminUidHiddenArg.addValue(getAdministratorUID());
+        commandBuilder.addArgument(copySecureArgsList.adminUidHiddenArg);
       }
       else
       {
@@ -1978,8 +1978,8 @@ public class LDAPConnectionConsoleInteraction
     secureArgsList.useStartTLSArg.setPresent(state.useStartTLS);
     if (adminUid != null)
     {
-      secureArgsList.adminUidArg.addValue(adminUid);
-      secureArgsList.adminUidArg.setPresent(true);
+      secureArgsList.adminUidHiddenArg.addValue(adminUid);
+      secureArgsList.adminUidHiddenArg.setPresent(true);
     }
     if (bindDn != null)
     {
@@ -2025,8 +2025,8 @@ public class LDAPConnectionConsoleInteraction
     secureArgsList.bindPasswordFileArg.getNameToValueMap().clear();
     secureArgsList.bindPasswordFileArg.setPresent(false);
     state.bindPassword = null;
-    secureArgsList.adminUidArg.clearValues();
-    secureArgsList.adminUidArg.setPresent(false);
+    secureArgsList.adminUidHiddenArg.clearValues();
+    secureArgsList.adminUidHiddenArg.setPresent(false);
   }
 
   private void initializeTrustManager() throws ArgumentException

commit c465af5b01eb04b869326e58bdb3495830dcbb8f

Fix bug in dsreplication and uninstall cli
Introduced by df993e4 -- OPENDJSDK-42 cli arguments fluent builder
The admin UID argument has been detached by error from the SecureConnectionCliArgs
in ReplicationCliArgumentParser and UninstallerArgumentParser.
This commit fixes the problem.

opendj-server-legacy/src/main/java/org/opends/guitools/uninstaller/UninstallCliHelper.java
@@ -1256,7 +1256,7 @@ public class UninstallCliHelper extends ConsoleApplication {
         {
           println();
           printErrorMessage(ERR_UNINSTALL_ERROR_UPDATING_REMOTE_FORCE.get(
-              "--"+parser.getSecureArgsList().adminUidHiddenArg.getLongIdentifier(),
+              "--"+parser.getSecureArgsList().adminUidArg.getLongIdentifier(),
               "--"+OPTION_LONG_BINDPWD,
               "--"+OPTION_LONG_BINDPWD_FILE,
               exceptionMsg));
@@ -1267,7 +1267,7 @@ public class UninstallCliHelper extends ConsoleApplication {
           throw new UserDataException(null,
               ERR_UNINSTALL_ERROR_UPDATING_REMOTE_NO_FORCE.get(
                   "--"+
-                  parser.getSecureArgsList().adminUidHiddenArg.getLongIdentifier(),
+                  parser.getSecureArgsList().adminUidArg.getLongIdentifier(),
                   "--"+OPTION_LONG_BINDPWD,
                   "--"+OPTION_LONG_BINDPWD_FILE,
                   "--"+parser.forceOnErrorArg.getLongIdentifier(),
@@ -1421,7 +1421,7 @@ public class UninstallCliHelper extends ConsoleApplication {
           LocalizableMessage msg =
             ERR_UNINSTALL_ERROR_UPDATING_REMOTE_NO_FORCE.get(
               "--"+
-              parser.getSecureArgsList().adminUidHiddenArg.getLongIdentifier(),
+              parser.getSecureArgsList().adminUidArg.getLongIdentifier(),
               "--"+OPTION_LONG_BINDPWD,
               "--"+OPTION_LONG_BINDPWD_FILE,
               "--"+parser.forceOnErrorArg.getLongIdentifier(),

opendj-server-legacy/src/main/java/org/opends/guitools/uninstaller/Uninstaller.java
@@ -1952,7 +1952,7 @@ public class Uninstaller extends GuiApplication implements CliApplication {
           LocalizableMessage msg =
             ERR_UNINSTALL_ERROR_UPDATING_REMOTE_NO_FORCE.get(
               "--"+
-              parser.getSecureArgsList().adminUidHiddenArg.getLongIdentifier(),
+              parser.getSecureArgsList().adminUidArg.getLongIdentifier(),
               "--"+OPTION_LONG_BINDPWD,
               "--"+OPTION_LONG_BINDPWD_FILE,
               "--"+parser.forceOnErrorArg.getLongIdentifier(),

opendj-server-legacy/src/main/java/org/opends/guitools/uninstaller/UninstallerArgumentParser.java
@@ -55,7 +55,6 @@ public class UninstallerArgumentParser extends SecureConnectionCliParser
   private BooleanArgument removeLDIFFilesArg;
 
   private StringArgument referencedHostNameArg;
-  private StringArgument adminUidArg;
 
   /** This CLI is always using the administration connector with SSL. */
   private final boolean alwaysSSL = true;
@@ -95,7 +94,6 @@ public class UninstallerArgumentParser extends SecureConnectionCliParser
   throws ArgumentException
   {
     LinkedHashSet<Argument> args = new LinkedHashSet<>();
-    adminUidArg = adminUid(INFO_DESCRIPTION_ADMIN_UID.get());
     cliArg = cliArgument();
     args.add(cliArg);
 
@@ -162,15 +160,16 @@ public class UninstallerArgumentParser extends SecureConnectionCliParser
     args.add(quietArg);
 
     ArrayList<Argument> defaultArgs = new ArrayList<>(createGlobalArguments(outStream, alwaysSSL));
+    secureArgsList.createVisibleAdminUidArgument(INFO_DESCRIPTION_ADMIN_UID.get());
     int index = defaultArgs.indexOf(secureArgsList.bindDnArg);
     if (index != -1)
     {
-      defaultArgs.add(index, adminUidArg);
+      defaultArgs.add(index, secureArgsList.adminUidArg);
       defaultArgs.remove(secureArgsList.bindDnArg);
     }
     else
     {
-      defaultArgs.add(adminUidArg);
+      defaultArgs.add(secureArgsList.adminUidArg);
     }
     defaultArgs.remove(secureArgsList.hostNameArg);
     defaultArgs.remove(secureArgsList.portArg);
@@ -315,7 +314,7 @@ public class UninstallerArgumentParser extends SecureConnectionCliParser
    */
   public String getDefaultAdministratorUID()
   {
-    return adminUidArg.getDefaultValue();
+    return secureArgsList.adminUidArg.getDefaultValue();
   }
 
   /**

opendj-server-legacy/src/main/java/org/opends/server/admin/client/cli/SecureConnectionCliArgs.java
@@ -75,7 +75,7 @@ public final class SecureConnectionCliArgs
   /** The 'bindDN' global argument. */
   public StringArgument bindDnArg;
   /** The 'adminUID' global argument. */
-  public StringArgument adminUidHiddenArg;
+  public StringArgument adminUidArg;
   /** The 'bindPasswordFile' global argument. */
   public FileBasedArgument bindPasswordFileArg;
   /** The 'bindPassword' global argument. */
@@ -157,11 +157,11 @@ public final class SecureConnectionCliArgs
    */
   public String getAdministratorUID()
   {
-    if (adminUidHiddenArg.isPresent())
+    if (adminUidArg.isPresent())
     {
-      return adminUidHiddenArg.getValue();
+      return adminUidArg.getValue();
     }
-    return adminUidHiddenArg.getDefaultValue();
+    return adminUidArg.getDefaultValue();
   }
 
   /**
@@ -173,7 +173,7 @@ public final class SecureConnectionCliArgs
    */
   public boolean useAdminUID()
   {
-    return !adminUidHiddenArg.isHidden();
+    return !adminUidArg.isHidden();
   }
 
   /**
@@ -230,7 +230,7 @@ public final class SecureConnectionCliArgs
     argList.add(bindDnArg);
 
     // Classes that required admin UID to be not hidden must use CommonsArguments.adminUid().
-    adminUidHiddenArg = adminUidHiddenArgument(INFO_DESCRIPTION_ADMIN_UID.get());
+    adminUidArg = adminUidHiddenArgument(INFO_DESCRIPTION_ADMIN_UID.get());
 
     bindPasswordArg = bindPasswordArgument();
     argList.add(bindPasswordArg);
@@ -643,6 +643,24 @@ public final class SecureConnectionCliArgs
     {
       logger.error(LocalizableMessage.raw(
               "Internal error while reading arguments of this program from configuration"), e);
+    }
+  }
+
+  /**
+   * Replace the admin UID argument by a non hidden one.
+   *
+   * @param description
+   *         The localized description for the non hidden admin UID argument.
+   */
+  public void createVisibleAdminUidArgument(final LocalizableMessage description)
+  {
+    try
+    {
+      this.adminUidArg = adminUid(description);
+    }
+    catch (ArgumentException e)
+    {
+      // Will never append.
     }
   }
 

opendj-server-legacy/src/main/java/org/opends/server/tools/dsreplication/ReplicationCliArgumentParser.java
@@ -192,9 +192,6 @@ public class ReplicationCliArgumentParser extends SecureConnectionCliParser
   /** the 'change-number' argument for task reset-changenumber. */
   IntegerArgument resetChangeNumber;
 
-  /** The "adminUid" non hidden argument */
-  StringArgument adminUidArg;
-
   /** The text of the enable replication subcommand. */
   static final String ENABLE_REPLICATION_SUBCMD_NAME = "enable";
   /** The text of the disable replication subcommand. */
@@ -422,9 +419,9 @@ public class ReplicationCliArgumentParser extends SecureConnectionCliParser
                     .buildArgument();
     defaultArgs.add(index++, baseDNsArg);
 
-    adminUidArg = adminUid(
-            INFO_DESCRIPTION_REPLICATION_ADMIN_UID.get(ENABLE_REPLICATION_SUBCMD_NAME));
-    defaultArgs.add(index++, adminUidArg);
+    secureArgsList.createVisibleAdminUidArgument(
+        INFO_DESCRIPTION_REPLICATION_ADMIN_UID.get(ENABLE_REPLICATION_SUBCMD_NAME));
+    defaultArgs.add(index++, secureArgsList.adminUidArg);
 
     secureArgsList.bindPasswordArg =
             StringArgument.builder(OPTION_LONG_ADMIN_PWD)
@@ -965,7 +962,7 @@ public class ReplicationCliArgumentParser extends SecureConnectionCliParser
    */
   StringArgument getAdminUidArg()
   {
-    return adminUidArg;
+    return secureArgsList.adminUidArg;
   }
 
   /**

opendj-server-legacy/src/main/java/org/opends/server/util/cli/LDAPConnectionConsoleInteraction.java
@@ -619,9 +619,9 @@ public class LDAPConnectionConsoleInteraction
 
     // Get the LDAP bind credentials.
     state.bindDN = secureArgsList.bindDnArg.getValue();
-    state.adminUID= secureArgsList.adminUidHiddenArg.getValue();
+    state.adminUID= secureArgsList.adminUidArg.getValue();
     final boolean useAdmin = secureArgsList.useAdminUID();
-    if (useAdmin && secureArgsList.adminUidHiddenArg.isPresent())
+    if (useAdmin && secureArgsList.adminUidArg.isPresent())
     {
       state.providedAdminUID = state.adminUID;
     }
@@ -726,9 +726,9 @@ public class LDAPConnectionConsoleInteraction
         }
         if (addAdmin)
         {
-          copySecureArgsList.adminUidHiddenArg.clearValues();
-          copySecureArgsList.adminUidHiddenArg.addValue(getAdministratorUID());
-          commandBuilder.addArgument(copySecureArgsList.adminUidHiddenArg);
+          copySecureArgsList.adminUidArg.clearValues();
+          copySecureArgsList.adminUidArg.addValue(getAdministratorUID());
+          commandBuilder.addArgument(copySecureArgsList.adminUidArg);
         }
         else if (addBindDN)
         {
@@ -739,9 +739,9 @@ public class LDAPConnectionConsoleInteraction
       }
       else if (useAdmin)
       {
-        copySecureArgsList.adminUidHiddenArg.clearValues();
-        copySecureArgsList.adminUidHiddenArg.addValue(getAdministratorUID());
-        commandBuilder.addArgument(copySecureArgsList.adminUidHiddenArg);
+        copySecureArgsList.adminUidArg.clearValues();
+        copySecureArgsList.adminUidArg.addValue(getAdministratorUID());
+        commandBuilder.addArgument(copySecureArgsList.adminUidArg);
       }
       else
       {
@@ -1967,8 +1967,8 @@ public class LDAPConnectionConsoleInteraction
     secureArgsList.useStartTLSArg.setPresent(state.useStartTLS);
     if (adminUid != null)
     {
-      secureArgsList.adminUidHiddenArg.addValue(adminUid);
-      secureArgsList.adminUidHiddenArg.setPresent(true);
+      secureArgsList.adminUidArg.addValue(adminUid);
+      secureArgsList.adminUidArg.setPresent(true);
     }
     if (bindDn != null)
     {
@@ -2014,8 +2014,8 @@ public class LDAPConnectionConsoleInteraction
     secureArgsList.bindPasswordFileArg.getNameToValueMap().clear();
     secureArgsList.bindPasswordFileArg.setPresent(false);
     state.bindPassword = null;
-    secureArgsList.adminUidHiddenArg.clearValues();
-    secureArgsList.adminUidHiddenArg.setPresent(false);
+    secureArgsList.adminUidArg.clearValues();
+    secureArgsList.adminUidArg.setPresent(false);
   }
 
   private void initializeTrustManager() throws ArgumentException