adc54476d751017e605fd0581f8918b2a20abd19
Fabio Pistolesi <fabio.pistolesi@forgerock.com>
2016-05-12 09:40:54 +0200

    Fix Checkstyle

--- opendj-core/src/main/java/org/forgerock/opendj/ldap/SortKey.java
+++ opendj-core/src/main/java/org/forgerock/opendj/ldap/SortKey.java
@@ -534,8 +534,7 @@ public final class SortKey {
             && equalsIgnoreCase(orderingMatchingRule, s.orderingMatchingRule);
     }
 
-    private boolean equalsIgnoreCase(String s1, String s2)
-    {
+    private boolean equalsIgnoreCase(String s1, String s2) {
         return s1 == null ? s2 == null : s1.equalsIgnoreCase(s2);
     }
 
