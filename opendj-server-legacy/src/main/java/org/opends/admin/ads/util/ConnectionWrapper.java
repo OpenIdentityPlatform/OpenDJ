OpenIdentityPlatform/OpenDJ

commit f3f9f37cd05c81867e2852746b169132d8f1b598

Partial OPENDJ-2625 Convert all code that uses JNDI to use the SDK instead
Passed ConnectionWrapper down several method calls.
Replaced uses of InitialLdapContext with ConnectionWrapper.

opendj-server-legacy/src/main/java/org/opends/admin/ads/util/ConnectionWrapper.java
@@ -69,6 +69,7 @@ public class ConnectionWrapper implements Closeable
   private final int connectTimeout;
   private final TrustManager trustManager;
   private final KeyManager keyManager;
+  private Type connectionType;
 
   /**
    * Creates a connection wrapper.
@@ -89,12 +90,21 @@ public class ConnectionWrapper implements Closeable
    *           If an error occurs
    */
   public ConnectionWrapper(String ldapUrl, Type connectionType, String bindDn, String bindPwd, int connectTimeout,
-      ApplicationTrustManager trustManager) throws NamingException
+      TrustManager trustManager) throws NamingException
   {
-    this(toHostPort(ldapUrl), connectionType, bindDn, bindPwd, connectTimeout, trustManager);
+    this(toHostPort(ldapUrl), connectionType, bindDn, bindPwd, connectTimeout, trustManager, null);
   }
 
-  private static HostPort toHostPort(String ldapUrl) throws NamingException
+  /**
+   * Converts an ldapUrl to a HostPort.
+   *
+   * @param ldapUrl
+   *          the ldapUrl to convert
+   * @return the host and port extracted from the ldapUrl
+   * @throws NamingException
+   *           if the ldapUrl is not a valid URL
+   */
+  public static HostPort toHostPort(String ldapUrl) throws NamingException
   {
     try
     {
@@ -132,6 +142,20 @@ public class ConnectionWrapper implements Closeable
   }
 
   /**
+   * Creates a connection wrapper by copying the provided one.
+   *
+   * @param other
+   *          the {@link ConnectionWrapper} to copy
+   * @throws NamingException
+   *           If an error occurs
+   */
+  public ConnectionWrapper(ConnectionWrapper other) throws NamingException
+  {
+    this(other.hostPort, other.connectionType, other.bindDn.toString(), other.bindPwd, other.connectTimeout,
+        other.trustManager, other.keyManager);
+  }
+
+  /**
    * Creates a connection wrapper.
    *
    * @param hostPort
@@ -155,6 +179,7 @@ public class ConnectionWrapper implements Closeable
       int connectTimeout, TrustManager trustManager, KeyManager keyManager) throws NamingException
   {
     this.hostPort = hostPort;
+    this.connectionType = connectionType;
     this.bindDn = DN.valueOf(bindDn);
     this.bindPwd = bindPwd;
     this.connectTimeout = connectTimeout;
@@ -162,7 +187,7 @@ public class ConnectionWrapper implements Closeable
     this.keyManager = keyManager;
 
     final Options options = toOptions(connectionType, bindDn, bindPwd, connectTimeout, trustManager, keyManager);
-    ldapContext = createAdministrativeContext(options, bindDn, bindPwd);
+    ldapContext = createAdministrativeContext(options);
     connectionFactory = new LDAPConnectionFactory(hostPort.getHost(), hostPort.getPort(), options);
     connection = buildConnection();
   }
@@ -222,19 +247,38 @@ public class ConnectionWrapper implements Closeable
   }
 
   /**
-   * Returns the LDAP URL used by the InitialLdapContext.
+   * Returns the LDAP URL used by this connection.
    *
-   * @return the LDAP URL used by the InitialLdapContext.
+   * @return the LDAP URL used by this connection.
    */
   public String getLdapUrl()
   {
     return ConnectionUtils.getLdapUrl(ldapContext);
   }
 
-  private InitialLdapContext createAdministrativeContext(Options options, String bindDn, String bindPwd)
-      throws NamingException
+  /**
+   * Returns whether this connection uses SSL.
+   *
+   * @return {@code true} if this connection uses SSL {@code false} otherwise.
+   */
+  public boolean isSSL()
   {
-    final InitialLdapContext ctx = createAdministrativeContext0(options, bindDn, bindPwd);
+    return ConnectionUtils.isSSL(ldapContext);
+  }
+
+  /**
+   * Returns whether this connection uses StartTLS.
+   *
+   * @return {@code true} if this connection uses StartTLS {@code false} otherwise.
+   */
+  public boolean isStartTLS()
+  {
+    return ConnectionUtils.isStartTLS(ldapContext);
+  }
+
+  private InitialLdapContext createAdministrativeContext(Options options) throws NamingException
+  {
+    final InitialLdapContext ctx = createAdministrativeContext0(options);
     if (!connectedAsAdministrativeUser(ctx))
     {
       throw new NoPermissionException(ERR_NOT_ADMINISTRATIVE_USER.get().toString());
@@ -242,24 +286,21 @@ public class ConnectionWrapper implements Closeable
     return ctx;
   }
 
-  private InitialLdapContext createAdministrativeContext0(Options options, String bindDn, String bindPwd)
-      throws NamingException
+  private InitialLdapContext createAdministrativeContext0(Options options) throws NamingException
   {
-    SSLContext sslContext = options.get(SSL_CONTEXT);
-    boolean useSSL = sslContext != null;
-    boolean useStartTLS = options.get(SSL_USE_STARTTLS);
+    boolean useSSL = options.get(SSL_CONTEXT) != null;
     final String ldapUrl = getLDAPUrl(getHostPort(), useSSL);
-    if (useSSL)
+    final String bindDnStr = bindDn.toString();
+    switch (connectionType)
     {
-      return createLdapsContext(ldapUrl, bindDn, bindPwd, connectTimeout, null, trustManager, keyManager);
-    }
-    else if (useStartTLS)
-    {
-      return createStartTLSContext(ldapUrl, bindDn, bindPwd, connectTimeout, null, trustManager, keyManager, null);
-    }
-    else
-    {
-      return createLdapContext(ldapUrl, bindDn, bindPwd, connectTimeout, null);
+    case LDAPS:
+      return createLdapsContext(ldapUrl, bindDnStr, bindPwd, connectTimeout, null, trustManager, keyManager);
+    case START_TLS:
+      return createStartTLSContext(ldapUrl, bindDnStr, bindPwd, connectTimeout, null, trustManager, keyManager, null);
+    case LDAP:
+      return createLdapContext(ldapUrl, bindDnStr, bindPwd, connectTimeout, null);
+    default:
+      throw new RuntimeException("Not implemented for connection type: " + connectionType);
     }
   }
 

opendj-server-legacy/src/main/java/org/opends/admin/ads/ADSContext.java
@@ -274,7 +274,6 @@ public class ADSContext
   }
 
   /** The context used to retrieve information. */
-  private final InitialLdapContext dirContext;
   private final ConnectionWrapper connectionWrapper;
 
   /**
@@ -286,7 +285,6 @@ public class ADSContext
   public ADSContext(ConnectionWrapper connectionWrapper)
   {
     this.connectionWrapper = connectionWrapper;
-    this.dirContext = connectionWrapper.getLdapContext();
   }
 
   /**
@@ -296,7 +294,7 @@ public class ADSContext
    */
   public InitialLdapContext getDirContext()
   {
-    return dirContext;
+    return connectionWrapper.getLdapContext();
   }
 
   /**
@@ -339,7 +337,7 @@ public class ADSContext
       {
         createContainerEntry(getServerContainerDN());
       }
-      dirContext.createSubcontext(dn, attrs).close();
+      connectionWrapper.getLdapContext().createSubcontext(dn, attrs).close();
       if (serverProperties.containsKey(ServerProperty.INSTANCE_PUBLIC_KEY_CERTIFICATE))
       {
         registerInstanceKeyCertificate(serverProperties, dn);
@@ -409,12 +407,12 @@ public class ADSContext
         Map<ServerProperty, Object> newServerProps = new HashMap<>(serverProperties);
         newServerProps.put(ServerProperty.ID, newServerId);
         LdapName newDn = makeDNFromServerProperties(newServerProps);
-        dirContext.rename(dn, newDn);
+        connectionWrapper.getLdapContext().rename(dn, newDn);
         dn = newDn;
         serverProperties.put(ServerProperty.ID, newServerId);
       }
       BasicAttributes attrs = makeAttrsFromServerProperties(serverProperties, false);
-      dirContext.modifyAttributes(dn, DirContext.REPLACE_ATTRIBUTE, attrs);
+      connectionWrapper.getLdapContext().modifyAttributes(dn, DirContext.REPLACE_ATTRIBUTE, attrs);
       if (serverProperties.containsKey(ServerProperty.INSTANCE_PUBLIC_KEY_CERTIFICATE))
       {
         registerInstanceKeyCertificate(serverProperties, dn);
@@ -464,7 +462,7 @@ public class ADSContext
         }
       }
 
-      dirContext.destroySubcontext(dn);
+      connectionWrapper.getLdapContext().destroySubcontext(dn);
     }
     catch (NameNotFoundException x)
     {
@@ -487,7 +485,7 @@ public class ADSContext
         String memberAttrName = ServerGroupProperty.MEMBERS.getAttributeName();
         String filter = "(" + memberAttrName + "=cn=" + serverID + ")";
         sc.setSearchScope(SearchControls.ONELEVEL_SCOPE);
-        ne = dirContext.search(getServerGroupContainerDN(), filter, sc);
+        ne = connectionWrapper.getLdapContext().search(getServerGroupContainerDN(), filter, sc);
         while (ne.hasMore())
         {
           SearchResult sr = ne.next();
@@ -530,11 +528,11 @@ public class ADSContext
           newAttrs.put(newAttr);
           if (newAttr.size() > 0)
           {
-            dirContext.modifyAttributes(groupDn, DirContext.REPLACE_ATTRIBUTE, newAttrs);
+            connectionWrapper.getLdapContext().modifyAttributes(groupDn, DirContext.REPLACE_ATTRIBUTE, newAttrs);
           }
           else
           {
-            dirContext.modifyAttributes(groupDn, DirContext.REMOVE_ATTRIBUTE, newAttrs);
+            connectionWrapper.getLdapContext().modifyAttributes(groupDn, DirContext.REMOVE_ATTRIBUTE, newAttrs);
           }
         }
       }
@@ -708,7 +706,7 @@ public class ADSContext
       SearchControls sc = new SearchControls();
 
       sc.setSearchScope(SearchControls.ONELEVEL_SCOPE);
-      ne = dirContext.search(getServerContainerDN(), "(objectclass=*)", sc);
+      ne = connectionWrapper.getLdapContext().search(getServerContainerDN(), "(objectclass=*)", sc);
       while (ne.hasMore())
       {
         SearchResult sr = ne.next();
@@ -724,7 +722,8 @@ public class ADSContext
             final String attrIDs[] = { "ds-cfg-public-key-certificate;binary" };
             sc1.setReturningAttributes(attrIDs);
 
-            ne2 = dirContext.search(getInstanceKeysContainerDN(), "(ds-cfg-key-id=" + keyId + ")", sc);
+            ne2 = connectionWrapper.getLdapContext().search(
+                getInstanceKeysContainerDN(), "(ds-cfg-key-id=" + keyId + ")", sc);
             boolean found = false;
             while (ne2.hasMore())
             {
@@ -789,7 +788,7 @@ public class ADSContext
     attrs.put(oc);
     try
     {
-      DirContext ctx = dirContext.createSubcontext(dn, attrs);
+      DirContext ctx = connectionWrapper.getLdapContext().createSubcontext(dn, attrs);
       ctx.close();
     }
     catch (NameAlreadyBoundException x)
@@ -826,7 +825,7 @@ public class ADSContext
         {
           // Rename to entry
           LdapName newDN = nameFromDN("cn=" + Rdn.escapeValue(newGroupId) + "," + getServerGroupContainerDN());
-          dirContext.rename(dn, newDN);
+          connectionWrapper.getLdapContext().rename(dn, newDN);
           dn = newDN;
         }
 
@@ -840,7 +839,7 @@ public class ADSContext
 
       BasicAttributes attrs = makeAttrsFromServerGroupProperties(serverGroupProperties);
       // attribute modification
-      dirContext.modifyAttributes(dn, DirContext.REPLACE_ATTRIBUTE, attrs);
+      connectionWrapper.getLdapContext().modifyAttributes(dn, DirContext.REPLACE_ATTRIBUTE, attrs);
     }
     catch (NameNotFoundException x)
     {
@@ -871,7 +870,7 @@ public class ADSContext
     {
       SearchControls sc = new SearchControls();
       sc.setSearchScope(SearchControls.ONELEVEL_SCOPE);
-      ne = dirContext.search(getServerGroupContainerDN(), "(objectclass=*)", sc);
+      ne = connectionWrapper.getLdapContext().search(getServerGroupContainerDN(), "(objectclass=*)", sc);
       while (ne.hasMore())
       {
         SearchResult sr = ne.next();
@@ -914,7 +913,7 @@ public class ADSContext
       sc.setSearchScope(SearchControls.ONELEVEL_SCOPE);
       String[] attList = { "cn", "userpassword", "ds-privilege-name", "description" };
       sc.setReturningAttributes(attList);
-      ne = dirContext.search(getAdministratorContainerDN(), "(objectclass=*)", sc);
+      ne = connectionWrapper.getLdapContext().search(getAdministratorContainerDN(), "(objectclass=*)", sc);
       while (ne.hasMore())
       {
         SearchResult sr = ne.next();
@@ -1019,7 +1018,7 @@ public class ADSContext
     try
     {
       Control[] controls = new Control[] { new SubtreeDeleteControl() };
-      LdapContext tmpContext = dirContext.newInstance(controls);
+      LdapContext tmpContext = connectionWrapper.getLdapContext().newInstance(controls);
       try
       {
         for (String dn : dns)
@@ -1102,7 +1101,7 @@ public class ADSContext
 
     try
     {
-      DirContext ctx = dirContext.createSubcontext(dnCentralAdmin, attrs);
+      DirContext ctx = connectionWrapper.getLdapContext().createSubcontext(dnCentralAdmin, attrs);
       ctx.close();
     }
     catch (NameAlreadyBoundException x)
@@ -1133,7 +1132,7 @@ public class ADSContext
 
     try
     {
-      dirContext.destroySubcontext(dnCentralAdmin);
+      connectionWrapper.getLdapContext().destroySubcontext(dnCentralAdmin);
     }
     catch (NameNotFoundException | NotContextException x)
     {
@@ -2112,7 +2111,7 @@ public class ADSContext
       throws ADSContextException
   {
     ADSContextHelper helper = new ADSContextHelper();
-    helper.registerInstanceKeyCertificate(dirContext, serverProperties, serverEntryDn);
+    helper.registerInstanceKeyCertificate(connectionWrapper, serverProperties, serverEntryDn);
   }
 
   /**
@@ -2145,7 +2144,8 @@ public class ADSContext
           { ADSContext.ServerProperty.INSTANCE_KEY_ID.getAttributeName(),
             ADSContext.ServerProperty.INSTANCE_PUBLIC_KEY_CERTIFICATE.getAttributeName() + ";binary" };
       searchControls.setReturningAttributes(attrIDs);
-      NamingEnumeration<SearchResult> keyEntries = dirContext.search(baseDN, searchFilter, searchControls);
+      NamingEnumeration<SearchResult> keyEntries =
+          connectionWrapper.getLdapContext().search(baseDN, searchFilter, searchControls);
       try
       {
         while (keyEntries.hasMore())

opendj-server-legacy/src/main/java/org/opends/admin/ads/ADSContextHelper.java
@@ -25,23 +25,23 @@ import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.BasicAttribute;
 import javax.naming.directory.BasicAttributes;
+import javax.naming.directory.DirContext;
 import javax.naming.directory.SearchResult;
-import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.LdapName;
 import javax.naming.ldap.Rdn;
 
-import org.opends.admin.ads.ADSContext.ServerProperty;
-import org.opends.admin.ads.ADSContextException.ErrorType;
-import org.opends.admin.ads.util.ConnectionWrapper;
 import org.forgerock.opendj.config.ManagedObjectNotFoundException;
+import org.forgerock.opendj.ldap.DN;
 import org.forgerock.opendj.server.config.client.LDIFBackendCfgClient;
 import org.forgerock.opendj.server.config.client.RootCfgClient;
 import org.forgerock.opendj.server.config.meta.BackendCfgDefn;
 import org.forgerock.opendj.server.config.meta.LDIFBackendCfgDefn;
+import org.opends.admin.ads.ADSContext.ServerProperty;
+import org.opends.admin.ads.ADSContextException.ErrorType;
+import org.opends.admin.ads.util.ConnectionWrapper;
 import org.opends.server.config.ConfigConstants;
 import org.opends.server.crypto.CryptoManagerImpl;
 import org.opends.server.types.CryptoManagerException;
-import org.forgerock.opendj.ldap.DN;
 
 /**
  * This is the only class in the org.opends.admin.ads package that uses the
@@ -61,7 +61,7 @@ class ADSContextHelper
 
   /**
    * Creates the Administration Suffix.
-   * @param ctx the DirContext to be used.
+   * @param conn the connection to be used.
    * @param backendName the name of the backend where the administration
    * suffix is stored.
    * @throws ADSContextException if the administration suffix could not be
@@ -120,7 +120,7 @@ class ADSContextHelper
   expected); add an instance key public-key certificate entry for the key
   certificate; and associate the certificate entry with the server entry via
   the key ID attribute.
-  @param ctx the InitialLdapContext on the server we want to update.
+  @param conn the connection on the server we want to update.
   @param serverProperties Properties of the server being registered to which
   the instance key entry belongs.
   @param serverEntryDn The server's ADS entry DN.
@@ -128,7 +128,7 @@ class ADSContextHelper
   problem getting the instance public key certificate ID.
    */
   void registerInstanceKeyCertificate(
-      InitialLdapContext ctx, Map<ServerProperty, Object> serverProperties,
+      ConnectionWrapper conn, Map<ServerProperty, Object> serverProperties,
       LdapName serverEntryDn)
   throws ADSContextException {
     assert serverProperties.containsKey(
@@ -162,8 +162,7 @@ class ADSContextHelper
     NamingEnumeration<SearchResult> results = null;
     try
     {
-      results = ctx.search(ADSContext.getInstanceKeysContainerDN(), keyAttrs,
-         attrIDs);
+      results = conn.getLdapContext().search(ADSContext.getInstanceKeysContainerDN(), keyAttrs, attrIDs);
       boolean found = false;
       while (results.hasMore()) {
         final Attribute keyIdAttr =
@@ -194,14 +193,14 @@ class ADSContextHelper
         final LdapName keyDn = new LdapName(
             ServerProperty.INSTANCE_KEY_ID.getAttributeName() + "=" + Rdn.escapeValue(keyID)
                 + "," + ADSContext.getInstanceKeysContainerDN());
-        ctx.createSubcontext(keyDn, keyAttrs).close();
+        conn.getLdapContext().createSubcontext(keyDn, keyAttrs).close();
       }
 
       if (serverEntryDn != null)
       {
         /* associate server entry with certificate entry via key ID attribute */
-        ctx.modifyAttributes(serverEntryDn,
-          InitialLdapContext.REPLACE_ATTRIBUTE,
+        conn.getLdapContext().modifyAttributes(serverEntryDn,
+          DirContext.REPLACE_ATTRIBUTE,
           new BasicAttributes(ServerProperty.INSTANCE_KEY_ID.getAttributeName(), keyID));
       }
     }

commit 578fbaa1f443cb0476b0cf0b4f5a22a4c6c4b498

Fix control-panel broken by commit f3f9f37cd05c81867e2852746b169132d8f
ConnectionWrapper.java:
Only use SSL with LDAPS (not StartTLS)

opendj-server-legacy/src/main/java/org/opends/admin/ads/util/ConnectionWrapper.java
@@ -192,7 +192,7 @@ public class ConnectionWrapper implements Closeable
     this.keyManager = keyManager;
 
     final Options options = toOptions(connectionType, bindDn, bindPwd, connectTimeout, trustManager, keyManager);
-    ldapContext = createAdministrativeContext(options);
+    ldapContext = createAdministrativeContext();
     connectionFactory = new LDAPConnectionFactory(hostPort.getHost(), hostPort.getPort(), options);
     connection = buildConnection();
   }
@@ -223,7 +223,8 @@ public class ConnectionWrapper implements Closeable
     {
       return new SSLContextBuilder()
           .setTrustManager(trustManager != null ? trustManager : new BlindTrustManager())
-          .setKeyManager(keyManager).getSSLContext();
+          .setKeyManager(keyManager)
+          .getSSLContext();
     }
     catch (GeneralSecurityException e)
     {
@@ -295,9 +296,9 @@ public class ConnectionWrapper implements Closeable
     return getConnectionType() == START_TLS;
   }
 
-  private InitialLdapContext createAdministrativeContext(Options options) throws NamingException
+  private InitialLdapContext createAdministrativeContext() throws NamingException
   {
-    final InitialLdapContext ctx = createAdministrativeContext0(options);
+    final InitialLdapContext ctx = createAdministrativeContext0();
     if (!connectedAsAdministrativeUser(ctx))
     {
       throw new NoPermissionException(ERR_NOT_ADMINISTRATIVE_USER.get().toString());
@@ -305,10 +306,9 @@ public class ConnectionWrapper implements Closeable
     return ctx;
   }
 
-  private InitialLdapContext createAdministrativeContext0(Options options) throws NamingException
+  private InitialLdapContext createAdministrativeContext0() throws NamingException
   {
-    boolean useSSL = options.get(SSL_CONTEXT) != null;
-    final String ldapUrl = getLDAPUrl(getHostPort(), useSSL);
+    final String ldapUrl = getLDAPUrl(getHostPort(), isSSL());
     final String bindDnStr = bindDn.toString();
     switch (connectionType)
     {