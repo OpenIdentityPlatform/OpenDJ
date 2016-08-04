OpenIdentityPlatform/OpenDJ

commit e55031aae085330b0d7b841d40cb1064c6c15867

use DN in dsreplication
ConnectionWrapper and EnableReplicationUserData now use DN.
Utils.getAdministratorDN() now returns a DN.

opendj-server-legacy/src/main/java/org/opends/server/tools/dsreplication/ReplicationCliMain.java
@@ -1201,7 +1201,7 @@ public class ReplicationCliMain extends ConsoleApplication
   private ConnectionWrapper newConnectionWrapper(
       LDAPConnectionConsoleInteraction ci, Type connType, int connectTimeout) throws NamingException
   {
-    return new ConnectionWrapper(getHostPort(ci), connType, ci.getBindDN(), ci.getBindPassword(),
+    return new ConnectionWrapper(getHostPort(ci), connType, DN.valueOf(ci.getBindDN()), ci.getBindPassword(),
         connectTimeout, ci.getTrustManager(), ci.getKeyManager());
   }
 
@@ -1475,7 +1475,7 @@ public class ReplicationCliMain extends ConsoleApplication
     return createAdministrativeConnection(uData, getAdministratorDN(uData.getAdminUid()));
   }
 
-  private ConnectionWrapper createAdministrativeConnection(MonoServerReplicationUserData uData, final String bindDn)
+  private ConnectionWrapper createAdministrativeConnection(MonoServerReplicationUserData uData, final DN bindDn)
   {
     try
     {
@@ -2380,7 +2380,7 @@ public class ReplicationCliMain extends ConsoleApplication
   private void setConnectionDetails(EnableReplicationServerData serverData, LDAPConnectionConsoleInteraction serverCI)
   {
     serverData.setHostPort(getHostPort2(serverCI));
-    serverData.setBindDn(serverCI.getBindDN());
+    serverData.setBindDn(DN.valueOf(serverCI.getBindDN()));
     serverData.setPwd(serverCI.getBindPassword());
   }
 
@@ -2489,7 +2489,7 @@ public class ReplicationCliMain extends ConsoleApplication
       final String adminUid = sourceServerCI.getProvidedAdminUID();
       uData.setHostPort(getHostPort2(sourceServerCI));
       uData.setAdminUid(adminUid);
-      uData.setBindDn(sourceServerCI.getProvidedBindDN());
+      uData.setBindDn(DN.valueOf(sourceServerCI.getProvidedBindDN()));
       uData.setAdminPwd(sourceServerCI.getBindPassword());
 
       if (adminUid != null)
@@ -3015,7 +3015,7 @@ public class ReplicationCliMain extends ConsoleApplication
   {
     initialize(uData);
 
-    final String adminDN = getAdministratorDN(uData.getAdminUid());
+    final DN adminDN = getAdministratorDN(uData.getAdminUid());
     final String adminPwd = uData.getAdminPwd();
     setConnectionDetails(uData.getServer1(), argParser.server1, adminDN, adminPwd);
     setConnectionDetails(uData.getServer2(), argParser.server2, adminDN, adminPwd);
@@ -3027,7 +3027,7 @@ public class ReplicationCliMain extends ConsoleApplication
   }
 
   private void setConnectionDetails(
-      EnableReplicationServerData server, ServerArgs args, String adminDN, String adminPwd)
+      EnableReplicationServerData server, ServerArgs args, DN adminDN, String adminPwd)
   {
     server.setHostPort(new HostPort(
         getValueOrDefault(args.hostNameArg), getValueOrDefault(args.portArg)));
@@ -3040,12 +3040,12 @@ public class ReplicationCliMain extends ConsoleApplication
     }
     else
     {
-      server.setBindDn(getValueOrDefault(args.bindDnArg));
+      server.setBindDn(DN.valueOf(getValueOrDefault(args.bindDnArg)));
       server.setPwd(pwd);
     }
   }
 
-  private boolean canConnectWithCredentials(EnableReplicationServerData server, String adminDN, String adminPwd)
+  private boolean canConnectWithCredentials(EnableReplicationServerData server, DN adminDN, String adminPwd)
   {
     try (ConnectionWrapper validCredentials = new ConnectionWrapper(
         server.getHostPort(), connectionType, adminDN, adminPwd, getConnectTimeout(), getTrustManager(sourceServerCI)))
@@ -3095,7 +3095,7 @@ public class ReplicationCliMain extends ConsoleApplication
   {
     uData.setBaseDNs(toDNs(argParser.getBaseDNs()));
     String adminUid = argParser.getAdministratorUID();
-    String bindDn = argParser.getBindDNToDisable();
+    DN bindDn = DN.valueOf(argParser.getBindDNToDisable());
     if (bindDn == null && adminUid == null)
     {
       adminUid = argParser.getAdministratorUIDOrDefault();
@@ -3784,7 +3784,7 @@ public class ReplicationCliMain extends ConsoleApplication
   private ReplicationCliReturnCode disableReplication(DisableReplicationUserData uData)
   {
     print(formatter.getFormattedWithPoints(INFO_REPLICATION_CONNECTING.get()));
-    String bindDn = uData.getAdminUid() != null
+    DN bindDn = uData.getAdminUid() != null
         ? getAdministratorDN(uData.getAdminUid())
         : uData.getBindDn();
 
@@ -8088,11 +8088,11 @@ public class ReplicationCliMain extends ConsoleApplication
       boolean forceAddBindPwdFile1 = false;
       if (useAdminUID)
       {
-        String bindDN1 = server1.getBindDn();
+        DN bindDN1 = server1.getBindDn();
         String adminUID = uData.getAdminUid();
         if (bindDN1 != null
             && adminUID != null
-            && !areDnsEqual(getAdministratorDN(adminUID), bindDN1))
+            && !getAdministratorDN(adminUID).equals(bindDN1))
         {
           forceAddBindDN1 = true;
           forceAddBindPwdFile1 = existsArg(firstServerCommandBuilder, OPTION_LONG_BINDPWD_FILE);
@@ -8180,11 +8180,11 @@ public class ReplicationCliMain extends ConsoleApplication
       boolean forceAddBindPwdFile2 = false;
       if (useAdminUID)
       {
-        String bindDN2 = server2.getBindDn();
+        DN bindDN2 = server2.getBindDn();
         String adminUID = uData.getAdminUid();
         if (bindDN2 != null
             && adminUID != null
-            && !areDnsEqual(getAdministratorDN(adminUID), bindDN2))
+            && !getAdministratorDN(adminUID).equals(bindDN2))
         {
           forceAddBindDN2 = true;
           forceAddBindPwdFile2 = existsArg(interactionBuilder, OPTION_LONG_BINDPWD_FILE);
@@ -8440,7 +8440,7 @@ public class ReplicationCliMain extends ConsoleApplication
                     .defaultValue("cn=Directory Manager")
                     .valuePlaceholder(INFO_BINDDN_PLACEHOLDER.get())
                     .buildArgument();
-    bindDN.addValue(uData.getServer1().getBindDn());
+    bindDN.addValue(uData.getServer1().getBindDn().toString());
     return bindDN;
   }
 
@@ -8454,7 +8454,7 @@ public class ReplicationCliMain extends ConsoleApplication
                     .defaultValue("cn=Directory Manager")
                     .valuePlaceholder(INFO_BINDDN_PLACEHOLDER.get())
                     .buildArgument();
-    bindDN.addValue(uData.getServer2().getBindDn());
+    bindDN.addValue(uData.getServer2().getBindDn().toString());
     return bindDN;
   }
 

opendj-cli/src/main/java/com/forgerock/opendj/cli/Utils.java
@@ -558,8 +558,8 @@ public final class Utils {
      *            The UID to be used to generate the DN.
      * @return The DN of the administrator for the given UID.
      */
-    public static String getAdministratorDN(String uid) {
-        return RDN.valueOf("cn=" + uid) + ",cn=Administrators, cn=admin data";
+    public static DN getAdministratorDN(String uid) {
+        return DN.valueOf(RDN.valueOf("cn=" + uid) + ",cn=Administrators, cn=admin data");
     }
 
     /**

opendj-server-legacy/src/main/java/org/opends/admin/ads/util/ConnectionWrapper.java
@@ -94,7 +94,7 @@ public class ConnectionWrapper implements Closeable
    * @throws NamingException
    *           If an error occurs
    */
-  public ConnectionWrapper(String ldapUrl, Type connectionType, String bindDn, String bindPwd, int connectTimeout,
+  public ConnectionWrapper(String ldapUrl, Type connectionType, DN bindDn, String bindPwd, int connectTimeout,
       TrustManager trustManager) throws NamingException
   {
     this(toHostPort(ldapUrl), connectionType, bindDn, bindPwd, connectTimeout, trustManager, null);
@@ -140,7 +140,7 @@ public class ConnectionWrapper implements Closeable
    * @throws NamingException
    *           If an error occurs
    */
-  public ConnectionWrapper(HostPort hostPort, Type connectionType, String bindDn, String bindPwd, int connectTimeout,
+  public ConnectionWrapper(HostPort hostPort, Type connectionType, DN bindDn, String bindPwd, int connectTimeout,
       TrustManager trustManager) throws NamingException
   {
     this(hostPort, connectionType, bindDn, bindPwd, connectTimeout, trustManager, null);
@@ -156,7 +156,7 @@ public class ConnectionWrapper implements Closeable
    */
   public ConnectionWrapper(ConnectionWrapper other) throws NamingException
   {
-    this(other.hostPort, other.connectionType, other.bindDn.toString(), other.bindPwd, other.connectTimeout,
+    this(other.hostPort, other.connectionType, other.bindDn, other.bindPwd, other.connectTimeout,
         other.trustManager, other.keyManager);
   }
 
@@ -180,12 +180,12 @@ public class ConnectionWrapper implements Closeable
    * @throws NamingException
    *           If an error occurs
    */
-  public ConnectionWrapper(HostPort hostPort, PreferredConnection.Type connectionType, String bindDn, String bindPwd,
+  public ConnectionWrapper(HostPort hostPort, PreferredConnection.Type connectionType, DN bindDn, String bindPwd,
       int connectTimeout, TrustManager trustManager, KeyManager keyManager) throws NamingException
   {
     this.hostPort = hostPort;
     this.connectionType = connectionType;
-    this.bindDn = DN.valueOf(bindDn);
+    this.bindDn = bindDn;
     this.bindPwd = bindPwd;
     this.connectTimeout = connectTimeout;
     this.trustManager = trustManager;
@@ -197,7 +197,7 @@ public class ConnectionWrapper implements Closeable
     connection = buildConnection();
   }
 
-  private static Options toOptions(Type connectionType, String bindDn, String bindPwd, long connectTimeout,
+  private static Options toOptions(Type connectionType, DN bindDn, String bindPwd, long connectTimeout,
       TrustManager trustManager, KeyManager keyManager) throws NamingException
   {
     final boolean isStartTls = START_TLS.equals(connectionType);
@@ -211,7 +211,7 @@ public class ConnectionWrapper implements Closeable
              .set(SSL_USE_STARTTLS, isStartTls);
     }
     SimpleBindRequest request = bindDn != null && bindPwd != null
-        ? newSimpleBindRequest(bindDn, bindPwd.toCharArray())
+        ? newSimpleBindRequest(bindDn.toString(), bindPwd.toCharArray())
         : newSimpleBindRequest(); // anonymous bind
     options.set(AUTHN_BIND_REQUEST, request);
     return options;

opendj-server-legacy/src/main/java/org/opends/admin/ads/util/ServerLoader.java
@@ -217,7 +217,7 @@ public class ServerLoader extends Thread
     {
       lastLdapUrl = connection.getLDAPURL();
       ConnectionWrapper conn =
-          new ConnectionWrapper(lastLdapUrl, connection.getType(), dn.toString(), pwd, timeout, trustManager);
+          new ConnectionWrapper(lastLdapUrl, connection.getType(), dn, pwd, timeout, trustManager);
       if (conn.getLdapContext() != null)
       {
         return conn;

opendj-server-legacy/src/main/java/org/opends/guitools/controlpanel/browser/LDAPConnectionPool.java
@@ -394,7 +394,7 @@ public class LDAPConnectionPool {
   {
     final HostPort hostPort = new HostPort(ldapUrl.getHost(), ldapUrl.getPort());
     final Type connectiontype = isSecureLDAPUrl(ldapUrl) ? LDAPS : LDAP;
-    return new ConnectionWrapper(hostPort, connectiontype, ar.dn.toString(), ar.password,
+    return new ConnectionWrapper(hostPort, connectiontype, ar.dn, ar.password,
         getConnectTimeout(), getTrustManager(), getKeyManager());
   }
 

opendj-server-legacy/src/main/java/org/opends/guitools/controlpanel/datamodel/ControlPanelInfo.java
@@ -40,6 +40,7 @@ import org.forgerock.i18n.LocalizableMessage;
 import org.forgerock.i18n.slf4j.LocalizedLogger;
 import org.forgerock.opendj.config.ConfigurationFramework;
 import org.forgerock.opendj.config.server.ConfigException;
+import org.forgerock.opendj.ldap.DN;
 import org.opends.admin.ads.util.ApplicationTrustManager;
 import org.opends.admin.ads.util.ConnectionWrapper;
 import org.opends.guitools.controlpanel.browser.IconPool;
@@ -102,7 +103,7 @@ public class ControlPanelInfo
   private String ldapsURL;
   private String adminConnectorURL;
   private String localAdminConnectorURL;
-  private String lastWorkingBindDN;
+  private DN lastWorkingBindDN;
   private String lastWorkingBindPwd;
   private String lastRemoteHostName;
   private String lastRemoteAdministrationURL;
@@ -297,7 +298,7 @@ public class ControlPanelInfo
     this.connWrapper = connWrapper;
     if (connWrapper != null)
     {
-      lastWorkingBindDN = connWrapper.getBindDn().toString();
+      lastWorkingBindDN = connWrapper.getBindDn();
       lastWorkingBindPwd = connWrapper.getBindPassword();
       lastRemoteHostName = connWrapper.getHostPort().getHost();
       lastRemoteAdministrationURL = connWrapper.getLdapUrl();

opendj-server-legacy/src/main/java/org/opends/guitools/controlpanel/ui/AbstractBrowseEntriesPanel.java
@@ -1197,8 +1197,7 @@ abstract class AbstractBrowseEntriesPanel extends StatusGenericPanel implements 
           {
             if (getInfo().getUserDataDirContext() == null)
             {
-              ConnectionWrapper connUserData =
-                  createUserDataDirContext(conn.getBindDn().toString(), conn.getBindPassword());
+              ConnectionWrapper connUserData = createUserDataDirContext(conn.getBindDn(), conn.getBindPassword());
               getInfo().setUserDataDirContext(connUserData);
             }
             final NamingException[] fNe = { null };
@@ -1412,7 +1411,7 @@ abstract class AbstractBrowseEntriesPanel extends StatusGenericPanel implements 
    * @throws ConfigReadException
    *           if an error occurs reading the configuration.
    */
-  private ConnectionWrapper createUserDataDirContext(final String bindDN, final String bindPassword)
+  private ConnectionWrapper createUserDataDirContext(final DN bindDN, final String bindPassword)
       throws NamingException, IOException, ConfigReadException
   {
     createdUserDataConn = null;
@@ -1529,7 +1528,7 @@ abstract class AbstractBrowseEntriesPanel extends StatusGenericPanel implements 
    * @param bindPassword
    *          the bind password.
    */
-  private void handleCertificateException(UserDataCertificateException ce, String bindDN, String bindPassword)
+  private void handleCertificateException(UserDataCertificateException ce, DN bindDN, String bindPassword)
       throws NamingException, IOException, ConfigReadException
   {
     CertificateDialog dlg = new CertificateDialog(null, ce);

opendj-server-legacy/src/main/java/org/opends/guitools/controlpanel/ui/LocalOrRemotePanel.java
@@ -538,16 +538,18 @@ public class LocalOrRemotePanel extends StatusGenericPanel
           ConnectionWrapper conn = null;
           try
           {
+            DN bindDn = DN.valueOf(dn.getText());
+            String bindPwd = String.valueOf(pwd.getPassword());
             if (isLocal)
             {
               usedUrl = info.getAdminConnectorURL();
-              conn = Utilities.getAdminDirContext(info, dn.getText(), String.valueOf(pwd.getPassword()));
+              conn = Utilities.getAdminDirContext(info, bindDn, bindPwd);
             }
             else
             {
               HostPort hostPort = new HostPort(hostName.getText().trim(), Integer.valueOf(port.getText().trim()));
               usedUrl = ConnectionUtils.getLDAPUrl(hostPort, true);
-              conn = new ConnectionWrapper(hostPort, LDAPS, dn.getText(), String.valueOf(pwd.getPassword()),
+              conn = new ConnectionWrapper(hostPort, LDAPS, bindDn, bindPwd,
                   info.getConnectTimeout(), info.getTrustManager());
               checkVersion(conn);
             }

commit 3011f14673b669dc68cc54772f956dc255724d77

OPENDJ-3235 Replication: NPE when disabling replication on cn=schema
Fixed problem introduced by commit e55031aae08

opendj-server-legacy/src/main/java/org/opends/server/tools/dsreplication/ReplicationCliMain.java
@@ -292,7 +292,7 @@ public class ReplicationCliMain extends ConsoleApplication
      * @return whether the current task should continue
      */
     boolean confirmOperation(SourceDestinationServerUserData uData, ConnectionWrapper connSource,
-        ConnectionWrapper connDestination, final boolean defaultValue);
+        ConnectionWrapper connDestination, boolean defaultValue);
   }
 
   /** The argument parser to be used. */
@@ -2690,7 +2690,6 @@ public class ReplicationCliMain extends ConsoleApplication
 
     try
     {
-
       List<DN> suffixes = toDNs(argParser.getBaseDNs());
       checkSuffixesForInitializeReplication(suffixes, conn, true);
       if (suffixes.isEmpty())
@@ -3095,7 +3094,8 @@ public class ReplicationCliMain extends ConsoleApplication
   {
     uData.setBaseDNs(toDNs(argParser.getBaseDNs()));
     String adminUid = argParser.getAdministratorUID();
-    DN bindDn = DN.valueOf(argParser.getBindDNToDisable());
+    String bindDnStr = argParser.getBindDNToDisable();
+    DN bindDn = bindDnStr != null ? DN.valueOf(bindDnStr) : null;
     if (bindDn == null && adminUid == null)
     {
       adminUid = argParser.getAdministratorUIDOrDefault();
@@ -5680,7 +5680,6 @@ public class ReplicationCliMain extends ConsoleApplication
       }
     }
 
-
     boolean somethingDisplayed = false;
     if (!oneReplicated && displayAll)
     {
@@ -6252,7 +6251,6 @@ public class ReplicationCliMain extends ConsoleApplication
   {
     print(formatter.getFormattedWithPoints(
         INFO_REPLICATION_ENABLE_CONFIGURING_REPLICATION_SERVER.get(conn.getHostPort())));
-
 
     /* Configure Synchronization plugin. */
     ReplicationSynchronizationProviderCfgClient sync = null;