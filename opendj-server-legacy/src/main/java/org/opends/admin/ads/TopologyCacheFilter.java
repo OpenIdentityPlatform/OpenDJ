OpenIdentityPlatform/OpenDJ

commit b07dd3c9cf16b40636b797aeb801aafa73d21fa0

ReplicationCliMain.java: various code cleanups

opendj-server-legacy/src/main/java/org/opends/admin/ads/TopologyCacheFilter.java
@@ -16,6 +16,7 @@
  */
 package org.opends.admin.ads;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -35,8 +36,8 @@ public class TopologyCacheFilter
 
   /**
    * Returns whether we must search for base DN information or not.
-   * @return <CODE>true</CODE> if we must search base DN information and
-   * <CODE>false</CODE> otherwise.
+   * @return {@code true} if we must search base DN information and
+   * {@code false} otherwise.
    */
   boolean searchBaseDNInformation()
   {
@@ -54,11 +55,10 @@ public class TopologyCacheFilter
     this.searchBaseDNInformation = searchBaseDNInformation;
   }
 
-
   /**
    * Returns whether we must search for monitoring information or not.
-   * @return <CODE>true</CODE> if we must search monitoring information and
-   * <CODE>false</CODE> otherwise.
+   * @return {@code true} if we must search monitoring information and
+   * {@code false} otherwise.
    */
   boolean searchMonitoringInformation()
   {
@@ -80,11 +80,22 @@ public class TopologyCacheFilter
    * Adds one of the base DNs we must search for.  If at least one baseDN
    * is added using this method, only the added baseDNs are searched.  If no
    * base DN is added, all the base DNs will be retrieved.
-   * @param dn the DN of the base DN to look for.
+   * @param dn the base DN to look for.
    */
   public void addBaseDNToSearch(DN dn)
   {
     baseDNs.add(dn);
+  }
+
+  /**
+   * Adds all the base DNs we must search for.  If at least one baseDN
+   * is added using this method, only the added baseDNs are searched.
+   * If no base DN is added, all the base DNs will be retrieved.
+   * @param dns the base DNs to look for.
+   */
+  public void addBaseDNsToSearch(Collection<DN> dns)
+  {
+    dns.addAll(dns);
   }
 
   /**
@@ -99,8 +110,8 @@ public class TopologyCacheFilter
 
   /**
    * Tells whether this filter specifies to search for all the base DNs or not.
-   * @return <CODE>true</CODE> if the filter specifies to search for all the
-   * base DNs and <CODE>false</CODE> otherwise.
+   * @return {@code true} if the filter specifies to search for all the
+   * base DNs and {@code false} otherwise.
    */
   boolean searchAllBaseDNs()
   {

opendj-server-legacy/src/main/java/org/opends/admin/ads/ServerDescriptor.java
@@ -850,11 +850,7 @@ public class ServerDescriptor
 
   private static boolean isAddReplica(TopologyCacheFilter cacheFilter, DN baseDn)
   {
-    if (cacheFilter.searchAllBaseDNs())
-    {
-      return true;
-    }
-    return cacheFilter.getBaseDNsToSearch().contains(baseDn);
+    return cacheFilter.searchAllBaseDNs() || cacheFilter.getBaseDNsToSearch().contains(baseDn);
   }
 
   private static void updateReplication(ServerDescriptor desc, ConnectionWrapper conn, TopologyCacheFilter cacheFilter)

opendj-server-legacy/src/main/java/org/opends/server/tools/dsreplication/ReplicationCliMain.java
@@ -4608,7 +4608,7 @@ public class ReplicationCliMain extends ConsoleApplication
     filter.setSearchMonitoringInformation(false);
     filter.addBaseDNToSearch(ADSContext.getAdministrationSuffixDN());
     filter.addBaseDNToSearch(Constants.SCHEMA_DN);
-    addBaseDNs(filter, uData.getBaseDNs());
+    filter.addBaseDNsToSearch(uData.getBaseDNs());
     ServerDescriptor serverDesc1 = createStandalone(conn1, filter);
     ServerDescriptor serverDesc2 = createStandalone(conn2, filter);
 
@@ -5179,7 +5179,7 @@ public class ReplicationCliMain extends ConsoleApplication
       TopologyCache cache = new TopologyCache(adsCtx, getTrustManager(sourceServerCI), getConnectTimeout());
       cache.setPreferredConnections(cnx);
       cache.getFilter().setSearchMonitoringInformation(false);
-      addBaseDNs(cache.getFilter(), uData.getBaseDNs());
+      cache.getFilter().addBaseDNsToSearch(uData.getBaseDNs());
       cache.reloadTopology();
       return cache;
     }
@@ -5220,7 +5220,7 @@ public class ReplicationCliMain extends ConsoleApplication
     if (!uData.disableAll())
     {
       filter.addBaseDNToSearch(ADSContext.getAdministrationSuffixDN());
-      addBaseDNs(filter, uData.getBaseDNs());
+      filter.addBaseDNsToSearch(uData.getBaseDNs());
     }
     ServerDescriptor server = createStandalone(conn, filter);
 
@@ -5239,7 +5239,7 @@ public class ReplicationCliMain extends ConsoleApplication
         cache.getFilter().setSearchMonitoringInformation(false);
         if (!uData.disableAll())
         {
-          addBaseDNs(cache.getFilter(), uData.getBaseDNs());
+          cache.getFilter().addBaseDNsToSearch(uData.getBaseDNs());
         }
         cache.reloadTopology();
       }
@@ -5614,14 +5614,6 @@ public class ReplicationCliMain extends ConsoleApplication
     return false;
   }
 
-  private void addBaseDNs(TopologyCacheFilter filter, List<DN> baseDNs)
-  {
-    for (DN dn : baseDNs)
-    {
-      filter.addBaseDNToSearch(dn);
-    }
-  }
-
   /**
    * Displays the replication status of the different base DNs in the servers registered in the ADS.
    *
@@ -5637,21 +5629,7 @@ public class ReplicationCliMain extends ConsoleApplication
       StatusReplicationUserData uData) throws ReplicationCliException
   {
     ADSContext adsCtx = new ADSContext(conn);
-
-    TopologyCache cache;
-    try
-    {
-      cache = new TopologyCache(adsCtx, getTrustManager(sourceServerCI), getConnectTimeout());
-      cache.setPreferredConnections(getPreferredConnections(conn));
-      addBaseDNs(cache.getFilter(), uData.getBaseDNs());
-      cache.reloadTopology();
-    }
-    catch (TopologyCacheException tce)
-    {
-      throw new ReplicationCliException(
-          ERR_REPLICATION_READING_ADS.get(tce.getMessage()),
-          ERROR_READING_TOPOLOGY_CACHE, tce);
-    }
+    TopologyCache cache = createTopologyCache(conn, uData, adsCtx);
     if (mustPrintCommandBuilder())
     {
       printNewCommandBuilder(STATUS_REPLICATION_SUBCMD_NAME, uData);
@@ -5751,6 +5729,25 @@ public class ReplicationCliMain extends ConsoleApplication
       println(displayAll
           ? INFO_REPLICATION_STATUS_NO_REPLICATION_INFORMATION.get()
           : INFO_REPLICATION_STATUS_NO_BASEDNS.get());
+    }
+  }
+
+  private TopologyCache createTopologyCache(ConnectionWrapper conn, StatusReplicationUserData uData, ADSContext adsCtx)
+      throws ReplicationCliException
+  {
+    try
+    {
+      TopologyCache cache = new TopologyCache(adsCtx, getTrustManager(sourceServerCI), getConnectTimeout());
+      cache.setPreferredConnections(getPreferredConnections(conn));
+      cache.getFilter().addBaseDNsToSearch(uData.getBaseDNs());
+      cache.reloadTopology();
+      return cache;
+    }
+    catch (TopologyCacheException tce)
+    {
+      throw new ReplicationCliException(
+          ERR_REPLICATION_READING_ADS.get(tce.getMessage()),
+          ERROR_READING_TOPOLOGY_CACHE, tce);
     }
   }
 
@@ -8799,7 +8796,7 @@ public class ReplicationCliMain extends ConsoleApplication
       {
         TopologyCache cache = new TopologyCache(adsCtx, getTrustManager(sourceServerCI), getConnectTimeout());
         cache.getFilter().setSearchMonitoringInformation(false);
-        addBaseDNs(cache.getFilter(), uData.getBaseDNs());
+        cache.getFilter().addBaseDNsToSearch(uData.getBaseDNs());
         cache.reloadTopology();
         suffixes.addAll(cache.getSuffixes());
       }
@@ -9103,13 +9100,13 @@ public class ReplicationCliMain extends ConsoleApplication
   private ConnectionWrapper getConnection(TopologyCache cache, ServerDescriptor server) throws NamingException
   {
     ConnectionWrapper conn = cache.getAdsContext().getConnection();
-    DN dn = conn.getBindDn();
-    String pwd = conn.getBindPassword();
+
     TopologyCacheFilter filter = new TopologyCacheFilter();
     filter.setSearchMonitoringInformation(false);
     filter.setSearchBaseDNInformation(false);
+
     ServerLoader loader = new ServerLoader(server.getAdsProperties(),
-        dn, pwd, getTrustManager(sourceServerCI), getConnectTimeout(),
+        conn.getBindDn(), conn.getBindPassword(), getTrustManager(sourceServerCI), getConnectTimeout(),
         cache.getPreferredConnections(), filter);
     return loader.createConnectionWrapper();
   }

commit c40b9b2a2becc7784ccc5b4d4c3c0f86f1dc1774

dsreplication enable: make it work again when enabling from DS to DS
Fixes issue introduced by commit b07dd3c9cf16b40636b797aeb801aafa73d21fa0

opendj-server-legacy/src/main/java/org/opends/admin/ads/TopologyCacheFilter.java
@@ -95,7 +95,7 @@ public class TopologyCacheFilter
    */
   public void addBaseDNsToSearch(Collection<DN> dns)
   {
-    dns.addAll(dns);
+    baseDNs.addAll(dns);
   }
 
   /**