OpenIdentityPlatform/OpenDJ

commit f2b5fa18b58db09562d03a7d247e21c111e78056

OPENDJ-3179: Migrate LDAP Connection Handler to SDK Grizzly transport
Add a new LDAPConnectionHandler based on Grizzly and using the reactive API

opendj-server-legacy/src/main/java/org/forgerock/opendj/adapter/server3x/Adapters.java
@@ -146,7 +146,7 @@ public final class Adapters {
                   @Override
                   public void handleInternalSearchEntry(InternalSearchOperation searchOperation,
                           SearchResultEntry searchEntry) throws DirectoryException {
-                      handler.handleEntry(from(searchEntry));
+                      handler.handleEntry(partiallyWrap(searchEntry));
                   }
               };
 

opendj-core/pom.xml
@@ -50,6 +50,18 @@
         </dependency>
 
         <dependency>
+            <groupId>org.reactivestreams</groupId>
+            <artifactId>reactive-streams</artifactId>
+            <version>1.0.0</version>
+        </dependency>
+
+        <dependency>
+            <groupId>io.reactivex.rxjava2</groupId>
+            <artifactId>rxjava</artifactId>
+            <version>2.0.0-RC3</version>
+        </dependency>
+
+        <dependency>
             <groupId>org.forgerock.commons</groupId>
             <artifactId>i18n-slf4j</artifactId>
         </dependency>
@@ -113,8 +125,7 @@
                             com.forgerock.opendj.ldap*,
                             org.forgerock.opendj.io,
                             org.forgerock.opendj.ldap*,
-                            org.forgerock.opendj.ldif,
-                            org.forgerock.opendj.security
+                            org.forgerock.opendj.ldif
                         </Export-Package>
                         <Build-Maven>Apache Maven ${maven.version}</Build-Maven>
                         <SCM-Revision>${buildRevision}</SCM-Revision>

opendj-core/src/main/java/com/forgerock/reactive/Action.java
@@ -11,25 +11,19 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
- * Copyright 2010 Sun Microsystems, Inc.
- * Portions copyright 2011-2016 ForgeRock AS.
+ * Copyright 2016 ForgeRock AS.
  */
-
-package org.forgerock.opendj.grizzly;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-import org.forgerock.opendj.io.ASN1Reader;
-import org.forgerock.opendj.io.ASN1ReaderTestCase;
-import org.glassfish.grizzly.memory.ByteBufferWrapper;
+package com.forgerock.reactive;
 
 /**
- * This class provides test cases for ASN1BufferReader.
+ * A functional interface similar to Runnable but allows throwing a checked exception.
  */
-public class ASN1BufferReaderTestCase extends ASN1ReaderTestCase {
-    @Override
-    protected ASN1Reader getReader(final byte[] b, final int maxElementSize) throws IOException {
-        return new ASN1BufferReader(maxElementSize, new ByteBufferWrapper(ByteBuffer.wrap(b)));
-    }
+public interface Action {
+    /**
+     * Runs the action and optionally throws a checked exception.
+     *
+     * @throws Exception
+     *             if the implementation wishes to throw a checked exception
+     */
+    void run() throws Exception;
 }

opendj-core/src/main/java/com/forgerock/reactive/Completable.java
@@ -1,0 +1,58 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2016 ForgeRock AS.
+ */
+package com.forgerock.reactive;
+
+import org.reactivestreams.Publisher;
+
+/** {@link Completable} is used to communicates a terminated operation which doesn't produce a result. */
+public interface Completable extends Publisher<Void> {
+
+    /** Emitter is used to notify when the operation has been completed, successfully or not. */
+    public interface Emitter {
+        /** Notify that this {@link Completable} is now completed. */
+        void complete();
+
+        /**
+         * Notify that this {@link Completable} cannot be completed because of an error.
+         *
+         * @param error
+         *            The error preventing this {@link Completable} to complete
+         */
+        void onError(Throwable error);
+    }
+
+    /** Adapts the streaming api to a callback one. */
+    public interface OnSubscribe {
+        /**
+         * Called when the streaming api has been subscribed.
+         *
+         * @param e
+         *            The {@link Emitter} to use to communicate the completeness of this {@link Completable}
+         */
+        void onSubscribe(Emitter e);
+    }
+
+    /**
+     * Creates a {@link Single} which will emit the specified value when this {@link Completable} complete.
+     *
+     * @param <V>
+     *            Type of the value to emit
+     * @param value
+     *            The value to emit on completion
+     * @return A new {@link Single}
+     */
+    <V> Single<V> toSingle(V value);
+}

opendj-core/src/main/java/com/forgerock/reactive/Consumer.java
@@ -11,25 +11,24 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
- * Copyright 2010 Sun Microsystems, Inc.
- * Portions copyright 2011-2016 ForgeRock AS.
+ * Copyright 2016 ForgeRock AS.
  */
-
-package org.forgerock.opendj.grizzly;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-import org.forgerock.opendj.io.ASN1Reader;
-import org.forgerock.opendj.io.ASN1ReaderTestCase;
-import org.glassfish.grizzly.memory.ByteBufferWrapper;
+package com.forgerock.reactive;
 
 /**
- * This class provides test cases for ASN1BufferReader.
+ * A functional interface (callback) that consumes a single value.
+ *
+ * @param <V>
+ *            the value type
  */
-public class ASN1BufferReaderTestCase extends ASN1ReaderTestCase {
-    @Override
-    protected ASN1Reader getReader(final byte[] b, final int maxElementSize) throws IOException {
-        return new ASN1BufferReader(maxElementSize, new ByteBufferWrapper(ByteBuffer.wrap(b)));
-    }
+public interface Consumer<V> {
+    /**
+     * Consume the given value.
+     *
+     * @param item
+     *            the value
+     * @throws Exception
+     *             on error
+     */
+    void accept(V item) throws Exception;
 }

opendj-core/src/main/java/com/forgerock/reactive/ReactiveFilter.java
@@ -1,0 +1,185 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2016 ForgeRock AS.
+ */
+package com.forgerock.reactive;
+
+import org.forgerock.util.Function;
+import org.forgerock.util.promise.NeverThrowsException;
+
+/**
+ * Filters and/or transforms the request and/or response of an exchange.
+ *
+ *                +--------+              +---------+
+ *   ---> I1 ---> |        | ---> I2 ---> |         |
+ *                | Filter |              | Handler |
+ *   <--- O1 --<  |        | <--- O2 <--- |         |
+ *                +--------+              +---------+
+ *
+ *                +--------+              +---------+              +---------+
+ *   ---> I1 ---> |  One   | ---> I2 ---> | AndThen | ---> I3 ---> |         |
+ *                | Filter |              | Filter  |              | Handler |
+ *   <--- O1 --<  |        | <--- O2 <--- |         | <--- O3 ---< |         |
+ *                +--------+              +---------+              +---------+
+ *
+ * @param <C>
+ *            Context in which the filter will be evaluated
+ * @param <I1>
+ *            Type of the request received by this filter
+ * @param <O1>
+ *            Type of the response answered by this filter
+ * @param <I2>
+ *            Type of the request emitted to the handler/next filter
+ * @param <O2>
+ *            Type of the response emitted by the handler/next filter
+ */
+public abstract class ReactiveFilter<C, I1, O1, I2, O2> {
+
+    /**
+     * A simple {@link ReactiveHandler} performing simple filtering without type transformation.
+     *
+     * @param <C>
+     *            Context in which the filter will be evaluated
+     * @param <I>
+     *            Type of the filtered request
+     * @param <O>
+     *            Type of the filtered response
+     */
+    public static abstract class SimpleReactiveFilter<C, I, O> extends ReactiveFilter<C, I, O, I, O> {
+    }
+
+    /**
+     * Optionally concatenate a new filter to this filter.
+     *
+     * @param condition
+     *            If true, next will be concatenated to this filter
+     * @param next
+     *            The filter to optionally concatenate with this one
+     * @return If condition is true, returns the concatenation of this filter and next. Otherwise, return this filter.
+     */
+    public ReactiveFilter<C, I1, O1, I2, O2> andThen(final boolean condition,
+            final ReactiveFilter<C, I2, O2, I2, O2> next) {
+        if (!condition) {
+            return ReactiveFilter.this;
+        }
+        return new ConcatenatedFilter<C, I1, O1, I2, O2>(
+                new Function<ReactiveHandler<C, I2, O2>, ReactiveHandler<C, I1, O1>, NeverThrowsException>() {
+                    @Override
+                    public ReactiveHandler<C, I1, O1> apply(ReactiveHandler<C, I2, O2> handler) {
+                        return andThen(next.andThen(handler));
+                    }
+                });
+    }
+
+    /**
+     * Concatenate a transformer filter to this filter.
+     *
+     * @param <I3>
+     *            Target type of the request transformed by the filter
+     * @param <O3>
+     *            Source type of the response transformed by the filter
+     * @param next
+     *            The transformer filter to add after this filter.
+     * @return A new {@link ReactiveFilter} results of the concatenation of this filter and the transformer filter
+     */
+    public <I3, O3> ReactiveFilter<C, I1, O1, I3, O3> andThen(final ReactiveFilter<C, I2, O2, I3, O3> next) {
+        return new ConcatenatedFilter<C, I1, O1, I3, O3>(
+                new Function<ReactiveHandler<C, I3, O3>, ReactiveHandler<C, I1, O1>, NeverThrowsException>() {
+                    @Override
+                    public ReactiveHandler<C, I1, O1> apply(ReactiveHandler<C, I3, O3> handler) {
+                        return andThen(next.andThen(handler));
+                    }
+                });
+    }
+
+    /**
+     * Add a new filtering step after this one.
+     *
+     * @param next
+     *            The filter to add after this one
+     * @return A new {@link ReactiveFilter} result of the concatenation of this filter and next
+     */
+    public ReactiveFilter<C, I1, O1, I2, O2> andThen(final SimpleReactiveFilter<C, I2, O2> next) {
+        return new ConcatenatedFilter<C, I1, O1, I2, O2>(
+                new Function<ReactiveHandler<C, I2, O2>, ReactiveHandler<C, I1, O1>, NeverThrowsException>() {
+                    @Override
+                    public ReactiveHandler<C, I1, O1> apply(ReactiveHandler<C, I2, O2> handler) {
+                        return andThen(next.andThen(handler));
+                    }
+                });
+    }
+
+    /**
+     * Terminate the filter chain with the specified handler.
+     *
+     * @param handler
+     *            {@link ReactiveHandler} in charge of processing the request
+     * @return A new {@link ReactiveHandler} results of the concatenation of this filter and handler
+     */
+    public ReactiveHandler<C, I1, O1> andThen(final ReactiveHandler<C, I2, O2> handler) {
+        final ReactiveFilter<C, I1, O1, I2, O2> parent = this;
+        return new ReactiveHandler<C, I1, O1>() {
+            @Override
+            public Single<O1> handle(final C context, final I1 request) throws Exception {
+                return parent.filter(context, request, handler);
+            }
+        };
+    }
+
+    /**
+     * Perform the request and/or response filtering and/or transformation.
+     *
+     * @param context
+     *            Context of the filtering/transformation
+     * @param request
+     *            Request to filter
+     * @param next
+     *            {@link ReactiveHandler} to call once the filtering/transformation is done
+     * @return The filtered and/or transformed response
+     * @throws Exception
+     *             If the operation cannot be done
+     */
+    public abstract Single<O1> filter(final C context, final I1 request, final ReactiveHandler<C, I2, O2> next)
+            throws Exception;
+
+    private static final class ConcatenatedFilter<C, I1, O1, I2, O2> extends ReactiveFilter<C, I1, O1, I2, O2> {
+        private final Function<ReactiveHandler<C, I2, O2>, ReactiveHandler<C, I1, O1>, NeverThrowsException> converter;
+
+        ConcatenatedFilter(
+                Function<ReactiveHandler<C, I2, O2>, ReactiveHandler<C, I1, O1>, NeverThrowsException> converter) {
+            this.converter = converter;
+        }
+
+        @Override
+        public <I3, O3> ReactiveFilter<C, I1, O1, I3, O3> andThen(final ReactiveFilter<C, I2, O2, I3, O3> nextFilter) {
+            return new ConcatenatedFilter<C, I1, O1, I3, O3>(
+                    new Function<ReactiveHandler<C, I3, O3>, ReactiveHandler<C, I1, O1>, NeverThrowsException>() {
+                        @Override
+                        public ReactiveHandler<C, I1, O1> apply(ReactiveHandler<C, I3, O3> handler) {
+                            return converter.apply(nextFilter.andThen(handler));
+                        }
+                    });
+        }
+
+        @Override
+        public ReactiveHandler<C, I1, O1> andThen(ReactiveHandler<C, I2, O2> handler) {
+            return converter.apply(handler);
+        }
+
+        @Override
+        public Single<O1> filter(C context, I1 request, ReactiveHandler<C, I2, O2> handler) throws Exception {
+            return converter.apply(handler).handle(context, request);
+        }
+    }
+}

opendj-core/src/main/java/com/forgerock/reactive/ReactiveHandler.java
@@ -1,0 +1,41 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2016 ForgeRock AS.
+ */
+package com.forgerock.reactive;
+
+/**
+ * Handle the processing of a request in a given context and return the resulting response.
+ *
+ * @param <CTX>
+ *            Type of the context
+ * @param <REQ>
+ *            Type of the request to process
+ * @param <REP>
+ *            Type of the response
+ */
+public interface ReactiveHandler<CTX, REQ, REP> {
+    /**
+     * Process the request given the context.
+     *
+     * @param context
+     *            Context in which the request must be processed
+     * @param request
+     *            The response to process
+     * @return A {@link Single} response.
+     * @throws Exception
+     *             if the request cannot be processed
+     */
+    Single<REP> handle(final CTX context, final REQ request) throws Exception;
+}

opendj-core/src/main/java/com/forgerock/reactive/RxJavaStreams.java
@@ -1,0 +1,310 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2016 ForgeRock AS.
+ */
+package com.forgerock.reactive;
+
+import org.forgerock.util.Function;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+import io.reactivex.CompletableEmitter;
+import io.reactivex.CompletableOnSubscribe;
+import io.reactivex.Flowable;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.SingleSource;
+
+/**
+ * {@link Stream} and {@link Single} implementations based on RxJava.
+ */
+public final class RxJavaStreams {
+
+    private RxJavaStreams() {
+        // Hide
+    }
+
+    /**
+     * Create a new {@link Stream} from the given {@link Publisher}.
+     *
+     * @param <V>
+     *            Type of data emitted
+     * @param publisher
+     *            The {@link Publisher} to convert
+     * @return A new {@link Stream}
+     */
+    public static <V> Stream<V> streamFromPublisher(final Publisher<V> publisher) {
+        return new RxJavaStream<>(Flowable.fromPublisher(publisher));
+    }
+
+    /**
+     * Create a new {@link Stream} composed only of the given value.
+     *
+     * @param <V>
+     *            Type of data emitted
+     * @param value
+     *            The value emitted by this stream
+     * @return A new {@link Stream}
+     */
+    public static <V> Stream<V> streamFrom(final V value) {
+        return new RxJavaStream<>(Flowable.just(value));
+    }
+
+    /**
+     * Create a new {@link Stream} composed only of the given error.
+     *
+     * @param <V>
+     *            Type of data emitted
+     * @param error
+     *            The error emitted by this stream
+     * @return A new {@link Stream}
+     */
+    public static <V> Stream<V> streamError(final Throwable error) {
+        return new RxJavaStream<>(Flowable.<V> error(error));
+    }
+
+    /**
+     * Create a new empty {@link Stream}.
+     *
+     * @param <V>
+     *            Type of data emitted
+     * @return An empty {@link Stream}
+     */
+    public static <V> Stream<V> emptyStream() {
+        return new RxJavaStream<>(Flowable.<V> empty());
+    }
+
+    /**
+     * Create a new {@link Single} from the given {@link Publisher}.
+     *
+     * @param <V>
+     *            Type of the datum emitted
+     * @param publisher
+     *            The {@link Publisher} to convert
+     * @return A new {@link Stream}
+     */
+    public static <V> Single<V> singleFromPublisher(final Publisher<V> publisher) {
+        return new RxJavaSingle<>(io.reactivex.Single.fromPublisher(publisher));
+    }
+
+    /**
+     * Create a new {@link Single} from the given value.
+     *
+     * @param <V>
+     *            Type of the datum emitted
+     * @param value
+     *            The value contained by this {@link Single}
+     * @return A new {@link Single}
+     */
+    public static <V> Single<V> singleFrom(final V value) {
+        return new RxJavaSingle<>(io.reactivex.Single.just(value));
+    }
+
+    /**
+     * Creates a bridge from callback world to {@link Single}.
+     *
+     * @param <V>
+     *            Type of the datum emitted
+     * @param onSubscribe
+     *            Action to perform once this {@link Single} has been subscribed to.
+     * @return A new {@link Single}
+     */
+    public static <V> Single<V> newSingle(final Single.OnSubscribe<V> onSubscribe) {
+        return new RxJavaSingle<>(io.reactivex.Single.create(new SingleOnSubscribe<V>() {
+            @Override
+            public void subscribe(final SingleEmitter<V> e) throws Exception {
+                onSubscribe.onSubscribe(new Single.Emitter<V>() {
+                    @Override
+                    public void onSuccess(V value) {
+                        e.onSuccess(value);
+                    }
+
+                    @Override
+                    public void onError(Throwable error) {
+                        e.onError(error);
+                    }
+                });
+            }
+        }));
+    }
+
+    /**
+     * Creates a bridge from callback world to {@link Completable}.
+     *
+     * @param onSubscribe
+     *            Action to perform once this {@link Completable} has been subscribed to.
+     * @return A new {@link Completable}
+     */
+    public static Completable newCompletable(final Completable.OnSubscribe onSubscribe) {
+        return new RxJavaCompletable(io.reactivex.Completable.create(new CompletableOnSubscribe() {
+            @Override
+            public void subscribe(final CompletableEmitter e) throws Exception {
+                onSubscribe.onSubscribe(new Completable.Emitter() {
+                    @Override
+                    public void complete() {
+                        e.onComplete();
+                    }
+                    @Override
+                    public void onError(Throwable t) {
+                        e.onError(t);
+                    }
+                });
+            }
+        }));
+    }
+
+    private static final class RxJavaStream<V> implements Stream<V> {
+
+        private Flowable<V> impl;
+
+        private RxJavaStream(final Flowable<V> impl) {
+            this.impl = impl;
+        }
+
+        @Override
+        public void subscribe(Subscriber<? super V> s) {
+            impl.subscribe(s);
+        }
+
+        @Override
+        public <O> Stream<O> map(final Function<V, O, Exception> function) {
+            return new RxJavaStream<>(impl.map(new io.reactivex.functions.Function<V, O>() {
+                @Override
+                public O apply(V t) throws Exception {
+                    return function.apply(t);
+                }
+            }));
+        }
+
+        @Override
+        public <O> Stream<O> flatMap(final Function<? super V, ? extends Publisher<? extends O>, Exception> function,
+                int maxConcurrency) {
+            return new RxJavaStream<>(impl.flatMap(new io.reactivex.functions.Function<V, Publisher<? extends O>>() {
+                @Override
+                public Publisher<? extends O> apply(V t) throws Exception {
+                    return function.apply(t);
+                }
+            }, maxConcurrency));
+        }
+
+        @Override
+        public void subscribe(final Consumer<V> onResult, final Consumer<Throwable> onError, final Action onComplete) {
+            impl.subscribe(new io.reactivex.functions.Consumer<V>() {
+                @Override
+                public void accept(V t) throws Exception {
+                    onResult.accept(t);
+                }
+            }, new io.reactivex.functions.Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable t) throws Exception {
+                    onError.accept(t);
+                }
+            }, new io.reactivex.functions.Action() {
+                @Override
+                public void run() throws Exception {
+                    onComplete.run();
+                }
+            });
+        }
+
+        @Override
+        public Stream<V> onErrorResumeWith(final Function<Throwable, Publisher<V>, Exception> function) {
+            return new RxJavaStream<>(
+                    impl.onErrorResumeNext(new io.reactivex.functions.Function<Throwable, Publisher<? extends V>>() {
+                        @Override
+                        public Publisher<? extends V> apply(Throwable t) throws Exception {
+                            return function.apply(t);
+                        }
+                    }));
+        }
+
+        @Override
+        public void subscribe() {
+            impl.subscribe();
+        }
+    }
+
+    private static final class RxJavaSingle<V> implements Single<V> {
+
+        private final io.reactivex.Single<V> impl;
+
+        private RxJavaSingle(io.reactivex.Single<V> impl) {
+            this.impl = impl;
+        }
+
+        @Override
+        public Stream<V> toStream() {
+            return new RxJavaStream<>(impl.toFlowable());
+        }
+
+        @Override
+        public <O> Single<O> map(final Function<V, O, Exception> function) {
+            return new RxJavaSingle<>(impl.map(new io.reactivex.functions.Function<V, O>() {
+                @Override
+                public O apply(V t) throws Exception {
+                    return function.apply(t);
+                }
+            }));
+        }
+
+        @Override
+        public void subscribe(Subscriber<? super V> s) {
+            impl.toFlowable().subscribe(s);
+        }
+
+        @Override
+        public void subscribe(final Consumer<? super V> resultConsumer, final Consumer<Throwable> errorConsumer) {
+            impl.subscribe(new io.reactivex.functions.Consumer<V>() {
+                @Override
+                public void accept(V t) throws Exception {
+                    resultConsumer.accept(t);
+                }
+            }, new io.reactivex.functions.Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable t) throws Exception {
+                    errorConsumer.accept(t);
+                }
+            });
+        }
+
+        @Override
+        public <O> Single<O> flatMap(final Function<V, Publisher<O>, Exception> function) {
+            return new RxJavaSingle<>(impl.flatMap(new io.reactivex.functions.Function<V, SingleSource<O>>() {
+                @Override
+                public SingleSource<O> apply(V t) throws Exception {
+                    return io.reactivex.Single.fromPublisher(function.apply(t));
+                }
+            }));
+        }
+    }
+
+    private static final class RxJavaCompletable implements Completable {
+
+        private final io.reactivex.Completable impl;
+
+        RxJavaCompletable(io.reactivex.Completable impl) {
+            this.impl = impl;
+        }
+
+        @Override
+        public <V> Single<V> toSingle(V value) {
+            return new RxJavaSingle<>(impl.toSingleDefault(value));
+        }
+
+        @Override
+        public void subscribe(Subscriber<? super Void> s) {
+            impl.<Void>toFlowable().subscribe(s);
+        }
+    }
+}

commit 48b4370dd38139a8e6c024c9a794b957d343d1dd

OPENDJ-3489 Cannot set LDAPS to use LDAP keystore-based key/trust manager provider
Fix typo introduced in revision f2b5fa18b58db09562d03a7d247e21c111e78056

opendj-server-legacy/src/main/java/org/forgerock/opendj/adapter/server3x/Adapters.java
@@ -146,7 +146,7 @@ public final class Adapters {
                   @Override
                   public void handleInternalSearchEntry(InternalSearchOperation searchOperation,
                           SearchResultEntry searchEntry) throws DirectoryException {
-                      handler.handleEntry(partiallyWrap(searchEntry, 3));
+                      handler.handleEntry(from(searchEntry));
                   }
               };
 