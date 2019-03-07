67f3ada30a65e6ad1565e915ca76f0811dcff027
Maxim Thomas <maxim.thomas@gmail.com>
2019-03-07 09:28:24 +0300

    allow pre encoded passwords (#54)

--- opendj-packages/opendj-docker/bootstrap/setup.sh
+++ opendj-packages/opendj-docker/bootstrap/setup.sh
@@ -9,7 +9,6 @@ echo "Setting up default OpenDJ instance"
   --baseDN $BASE_DN -h localhost --rootUserPassword "$ROOT_PASSWORD" \
   --acceptLicense --no-prompt  $ADD_BASE_ENTRY #--sampleData 1
 
-
 if [ -d /opt/opendj/bootstrap/schema/ ]; then
   echo "Loading initial schema:"
   for file in /opt/opendj/bootstrap/schema/*;  do
@@ -19,7 +18,16 @@ if [ -d /opt/opendj/bootstrap/schema/ ]; then
 fi
 
 if [ -d /opt/opendj/bootstrap/data/ ]; then
-  echo "Loading initial data:"
+  #allow pre encoded passwords
+  /opt/opendj/bin/dsconfig \
+         set-password-policy-prop \
+         --bindDN "cn=Directory Manager" \
+         --bindPassword "$ROOT_PASSWORD" \
+         --policy-name "Default Password Policy" \
+         --set allow-pre-encoded-passwords:true \
+         --trustAll \
+         --no-prompt
+
   for file in /opt/opendj/bootstrap/data/*;  do
       echo "Loading $file ..."
       /opt/opendj/bin/ldapmodify -D "$ROOT_USER_DN" -h localhost -p $PORT -w $ROOT_PASSWORD -f $file
