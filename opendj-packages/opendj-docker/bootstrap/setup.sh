925897989377f69deb09787d75b8e5c26e6aea72
maximthomas <maxim.thomas@gmail.com>
2019-03-07 09:16:33 +0300

    move to allow pre encoded passwords to bootstrap data section

--- opendj-packages/opendj-docker/bootstrap/setup.sh
+++ opendj-packages/opendj-docker/bootstrap/setup.sh
@@ -9,17 +9,6 @@ echo "Setting up default OpenDJ instance"
   --baseDN $BASE_DN -h localhost --rootUserPassword "$ROOT_PASSWORD" \
   --acceptLicense --no-prompt  $ADD_BASE_ENTRY #--sampleData 1
 
-#allow pre encoded passwords
-/opt/opendj/bin/dsconfig \
-       set-password-policy-prop \
-       --bindDN "cn=Directory Manager" \
-       --bindPassword "$ROOT_PASSWORD" \
-       --policy-name "Default Password Policy" \
-       --set allow-pre-encoded-passwords:true \
-       --trustAll \
-       --no-prompt
-
-
 if [ -d /opt/opendj/bootstrap/schema/ ]; then
   echo "Loading initial schema:"
   for file in /opt/opendj/bootstrap/schema/*;  do
@@ -29,6 +18,16 @@ if [ -d /opt/opendj/bootstrap/schema/ ]; then
 fi
 
 if [ -d /opt/opendj/bootstrap/data/ ]; then
+  #allow pre encoded passwords
+  /opt/opendj/bin/dsconfig \
+         set-password-policy-prop \
+         --bindDN "cn=Directory Manager" \
+         --bindPassword "$ROOT_PASSWORD" \
+         --policy-name "Default Password Policy" \
+         --set allow-pre-encoded-passwords:true \
+         --trustAll \
+         --no-prompt
+
   for file in /opt/opendj/bootstrap/data/*;  do
       echo "Loading $file ..."
       /opt/opendj/bin/ldapmodify -D "$ROOT_USER_DN" -h localhost -p $PORT -w $ROOT_PASSWORD -f $file
