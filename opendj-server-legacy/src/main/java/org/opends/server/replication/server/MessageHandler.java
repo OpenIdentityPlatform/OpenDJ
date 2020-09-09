OpenIdentityPlatform/OpenDJ

commit 7160f59040db3b159e1d73d9ba58055e8806163d

OPENDJ-3337 Make sure ReplicationOffline messages are not repeatedly returned by cursors.
ReplicationOffline messages need special treatment, as they do not update server states.
Prevent returning already processed messages, especially when they're the last message for the domain.
Moreover set a server as following the replication when there are no new messages to send.

opendj-server-legacy/src/main/java/org/opends/server/replication/server/MessageHandler.java
@@ -304,6 +304,13 @@ class MessageHandler extends MonitorProvider<MonitorProviderCfg>
           synchronized (msgQueue) // TODO JNR why synchronize(msgQueue) here?
           {
             msg = lateQueue.removeFirst();
+            // By default a server is always not following. A weird case where messages not representing
+            // an operation may happen, making the late queue repeatedly fill and be emptied without ever
+            // getting the server out of state "not following".
+            if (lateQueue.isEmpty() && msgQueue.isEmpty())
+            {
+              following = true;
+            }
           }
           if (updateServerState(msg))
           {

opendj-server-legacy/src/main/java/org/opends/server/replication/server/ReplicationServerDomain.java
@@ -423,7 +423,9 @@ public class ReplicationServerDomain extends MonitorProvider<MonitorProviderCfg>
       }
       return true;
     }
-    return false;
+    // Replica offline messages should not get to a connected DS, they are meant to be
+    // exchanged only between RSes
+    return updateMsg instanceof ReplicaOfflineMsg;
   }
 
   private PreparedAssuredInfo getPreparedAssuredInfo(UpdateMsg updateMsg,

opendj-server-legacy/src/main/java/org/opends/server/replication/server/changelog/file/ReplicaCursor.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
- * Copyright 2014-2015 ForgeRock AS.
+ * Copyright 2014-2016 ForgeRock AS.
  */
 package org.opends.server.replication.server.changelog.file;
 
@@ -74,8 +74,20 @@ public class ReplicaCursor implements DBCursor<UpdateMsg>
    */
   public void setOfflineCSN(CSN offlineCSN)
   {
-    this.replicaOfflineMsg.set(
-        offlineCSN != null ? new ReplicaOfflineMsg(offlineCSN) : null);
+    if (offlineCSN != null)
+    {
+      ReplicaOfflineMsg prevOfflineMsg = this.replicaOfflineMsg.get();
+      if (prevOfflineMsg == null || prevOfflineMsg.getCSN().isOlderThan(offlineCSN))
+      {
+        // Do not spin if the the message for this replica has been changed. Either a newer
+        // message has arrived or the next cursor iteration will pick it up.
+        this.replicaOfflineMsg.compareAndSet(prevOfflineMsg, new ReplicaOfflineMsg(offlineCSN));
+      }
+    }
+    else
+    {
+      this.replicaOfflineMsg.set(null);
+    }
   }
 
   /** {@inheritDoc} */

commit 029beee63ef0fa2eb3915241c3b3c866d0355b6c

FIX Replication not catching up properly if there is many pending changes  https://github.com/OpenIdentityPlatform/OpenDJ/issues/141
Don't move to following mode if lateQueue can be refilled

Catching up when more than 100 changes behind was broken by 7160f59040.
We read chunks of 100 entries to lateQueue, so lateQueue being empty is
not neccessarily indicative
of that we are done, just that this chunk of 100 is done.
We need to check if lateQueue can be refilled before deciding we are
done.

Workaround for ReplicaOfflineMsg in lateQueue

OpenDJ have two queues for replication playback.
- lateQueue which is gradually filled from history
- msgQueue which is filled with new changes while on-line

At startup OpenDJ starts by processing lateQueue, refilling lateQueue
until we run out of changes or we find a change that is also in
msgQueue. At which point processing can switch over to msgQueue.

The problem we run into is when we have DS:es that are offline, then we
have ReplicaOfflineMsg messages in lateQueue.
ReplicaOfflineMsg does not affect domainState and re-fill of lateQueue
is based on domainState, so if the last entry in lateQueue is an
ReplicaOfflineMsg we will never move past it.
If we have an empty msgQueue the current code manages to step out, but
if we do get something in msgQueue it will never be able to do the step
from lateQueue to msgQueue.

After removing a member that DS will forever be part of the replication
data as offline.

To work-around this problem this patch pushes the messages from
lateQueue to msgQueue if:
- We just refilled lateQueue and
  - lateQueue is not full, so another refill is not likely to give us
anything else
  - lateQueue only contains ReplicaOfflineMsg (does not
contributesToDomainState)

This allows us to move from processing lateQueue to msgQueue.

A limitation with this is that it will only work as long as we
do not get more ReplicaOfflineMsg that we can fit in lateQueue.

opendj-server-legacy/src/main/java/org/opends/server/replication/server/MessageHandler.java
@@ -258,7 +258,7 @@ class MessageHandler extends MonitorProvider<MonitorProviderCfg>
            *           restart as usual
            *   load this change on the delayList
            */
-          fillLateQueue();
+          boolean queueContributesToDomainState = fillLateQueue();
           if (lateQueue.isEmpty())
           {
             // we could not find any messages in the changelog
@@ -283,6 +283,15 @@ class MessageHandler extends MonitorProvider<MonitorProviderCfg>
             UpdateMsg msg = lateQueue.first();
             synchronized (msgQueue)
             {
+              if (!queueContributesToDomainState)
+              {
+               // If nothing in the queue contributesToDomainState, add it all to msgQueue so we can get out of here
+               while(!lateQueue.isEmpty())
+               {
+                 msgQueue.add(lateQueue.removeFirst());
+               }
+              }
+
               if (msgQueue.contains(msg))
               {
                 /* we finally catch up with the regular queue */
@@ -307,9 +316,14 @@ class MessageHandler extends MonitorProvider<MonitorProviderCfg>
             // By default a server is always not following. A weird case where messages not representing
             // an operation may happen, making the late queue repeatedly fill and be emptied without ever
             // getting the server out of state "not following".
+
             if (lateQueue.isEmpty() && msgQueue.isEmpty())
             {
-              following = true;
+              CSN nextChange = findOldestCSNFromReplicaDBs();
+              if (nextChange == null)
+              {
+                following = true;
+              }
             }
           }
           if (updateServerState(msg))
@@ -363,15 +377,25 @@ class MessageHandler extends MonitorProvider<MonitorProviderCfg>
    * Fills the late queue with the most recent changes, accepting only the
    * messages from provided replica ids.
    */
-  private void fillLateQueue() throws ChangelogException
+  private boolean fillLateQueue() throws ChangelogException
   {
+    boolean contributesToDomainState = false;
     try (DBCursor<UpdateMsg> cursor = replicationServerDomain.getCursorFrom(serverState);)
     {
       while (cursor.next() && isLateQueueBelowThreshold())
       {
-        lateQueue.add(cursor.getRecord());
+        UpdateMsg msg = cursor.getRecord();
+        lateQueue.add(msg);
+        contributesToDomainState |= msg.contributesToDomainState();
       }
     }
+
+    // If we stopped because we filled the queue, set contributesToDomainState to true
+    if (!contributesToDomainState && !isLateQueueBelowThreshold())
+    {
+      contributesToDomainState = true;
+    }
+    return contributesToDomainState;
   }
 
   private boolean isLateQueueBelowThreshold()