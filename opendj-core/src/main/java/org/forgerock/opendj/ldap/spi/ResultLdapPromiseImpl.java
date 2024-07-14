ae2c3be6e7f1104b7bfec0c6dfef465520b7c773
Valera V Harseko <vharseko@3a-systems.ru>
2024-07-14 15:51:50 +0300

    Revert "[#167] FIX Abandon async search"
    This reverts commit 45cc91051686971f2207759b815c41ffd5752199.

--- opendj-core/src/main/java/org/forgerock/opendj/ldap/spi/ResultLdapPromiseImpl.java
+++ opendj-core/src/main/java/org/forgerock/opendj/ldap/spi/ResultLdapPromiseImpl.java
@@ -160,12 +160,4 @@ public abstract class ResultLdapPromiseImpl<R extends Request, S extends Result>
     public boolean checkForTimeout() {
         return true;
     }
-
-    @Override
-    public boolean cancel(boolean mayInterruptIfRunning) {
-        if (timestamp==0L) {
-            return super.cancel(mayInterruptIfRunning);
-        }
-        return false;
-    }
 }
