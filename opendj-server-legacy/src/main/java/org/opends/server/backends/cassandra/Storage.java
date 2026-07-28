dc3e65ec81705f75d8760b628fd8b395bfa0e6ec
Valery Kharseko <vharseko@3a-systems.ru>
2026-07-28 19:18:44 +0300

    Fix java/overly-large-range CodeQL alerts by correcting the A-z letter ranges (#779)

--- opendj-server-legacy/src/main/java/org/opends/server/backends/cassandra/Storage.java
+++ opendj-server-legacy/src/main/java/org/opends/server/backends/cassandra/Storage.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
- * Copyright 2023-2024 3A Systems, LLC.
+ * Copyright 2023-2026 3A Systems, LLC.
  */
 package org.opends.server.backends.cassandra;
 
@@ -147,11 +147,11 @@ public class Storage implements org.opends.server.backends.pluggable.spi.Storage
 	}
 
 	String getKeyspaceName() {
-		return "\""+System.getProperty("keyspace",config.getDBDirectory()).replaceAll("[^a-zA-z0-9_]", "_")+"\"";
+		return "\""+System.getProperty("keyspace",config.getDBDirectory()).replaceAll("[^a-zA-Z0-9_]", "_")+"\"";
 	}
 	
 	String getTableName() {
-		return getKeyspaceName()+".\""+config.getBackendId().replaceAll("[^a-zA-z0-9_]", "_")+"\"";
+		return getKeyspaceName()+".\""+config.getBackendId().replaceAll("[^a-zA-Z0-9_]", "_")+"\"";
 	}
 	
 	@Override
