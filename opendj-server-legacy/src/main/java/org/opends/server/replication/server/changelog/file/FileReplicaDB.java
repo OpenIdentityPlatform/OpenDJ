6332cb2c6a793807fa4b0ebe9d3889434248dbea
Valera V Harseko <vharseko@3a-systems.ru>
2026-08-03 16:47:47 +0300

    [#819] Release the log when a changelog DB constructor cannot read its limits
    FileReplicaDB opened its log and read the CSN limits afterwards: when that
    read failed, the constructor threw with the Log instance already reference
    counted in the JVM-wide log cache and nobody left to close it. The entry
    stayed pinned together with its file handles, and the next open of the same
    path got the stale instance back. FileChangeNumberIndexDB had the same shape
    around its change number initialization.
    
    Close the log before rethrowing, as shutdown() does with the same log.

--- opendj-server-legacy/src/main/java/org/opends/server/replication/server/changelog/file/FileReplicaDB.java
+++ opendj-server-legacy/src/main/java/org/opends/server/replication/server/changelog/file/FileReplicaDB.java
@@ -12,6 +12,7 @@
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
  * Copyright 2014-2016 ForgeRock AS.
+ * Portions Copyright 2026 3A Systems, LLC.
  */
 package org.opends.server.replication.server.changelog.file;
 
@@ -113,7 +114,17 @@ class FileReplicaDB
     this.replicationServer = replicationServer;
     this.replicationEnv = replicationEnv;
     this.log = createLog(replicationEnv, cryptoSuite);
-    this.csnLimits = new CSNLimits(readOldestCSN(), readNewestCSN());
+    try
+    {
+      this.csnLimits = new CSNLimits(readOldestCSN(), readNewestCSN());
+    }
+    catch (ChangelogException | RuntimeException e)
+    {
+      // This instance never escapes the failed constructor, so nobody could ever call shutdown()
+      // to release the reference taken on the log by createLog().
+      log.close();
+      throw e;
+    }
 
     DirectoryServer.deregisterMonitorProvider(dbMonitor);
     DirectoryServer.registerMonitorProvider(dbMonitor);
